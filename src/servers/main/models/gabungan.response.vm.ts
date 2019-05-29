bb4f1062782fcaee962bfdd51b4706a4f941b91f
Adry Andreas <adryhappier@gmail.com>
2019-05-29 16:27:12 +0700

    fix import

--- src/servers/main/models/gabungan.response.vm.ts
+++ src/servers/main/models/gabungan.response.vm.ts
@@ -1,11 +1,10 @@
 import { ApiModelProperty } from '../../../shared/external/nestjs-swagger';
 import { GabunganSearchVm } from './gabungan.vm';
-import { BaseMetaResponseVm } from 'src/shared/models/base-meta-response.vm';
+import { BaseMetaResponseVm } from '../../../shared/models/base-meta-response.vm';
 
 export class GabunganFindAllResponseVm extends BaseMetaResponseVm  {
 
   @ApiModelProperty({ type: () => [GabunganSearchVm] })
   data: GabunganSearchVm[];
 }
-
 
