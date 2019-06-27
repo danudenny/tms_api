314b57058436803a53940d18128660febdd03524
Muhammad Labib Ramadhan <mlabib.ramadhan@gmail.com>
2019-06-28 06:48:50 +0700

    Enable auth guard on web awb trouble controller

--- src/servers/main/controllers/web/web-awb-trouble.controller.ts
+++ src/servers/main/controllers/web/web-awb-trouble.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post, UseGuards } from '@nestjs/common';
 
 import { ApiBearerAuth, ApiOkResponse, ApiUseTags } from '../../../../shared/external/nestjs-swagger';
+import { AuthenticatedGuard } from '../../../../shared/guards/authenticated.guard';
 import { AwbTroublePayloadVm } from '../../models/awb-trouble-payload.vm';
 import { AwbTroubleResponseVm } from '../../models/awb-trouble-response.vm';
 import { WebAwbTroubleService } from '../../services/web/web-awb-trouble.service';
@@ -11,7 +12,7 @@ export class WebAwbTroubleControlelr {
   @Post('troubledList')
   @HttpCode(HttpStatus.OK)
   @ApiBearerAuth()
-  // @UseGuards(AuthenticatedGuard)
+  @UseGuards(AuthenticatedGuard)
   @ApiOkResponse({ type: AwbTroubleResponseVm })
   public async troubledList(@Body() payload: AwbTroublePayloadVm) {
     return WebAwbTroubleService.findAllByRequest(payload);
