b18bae6ba49a4ac12c9e63e883c432db6b46dcb3
Muhammad Labib Ramadhan <mlabib.ramadhan@gmail.com>
2019-06-28 06:44:05 +0700

    Add web awb trouble service

--- src/servers/main/controllers/web/web-awb-trouble.controller.ts
+++ src/servers/main/controllers/web/web-awb-trouble.controller.ts
@@ -1,11 +1,9 @@
 import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 
 import { ApiBearerAuth, ApiOkResponse, ApiUseTags } from '../../../../shared/external/nestjs-swagger';
-import { AwbTrouble } from '../../../../shared/orm-entity/awb-trouble';
-import { MetaService } from '../../../../shared/services/meta.service';
-import { OrionRepositoryService } from '../../../../shared/services/orion-repository.service';
 import { AwbTroublePayloadVm } from '../../models/awb-trouble-payload.vm';
 import { AwbTroubleResponseVm } from '../../models/awb-trouble-response.vm';
+import { WebAwbTroubleService } from '../../services/web/web-awb-trouble.service';
 
 @ApiUseTags('Web Awb Trouble')
 @Controller('web/pod')
@@ -16,31 +14,6 @@ export class WebAwbTroubleControlelr {
   // @UseGuards(AuthenticatedGuard)
   @ApiOkResponse({ type: AwbTroubleResponseVm })
   public async troubledList(@Body() payload: AwbTroublePayloadVm) {
-    const repository = new OrionRepositoryService(AwbTrouble);
-    const q = repository.findAllRaw();
-
-    payload.globalSearchFields = [
-      {
-        field: 'awbNumber',
-      },
-    ];
-
-    payload.applyToOrionRepositoryQuery(q, true);
-
-    q.selectRaw(
-      ['awb_trouble_id', 'awbTroubleId'],
-      ['awb_number', 'awbNumber'],
-      ['status_resolve_id', 'statusResolveId'],
-      ['created_time', 'scanInDateTime'],
-    );
-
-    const data = await q.exec();
-    const total = await q.countWithoutTakeAndSkip();
-
-    const response = new AwbTroubleResponseVm();
-    response.data = data;
-    response.paging = MetaService.set(payload.page, payload.limit, total);
-
-    return response;
+    return WebAwbTroubleService.findAllByRequest(payload);
   }
 }
---
+++ src/servers/main/services/web/web-awb-trouble.service.ts
@@ -0,0 +1,37 @@
+import { BaseMetaPayloadVm } from '../../../../shared/models/base-meta-payload.vm';
+import { AwbTrouble } from '../../../../shared/orm-entity/awb-trouble';
+import { MetaService } from '../../../../shared/services/meta.service';
+import { OrionRepositoryService } from '../../../../shared/services/orion-repository.service';
+import { AwbTroubleResponseVm } from '../../models/awb-trouble-response.vm';
+
+export class WebAwbTroubleService {
+  public static async findAllByRequest(payload: BaseMetaPayloadVm) {
+    const repository = new OrionRepositoryService(AwbTrouble);
+    const q = repository.findAllRaw();
+
+    payload.globalSearchFields = [
+      {
+        field: 'awbNumber',
+      },
+    ];
+
+    payload.applyToOrionRepositoryQuery(q, true);
+
+    // TODO: resolve desc, awbTroubleName
+    q.selectRaw(
+      ['awb_trouble_id', 'awbTroubleId'],
+      ['awb_number', 'awbNumber'],
+      ['status_resolve_id', 'statusResolveId'],
+      ['created_time', 'scanInDateTime'],
+    );
+
+    const data = await q.exec();
+    const total = await q.countWithoutTakeAndSkip();
+
+    const response = new AwbTroubleResponseVm();
+    response.data = data;
+    response.paging = MetaService.set(payload.page, payload.limit, total);
+
+    return response;
+  }
+}
