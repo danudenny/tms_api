6c086d432ef226a83328f63fd57ce06194ba0b6d
Adry Andreas <adryhappier@gmail.com>
2019-07-10 10:14:28 +0700

    add config cloud storage s3

--- src/config/default.js
+++ src/config/default.js
@@ -17,6 +17,13 @@ module.exports = {
     root: path.resolve(__dirname, ".."),
     assets: path.resolve(__dirname, "..", "assets"),
   },
+  cloudStorage: {
+    cloudUrl: 'https://sicepattesting.s3-ap-southeast-1.amazonaws.com',
+    cloudBucket: 'sicepattesting',
+    cloudRegion: 'ap-southeast-1',
+    cloudAccessKeyId: 'AKIAJHHTEMTLK42N7TQA',
+    cloudSecretAccessKey: '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',
+  },
   sentry: { dsn: 'http://69d535e3c29a42de850aafd6e04b6e44@sicepat-sentry.eastus.cloudapp.azure.com:9000/3' },
   servers: getAllServerConfigurations("default.js"),
 };
