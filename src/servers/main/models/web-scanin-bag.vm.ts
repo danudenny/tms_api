34aa4c443925931a86427cccb906609affad5f7d
Mohammad Satria <mohammadsatria.sicepat@gmail.com>
2019-07-25 14:42:12 +0700

    modify message required field

--- src/servers/main/models/web-scanin-bag.vm.ts
+++ src/servers/main/models/web-scanin-bag.vm.ts
@@ -1,7 +1,13 @@
 import { ApiModelProperty } from '../../../shared/external/nestjs-swagger';
+import { IsDefined } from 'class-validator';
 
 export class WebScanInBagVm  {
 
-  @ApiModelProperty()
+  @ApiModelProperty({
+    example: ['112039', '1120394'],
+    skipValidation: true,
+  })
+  // TODO: validation if array length = 0
+  @IsDefined({message: 'No gabung paket harus diisi'})
   bagNumber: string[];
 }
--- src/servers/main/models/web-scanin.vm.ts
+++ src/servers/main/models/web-scanin.vm.ts
@@ -1,6 +1,12 @@
 import { ApiModelProperty, ApiModelPropertyOptional } from '../../../shared/external/nestjs-swagger';
+import { IsDefined } from 'class-validator';
 
 export class WebScanInVm  {
-  @ApiModelProperty()
+  @ApiModelProperty({
+    example: ['112039', '11203922'],
+    skipValidation: true,
+  })
+  // TODO: validation if array length = 0
+  @IsDefined({message: 'No Resi harus diisi'})
   awbNumber: string[];
 }
