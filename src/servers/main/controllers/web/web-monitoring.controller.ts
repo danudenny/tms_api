765363daba3670b08c0f09197b5be2a6cb154961
darmadi1604 <darmadi1604@yahoo.com>
2019-08-13 14:19:39 +0700

    update new menu monitoring bag in

---
+++ src/servers/main/controllers/web/web-monitoring.controller.ts
@@ -0,0 +1,21 @@
+import { Body, Controller, HttpCode, HttpStatus, Post, UseGuards } from '@nestjs/common';
+
+import { ApiBearerAuth, ApiOkResponse, ApiUseTags } from '../../../../shared/external/nestjs-swagger';
+import { AuthenticatedGuard } from '../../../../shared/guards/authenticated.guard';
+import { BagMonitoringPayloadVm } from '../../models/bag-monitoring-payload.vm';
+import { BagMonitoringResponseVm } from '../../models/bag-monitoring-response.vm';
+import { WebMonitoringService } from '../../services/web/web-monitoring.service';
+import { PermissionTokenGuard } from 'src/shared/guards/permission-token.guard';
+
+@ApiUseTags('Web Monitoring')
+@Controller('web/monitoring')
+export class WebMonitoringController {
+  @Post('bagIn')
+  @HttpCode(HttpStatus.OK)
+  @ApiBearerAuth()
+  @UseGuards(AuthenticatedGuard, PermissionTokenGuard)
+  @ApiOkResponse({ type: BagMonitoringResponseVm })
+  public async troubledList(@Body() payload: BagMonitoringPayloadVm) {
+    return WebMonitoringService.findAllByRequest(payload);
+  }
+}
