fb273f1c1704a100ff1bfaba319c23cc1e34c846
Muhammad Labib Ramadhan <mlabib.ramadhan@gmail.com>
2019-08-22 06:55:54 +0700

    Improve mobile sync metadata persistence,  remove uneeded codes

--- src/servers/main/services/web/web-delivery-list.service.ts
+++ src/servers/main/services/web/web-delivery-list.service.ts
@@ -1,10 +1,7 @@
-import { HttpStatus, Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { toInteger } from 'lodash';
 import moment = require('moment');
 
-import { GetRoleResult } from '../../../../shared/models/get-role-result';
-import { AuthService } from '../../../../shared/services/auth.service';
-import { RequestErrorService } from '../../../../shared/services/request-error.service';
 import { MetaService } from '../../../../shared/services/meta.service';
 import { RawQueryService } from '../../../../shared/services/raw-query.service';
 import { WebDeliveryListFilterPayloadVm } from '../../models/web-delivery-payload.vm';
@@ -12,10 +9,9 @@ import { WebScanInListResponseVm } from '../../models/web-scanin-list.response.v
 
 @Injectable()
 export class WebDeliveryListService {
-  constructor() {}
   async findAllDeliveryList(
     payload: WebDeliveryListFilterPayloadVm,
-    ): Promise<WebScanInListResponseVm> {
+  ): Promise<WebScanInListResponseVm> {
     const page = toInteger(payload.page) || 1;
     const take = toInteger(payload.limit) || 10;
 
@@ -25,10 +21,13 @@ export class WebDeliveryListService {
 
     const [query, parameters] = RawQueryService.escapeQueryWithParameters(
       'select pod_scanin_date_time as "podScaninDateTime", awb_id as "awbId",branch_id as "branchId", user_id as "employeId" from pod_scan where pod_scanin_date_time >= :start AND pod_scanin_date_time <= :end LIMIT :take OFFSET :offset',
-      { take, start, end , offset },
+      { take, start, end, offset },
     );
 
-    const [querycount, parameterscount] = RawQueryService.escapeQueryWithParameters(
+    const [
+      querycount,
+      parameterscount,
+    ] = RawQueryService.escapeQueryWithParameters(
       'select count (*) from pod_scan where pod_scanin_date_time >= :start AND pod_scanin_date_time <= :end ',
       { start, end },
     );
@@ -41,28 +40,5 @@ export class WebDeliveryListService {
     result.paging = MetaService.set(page, take, toInteger(total[0].count));
 
     return result;
-  }
-
-  async permissionRoles(): Promise<GetRoleResult> {
-    const authMeta = AuthService.getAuthMetadata();
-    // const user = await this.userRepository.findByUserIdWithRoles());
-    // check user present
-    if (!!authMeta) {
-      // Populate return value
-      const result = new GetRoleResult();
-      result.userId = authMeta.userId;
-      result.username = authMeta.username;
-      result.email = authMeta.email;
-      result.displayName = authMeta.displayName;
-
-      return result;
-    } else {
-      RequestErrorService.throwObj(
-        {
-          message: 'global.error.USER_NOT_FOUND',
-        },
-        HttpStatus.BAD_REQUEST,
-      );
-    }
   }
 }
