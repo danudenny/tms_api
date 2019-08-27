f7a06c02d3910f91c8e6bdaba9bafe33d2b48c02
darmadi1604 <darmadi1604@yahoo.com>
2019-08-27 11:18:53 +0700

    update bugs fixing query on scanout deliver list

--- src/servers/main/services/web/web-delivery-out.service.ts
+++ src/servers/main/services/web/web-delivery-out.service.ts
@@ -1009,8 +1009,9 @@ export class WebDeliveryOutService {
     q.innerJoin(e => e.doPodDeliverDetails.awbItem.awb, 't4', j =>
       j.andWhere(e => e.isDeleted, w => w.isFalse()),
     );
-    q.andWhere(e => e.doPodDeliverDetails.awbStatusIdLast, w => w.equals(14000));
-    q.orWhere(e => e.doPodDeliverDetails.awbStatusIdLast, w => w.equals(21500));
+    q.andWhereIsolated(qw => {
+      qw.where(e => e.doPodDeliverDetails.awbStatusIdLast, w => w.equals(14000).or.equals(21500));
+    });
     q.groupByRaw('t1.do_pod_deliver_id, t2.nickname, t4.is_cod');
 
     const data = await q.exec();
