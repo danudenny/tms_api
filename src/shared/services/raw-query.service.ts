de569c14b1b9aac3958af90b32f6529fc83a9340
Adry Andreas <adryhappier@gmail.com>
2019-09-16 13:32:39 +0700

    update raw query service

--- src/shared/services/raw-query.service.ts
+++ src/shared/services/raw-query.service.ts
@@ -14,24 +14,35 @@ export class RawQueryService {
   public static query(sql: string, parameters?: any[]) {
     return this.manager.query(sql, parameters);
   }
+
+  public static async queryCount(sql: string, sqlParamters?: any[]) {
+    const sqlModified = `SELECT COUNT(1) AS total FROM (${sql}) as t`;
+    const results = await RawQueryService.query(sqlModified, sqlParamters);
+    return results && results.length ? + results[0].total : 0;
+  }
+
   public static async queryWithPaginate(
     sql: string,
     page: number = 1,
     limit: number = 100,
+    total: number = 0,
     countField: string = '1',
     distinct: boolean = false,
   ) {
-    const sqlCount = `SELECT COUNT(${
-      distinct ? 'DISTINCT ' : ''
-    }${countField}) AS total FROM (${sql}) as t`;
+    let totalData = total;
+    //  calculate total data
+    if (total == 0) {
+      const sqlCount = `SELECT COUNT(${
+        distinct ? 'DISTINCT ' : ''
+      }${countField}) AS total FROM (${sql}) as t`;
 
-    const countData = await this.manager.query(sqlCount);
-    const totalData =
-    countData && countData.length ? + countData[0].total : 0;
+      const countData = await this.manager.query(sqlCount);
+      totalData = countData && countData.length ? + countData[0].total : 0;
+    }
 
     const offset = (page - 1) * limit;
     const data = await this.manager.query(
-      sql + `LIMIT ${limit} OFFSET ${offset}`,
+      sql + ` LIMIT ${limit} OFFSET ${offset}`,
     );
     const paging = MetaService.set(page, limit, totalData);
     return [ data,  paging ];
