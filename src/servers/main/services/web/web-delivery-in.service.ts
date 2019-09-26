4e755412c1fd8d71c09a5a3ee5f5ad2e7846aae0
Mohammad Satria <mohammadsatria.sicepat@gmail.com>
2019-09-26 10:48:13 +0700

    query error do pod detail

--- src/servers/main/services/web/web-delivery-in.service.ts
+++ src/servers/main/services/web/web-delivery-in.service.ts
@@ -68,8 +68,8 @@ export class WebDeliveryInService {
       ['t2.awb_number', 'awbNumber'],
       ['t3.branch_name', 'branchNameScan'],
       ['t3.branch_code', 'branchCodeScan'],
-      ['t4.branch_name', 'branchNameFrom'],
-      ['t4.branch_code', 'branchCodeFrom'],
+      ['t3.branch_name', 'branchNameFrom'],
+      ['t3.branch_code', 'branchCodeFrom'],
       ['t5.nickname', 'employeeName'],
     );
 
@@ -79,9 +79,9 @@ export class WebDeliveryInService {
     q.innerJoin(e => e.branch, 't3', j =>
       j.andWhere(e => e.isDeleted, w => w.isFalse()),
     );
-    q.innerJoin(e => e.do_pod_detail.doPod.branch, 't4', j =>
-      j.andWhere(e => e.isDeleted, w => w.isFalse()),
-    );
+    // q.innerJoin(e => e.doPodDetail.doPod.branch, 't4', j =>
+    //   j.andWhere(e => e.isDeleted, w => w.isFalse()),
+    // );
     q.innerJoin(e => e.employee, 't5', j =>
       j.andWhere(e => e.isDeleted, w => w.isFalse()),
     );
@@ -139,7 +139,7 @@ export class WebDeliveryInService {
         'bagNumberCode',
       ],
       ['t3.branch_name', 'branchNameScan'],
-      ['t4.branch_name', 'branchNameFrom'],
+      ['t3.branch_name', 'branchNameFrom'],
       ['t5.nickname', 'employeeName'],
       ['COUNT (t7.*)', 'totalAwb'],
     );
@@ -156,9 +156,9 @@ export class WebDeliveryInService {
     q.innerJoin(e => e.branch, 't3', j =>
       j.andWhere(e => e.isDeleted, w => w.isFalse()),
     );
-    q.innerJoin(e => e.do_pod_detail.doPod.branch, 't4', j =>
-      j.andWhere(e => e.isDeleted, w => w.isFalse()),
-    );
+    // q.innerJoin(e => e.doPodDetail.doPod.branch, 't4', j =>
+    //   j.andWhere(e => e.isDeleted, w => w.isFalse()),
+    // );
     q.innerJoin(e => e.user.employee, 't5', j =>
       j.andWhere(e => e.isDeleted, w => w.isFalse()),
     );
