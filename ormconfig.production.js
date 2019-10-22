9427f812ea0150ae3d73defd684c432934f5798e
Adry Andreas <adryhappier@gmail.com>
2019-10-22 16:22:27 +0700

    :gear: config db and deploy

--- ormconfig.production.js
+++ ormconfig.production.js
@@ -1,12 +1,23 @@
 module.exports = {
   type: 'postgres',
-  host:
-    'sicepat-tms-masterdata-staging.cchjcxaiivov.ap-southeast-1.rds.amazonaws.com',
-  port: 5432,
-  username: 'sicepatstaging',
-  password: 's1c3p4T$t46Ingb05$sQu',
-  database: 'sicepattmsstaging2',
-  schema: 'public',
+  replication: {
+    master: {
+      host: 'sicepat-tms-reborn.cchjcxaiivov.ap-southeast-1.rds.amazonaws.com',
+      port: 5432,
+      username: 'sicepattmsuser',
+      password: 's1c3p4Ttm$us3R3ncrypT3dbo05$',
+      database: 'sicepattms',
+      schema: 'public',
+    },
+    slaves: [{
+      host: 'sicepat-tms-reborn.cchjcxaiivov.ap-southeast-1.rds.amazonaws.com',
+      port: 5432,
+      username: 'sicepattmsuser',
+      password: 's1c3p4Ttm$us3R3ncrypT3dbo05$',
+      database: 'sicepattms',
+      schema: 'public',
+    }]
+  },
   entities: ['dist/shared/orm-entity/*.js'],
   migrations: ['dist/shared/orm-migration/*.js'],
   subscribers: ['dist/shared/orm-subscriber/*.js'],
