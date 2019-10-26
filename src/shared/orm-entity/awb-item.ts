3ce440a75814afbbd9f8f6646dad8957a64def29
adityapfm99 <adityapfm99@gmail.com>
2019-10-26 13:30:56 +0700

    :snowman: update entity awbItem

--- src/shared/orm-entity/awb-item.ts
+++ src/shared/orm-entity/awb-item.ts
@@ -315,6 +315,25 @@ export class AwbItem extends BaseEntity {
   })
   isDeleted: boolean;
 
+  @Column('bigint', {
+    nullable: false,
+    name: 'partner_logistic_awb',
+  })
+  partnerLogisticAwb: number;
+
+  @Column('bigint', {
+    nullable: false,
+    name: 'origin_awb_id',
+  })
+  originAwbId: number;
+
+  @Column('boolean', {
+    nullable: false,
+    default: () => 'false',
+    name: 'is_return',
+  })
+  isReturn: boolean;
+
   @Column('numeric', {
     nullable: false,
     default: () => '0',
