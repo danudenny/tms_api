2bc11149af2948868e3f8689f17da082da28b3c1
adityapfm99 <adityapfm99@gmail.com>
2019-10-29 11:33:15 +0700

    :snowman: add endpoint awb return list

--- src/servers/main/services/web/web-awb-return.service.ts
+++ src/servers/main/services/web/web-awb-return.service.ts
@@ -3,6 +3,11 @@ import { AwbService } from '../v1/awb.service';
 import { CustomerAddress } from '../../../../shared/orm-entity/customer-address';
 import { map } from 'lodash';
 import { Awb } from '../../../../shared/orm-entity/awb';
+import { WebReturListResponseVm } from '../../models/web-retur-list-response.vm';
+import { BaseMetaPayloadVm } from '../../../../shared/models/base-meta-payload.vm';
+import { AwbReturn } from '../../../../shared/orm-entity/awb-return';
+import { OrionRepositoryService } from '../../../../shared/services/orion-repository.service';
+import { MetaService } from '../../../../shared/services/meta.service';
 
 export class WebAwbReturnService {
   static async getAwb(
@@ -46,11 +51,52 @@ export class WebAwbReturnService {
       where: {
         awbId: payload.awbId,
         isDeleted: false,
-      }
+      },
     });
     // create table awb item
     // create table awb history;
     // create table awb return;
     return null;
+  }
+
+  static async listReturn(
+    payload: BaseMetaPayloadVm,
+  ): Promise<WebReturListResponseVm> {
+    // mapping field
+    payload.fieldResolverMap['awbReturnId'] = 't1.awb_return_id';
+    payload.fieldResolverMap['originAwbId'] = 't1.origin_awb_id';
+    payload.fieldResolverMap['originAwbNumber'] = 't1.origin_awb_number';
+    payload.fieldResolverMap['returnAwbId'] = 't1.return_awb_id';
+    payload.fieldResolverMap['returnAwbNumber'] = 't1.return_awb_number';
+    payload.fieldResolverMap['isPartnerLogistic'] = 't1.is_partner_logistic';
+    payload.fieldResolverMap['branchId'] = 't1.branch_id';
+    payload.fieldResolverMap['createdTime'] = 't1.created_time';
+
+    const repo = new OrionRepositoryService(AwbReturn, 't1');
+    const q = repo.findAllRaw();
+
+    payload.applyToOrionRepositoryQuery(q, true);
+
+    q.selectRaw(
+      ['t1.awb_return_id', 'awbReturnId'],
+      ['t1.origin_awb_id', 'originAwbId'],
+      ['t1.origin_awb_number', 'originAwbNumber'],
+      ['t1.return_awb_id', 'returnAwbId'],
+      ['t1.return_awb_number', 'returnAwbNumber'],
+      ['t1.branch_id', 'branchId'],
+      ['t1.created_time', 'createdTime'],
+    );
+
+    q.andWhere(e => e.isDeleted, w => w.isFalse());
+
+    const data = await q.exec();
+    const total = await q.countWithoutTakeAndSkip();
+
+    const result = new WebReturListResponseVm();
+
+    result.data = data;
+    result.paging = MetaService.set(payload.page, payload.limit, total);
+
+    return result;
   }
 }
