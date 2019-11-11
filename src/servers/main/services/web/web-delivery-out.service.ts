b21320459df4ee32e8a63a3e01d40657642855df
adityapfm99 <adityapfm99@gmail.com>
2019-11-11 11:43:29 +0700

    :snowman: update condition bagOrderDetail

--- src/servers/main/services/web/web-delivery-out.service.ts
+++ src/servers/main/services/web/web-delivery-out.service.ts
@@ -1010,6 +1010,7 @@ export class WebDeliveryOutService {
   }
 
   async bagOrderDetail(payload: BagAwbVm): Promise<BagOrderResponseVm> {
+    const dpdd = payload.doPodId
     const bag = await BagService.validBagNumber(payload.bagNumber);
     if (bag) {
       const qz = createQueryBuilder();
@@ -1027,9 +1028,15 @@ export class WebDeliveryOutService {
         'bag_item_awb',
         'bag_item_awb.bag_item_id = bag_item_id.bag_item_id',
       );
-      qz.where('bag.bag_number = :bag AND bag_item_id.bag_seq = :seq ', {
+      qz.innerJoin(
+        'do_pod_detail_bag',
+        'do_pod_detail_bag',
+        'do_pod_detail_bag.bag_item_id = bag_item_id.bag_item_id',
+      );
+      qz.where('bag.bag_number = :bag AND bag_item_id.bag_seq = :seq AND do_pod_detail_bag.do_pod_id = :dpdd', {
         bag: bag.bag.bagNumber,
         seq: bag.bagSeq,
+        dpdd: dpdd,
       });
 
       const data = await qz.getRawMany();
