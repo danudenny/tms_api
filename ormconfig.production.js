3e398f0e1b3c21df5e0e60252abd55289ebd24fc
Adry Andreas <adryhappier@gmail.com>
2019-11-19 09:59:59 +0700

    :gear: config for production

--- ormconfig.production.js
+++ ormconfig.production.js
@@ -10,8 +10,7 @@ module.exports = {
   type: 'postgres',
   replication: {
     master: {
-      host:
-        'sicepat-tmspod-staging.cchjcxaiivov.ap-southeast-1.rds.amazonaws.com',
+      host: 'http://sicepat-tms-new.cchjcxaiivov.ap-southeast-1.rds.amazonaws.com',
       port: 5432,
       username: 'sicepattmsuser',
       password: 's1c3p4Ttm$us3R3ncrypT3dbo05$',
@@ -20,35 +19,7 @@ module.exports = {
     },
     slaves: [
       {
-        host:
-          'sicepat-tmspod-frontend-staging.cchjcxaiivov.ap-southeast-1.rds.amazonaws.com',
-        port: 5432,
-        username: 'sicepattmsuser',
-        password: 's1c3p4Ttm$us3R3ncrypT3dbo05$',
-        database: 'sicepattms',
-        schema: 'public',
-      },
-      {
-        host:
-          'sicepat-tmspod-api-staging.cchjcxaiivov.ap-southeast-1.rds.amazonaws.com',
-        port: 5432,
-        username: 'sicepattmsuser',
-        password: 's1c3p4Ttm$us3R3ncrypT3dbo05$',
-        database: 'sicepattms',
-        schema: 'public',
-      },
-      {
-        host:
-          'sicepat-tmspod-background-staging.cchjcxaiivov.ap-southeast-1.rds.amazonaws.com',
-        port: 5432,
-        username: 'sicepattmsuser',
-        password: 's1c3p4Ttm$us3R3ncrypT3dbo05$',
-        database: 'sicepattms',
-        schema: 'public',
-      },
-      {
-        host:
-          'sicepat-tmspod-process-staging.cchjcxaiivov.ap-southeast-1.rds.amazonaws.com',
+        host: 'sicepat-tms-aurora-rep-cluster.cluster-ro-cchjcxaiivov.ap-southeast-1.rds.amazonaws.com',
         port: 5432,
         username: 'sicepattmsuser',
         password: 's1c3p4Ttm$us3R3ncrypT3dbo05$',
--- src/config/default.js
+++ src/config/default.js
@@ -3,9 +3,9 @@ const path = require("path");
 
 module.exports = {
   redis: {
-    host: '3.1.243.32',
+    host: 'localhost',
     port: '6379',
-    password: '9F864DAF0B09974AA3F0E90646EEFEA3',
+    password: '',
     db: process.env.NODE_ENV === 'test' ? 1 : 0,
   },
   jwt: {
@@ -18,8 +18,8 @@ module.exports = {
     assets: path.resolve(__dirname, '..', 'assets'),
   },
   cloudStorage: {
-    cloudUrl: 'https://sicepattesting.s3-ap-southeast-1.amazonaws.com',
-    cloudBucket: 'sicepattesting',
+    cloudUrl: 'https://sicepatmasterdata.s3-ap-southeast-1.amazonaws.com',
+    cloudBucket: 'sicepatmasterdata',
     cloudRegion: 'ap-southeast-1',
     cloudAccessKeyId: 'AKIAJHHTEMTLK42N7TQA',
     cloudSecretAccessKey: '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',
