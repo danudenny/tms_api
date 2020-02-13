1327d14f028a9f121542b21b2e97604cba1782f8
Adry Andreas <adryhappier@gmail.com>
2020-02-13 14:10:21 +0700

    :zap: split init data login and delivery

--- src/servers/main/controllers/mobile/v1/mobile-init.controller.ts
+++ src/servers/main/controllers/mobile/v1/mobile-init.controller.ts
@@ -1,11 +1,17 @@
 import { Body, Controller, HttpCode, HttpStatus, Post, UseGuards } from '@nestjs/common';
 
-import { ResponseSerializerOptions } from '../../../../../shared/decorators/response-serializer-options.decorator';
-import { ApiBearerAuth, ApiOkResponse, ApiUseTags } from '../../../../../shared/external/nestjs-swagger';
+import {
+    ResponseSerializerOptions,
+} from '../../../../../shared/decorators/response-serializer-options.decorator';
+import {
+    ApiBearerAuth, ApiOkResponse, ApiUseTags,
+} from '../../../../../shared/external/nestjs-swagger';
 import { AuthenticatedGuard } from '../../../../../shared/guards/authenticated.guard';
 import { PermissionTokenGuard } from '../../../../../shared/guards/permission-token.guard';
 import { MobileInitDataPayloadVm } from '../../../models/mobile-init-data-payload.vm';
-import { MobileInitDataResponseVm } from '../../../models/mobile-init-data-response.vm';
+import {
+    MobileInitDataDeliveryResponseVm, MobileInitDataResponseVm,
+} from '../../../models/mobile-init-data-response.vm';
 import { V1MobileInitDataService } from '../../../services/mobile/v1/mobile-init-data.service';
 
 @ApiUseTags('Mobile Init Data')
@@ -20,7 +26,29 @@ export class V1MobileInitController {
   @ResponseSerializerOptions({ disable: true })
   @ApiOkResponse({ type: MobileInitDataResponseVm })
   public async initData(@Body() payload: MobileInitDataPayloadVm) {
-    return V1MobileInitDataService.getInitDataByRequest(payload.lastSyncDateTime);
+    return V1MobileInitDataService.getInitDataByRequest(
+      payload.lastSyncDateTime,
+    );
+  }
+
+  @Post('initDataLogin')
+  @HttpCode(HttpStatus.OK)
+  @ApiBearerAuth()
+  @UseGuards(AuthenticatedGuard, PermissionTokenGuard)
+  @ResponseSerializerOptions({ disable: true })
+  @ApiOkResponse({ type: MobileInitDataResponseVm })
+  public async initDataLogin(@Body() payload: MobileInitDataPayloadVm) {
+    return V1MobileInitDataService.getInitData(payload.lastSyncDateTime);
+  }
+
+  @Post('initDataDelivery')
+  @HttpCode(HttpStatus.OK)
+  @ApiBearerAuth()
+  @UseGuards(AuthenticatedGuard, PermissionTokenGuard)
+  @ResponseSerializerOptions({ disable: true })
+  @ApiOkResponse({ type: MobileInitDataDeliveryResponseVm })
+  public async initDataDelivery(@Body() payload: MobileInitDataPayloadVm) {
+    return V1MobileInitDataService.getInitDataDelivery(payload.lastSyncDateTime);
   }
 
   @Post('getHistory')
