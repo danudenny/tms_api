2db24fcd68141a7160208a0705b67bb564fe67a3
Candra Putra <candra.ptr19@gmail.com>
2020-03-03 10:50:51 +0700

    Mapping Role MasterData

--- src/servers/background/services/integration/masterdata.service.ts
+++ src/servers/background/services/integration/masterdata.service.ts
@@ -12,26 +12,32 @@ import { PinoLoggerService } from '../../../../shared/services/pino-logger.servi
 import { RoleTmsResponseVm } from '../../models/role-tms.response.vm';
 import { RepositoryService } from '../../../../shared/services/repository.service';
 import { BaseMetaPayloadVm } from '../../../../shared/models/base-meta-payload.vm';
+import { RoleMappingResponseVm } from '../../models/role-mapping.response.vm';
 
 import moment = require('moment');
+import { Role } from 'src/shared/orm-entity/role';
 
 @Injectable()
 export class MasterDataService {
+
+  private static get logTitle() {
+    return '[INTEGRATION MASTER DATA] ';
+  }
+
   static async mappingRole(payload: MappingRolePayloadVm): Promise<MappingRoleResponseVm> {
     const result = new MappingRoleResponseVm();
+
+    this.insertMappingRoleMasterData(payload);
 
     const users = await this.getUsers(payload.employeeRoleId);
 
     if (users.length > 0) {
       MappingRoleQueueService.addData(users, payload);
-
-      result.data =  users;
-      result.code = HttpStatus.OK;
-      result.message = 'Success';
-    } else {
-      result.code = HttpStatus.UNPROCESSABLE_ENTITY;
-      result.message = 'Employee Role Id Not Found';
     }
+
+    result.data =  users;
+    result.code = HttpStatus.OK;
+    result.message = 'Success';
 
     return result;
   }
@@ -39,13 +45,14 @@ export class MasterDataService {
   static async mappingRoleUser(payload: MappingRoleUserPayloadVm): Promise<MappingRoleResponseVm> {
     const result = new MappingRoleResponseVm();
 
-    const users = await this.getUsers(payload.employeeId, 1);
+    const users = await this.getUsers(payload.employeeId, 1, payload.branchIdLast, payload.branchIdNew);
 
     if (users.length > 0) {
       const obj = await this.getMappingRole(payload);
 
       if (Object.getOwnPropertyNames(obj).length > 0) {
         MappingRoleQueueService.addDataUser(users, obj);
+
         result.data =  users;
         result.code = HttpStatus.OK;
         result.message = 'Success';
@@ -89,13 +96,15 @@ export class MasterDataService {
     return result;
   }
 
-  private static async getUsers(id: number, mode: number = 0): Promise<any> {
+  private static async getUsers(id: number, mode: number = 0, branchIdLast: number = 0, branchIdNew: number = 0): Promise<any> {
     let where = ' e.employee_role_id = :id ';
+    let additionalSelect = ' , e.branch_id as branchIdLast, e.branch_id as branchIdNew ';
     if (mode == 1) {
+      additionalSelect = ' , ' + branchIdLast + ' as branchIdLast, ' + branchIdNew + ' as branchIdNew ';
       where = ' e.employee_id = :id ';
     }
     const query = `
-      SELECT u.user_id as userId, e.branch_id as branchId
+      SELECT u.user_id as userId ` + additionalSelect + `
       FROM employee e
       INNER JOIN users u ON u.employee_id=e.employee_id and u.username=e.nik and u.is_deleted=false
       WHERE ` + where + ` and e.branch_id is not null and e.is_deleted=false
@@ -106,17 +115,31 @@ export class MasterDataService {
     });
   }
 
-  public static async insertUserRole(userId: number, branchId: number, payload: any) {
-    const logTitle = '[INTEGRATION MASTER DATA] ';
-
+  public static async insertUserRole(userId: number, branchIdLast: number, branchIdNew: number, payload: any) {
     const arrRoleId = payload.roleIds;
+    let timeNow = moment().toDate();
 
     //#region Process For Tms
-    let timeNow = moment().toDate();
+      // Delete Previous branch_id_last
+    if (branchIdLast != branchIdNew) {
+      await UserRole.update(
+        {
+          userId,
+          branchId: branchIdLast,
+        },
+        {
+          userIdUpdated: payload.userIdUpdated,
+          updatedTime: timeNow,
+          isDeleted: true,
+        },
+      );
+    }
+
+      // Delete Previous User Role By user_id and branch_id_new
     await UserRole.update(
       {
         userId,
-        branchId,
+        branchId: branchIdNew,
       },
       {
         userIdUpdated: payload.userIdUpdated,
@@ -133,7 +156,7 @@ export class MasterDataService {
           {
             userId,
             roleId: rr.roleId,
-            branchId,
+            branchId: branchIdNew,
             createdTime: timeNow,
             updatedTime: timeNow,
             userIdCreated: userId,
@@ -153,15 +176,26 @@ export class MasterDataService {
     const pool: any = DatabaseConfig.getMasterDataDbPool();
     const client = await pool.connect();
     try {
+      timeNow = moment().toDate();
       const queryDelete = `
         UPDATE user_role
         SET user_id_updated = $1, updated_time = $2, is_deleted = true
         WHERE user_id = $3 and branch_id = $4 and is_deleted=false
       `;
-      await client.query(queryDelete, [payload.userIdUpdated, timeNow, userId, branchId], async function(err) {
-        PinoLoggerService.debug(logTitle, this.sql);
+
+      // Delete Previous branch_id_last
+      await client.query(queryDelete, [payload.userIdUpdated, timeNow, userId, branchIdLast], async function(err) {
+        PinoLoggerService.debug(this.logTitle, this.sql);
         if (err) {
-          PinoLoggerService.error(logTitle, err.message);
+          PinoLoggerService.error(this.logTitle, err.message);
+        }
+      });
+
+      // Delete Previous User Role By user_id and branch_id_new
+      await client.query(queryDelete, [payload.userIdUpdated, timeNow, userId, branchIdNew], async function(err) {
+        PinoLoggerService.debug(this.logTitle, this.sql);
+        if (err) {
+          PinoLoggerService.error(this.logTitle, err.message);
         }
       });
 
@@ -173,10 +207,10 @@ export class MasterDataService {
               user_id, role_id, branch_id, created_time, updated_time, user_id_created, user_id_updated
             ) VALUES ($1, $2, $3, $4, $5, $6, $7)
           `;
-          await client.query(queryInsert, [userId, rr.roleIdTms, branchId, timeNow, timeNow, userId, userId], async function(err) {
-            PinoLoggerService.debug(logTitle, this.sql);
+          await client.query(queryInsert, [userId, rr.roleIdTms, branchIdNew, timeNow, timeNow, userId, userId], async function(err) {
+            PinoLoggerService.debug(this.logTitle, this.sql);
             if (err) {
-              PinoLoggerService.error(logTitle, err.message);
+              PinoLoggerService.error(this.logTitle, err.message);
             }
           });
         }
@@ -225,4 +259,53 @@ export class MasterDataService {
       return temp;
     }
   }
+
+  public static async insertMappingRoleMasterData(payload: MappingRolePayloadVm) {
+    const pool: any = DatabaseConfig.getMasterDataDbPool();
+    const client = await pool.connect();
+    const timeNow = moment().toDate();
+
+    try {
+      const queryDelete = `
+        UPDATE role_mapping
+        SET user_id_updated = $1, updated_time = $2, is_deleted = true
+        WHERE employee_role_id = $3 and is_deleted=false
+      `;
+
+      // Delete Previous role_mapping by employee_role_id
+      await client.query(queryDelete, [payload.userIdUpdated, timeNow, payload.employeeRoleId], async function(err) {
+        PinoLoggerService.debug(this.logTitle, this.sql);
+        if (err) {
+          PinoLoggerService.error(this.logTitle, err.message);
+        }
+      });
+
+      for (const rr of payload.roleIds) {
+        let roleNameTms = '';
+        const role = await Role.findOne({
+          where: {
+            roleId: rr.roleIdTms,
+            isDeleted: false,
+          },
+        });
+        if (role) {
+          roleNameTms = role.roleName;
+        }
+        const query = `
+          INSERT INTO role_mapping (
+            employee_role_id, role_id, role_id_tms, role_name_tms, created_time, updated_time, user_id_created, user_id_updated
+          ) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
+        `;
+        await client.query(query, [payload.employeeRoleId, rr.roleId, rr.roleIdTms, roleNameTms, timeNow, timeNow, payload.userIdUpdated, payload.userIdUpdated], async function(err) {
+          PinoLoggerService.debug(this.logTitle, this.sql);
+          if (err) {
+            PinoLoggerService.error(this.logTitle, err.message);
+          }
+        });
+      }
+    } finally {
+      client.release();
+    }
+  }
+
 }
