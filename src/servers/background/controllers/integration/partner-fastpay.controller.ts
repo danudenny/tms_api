294e1b82fb1b365b0ea4d4b4d98b726d2c3616b1
Adry Andreas <adryhappier@gmail.com>
2020-03-17 11:27:51 +0700

    :zap: add auth token for fastpay

--- src/servers/background/controllers/integration/partner-fastpay.controller.ts
+++ src/servers/background/controllers/integration/partner-fastpay.controller.ts
@@ -1,26 +1,22 @@
-import { Body, Controller, Post, Req, HttpCode, HttpStatus } from '@nestjs/common';
-import { PartnerService } from '../../services/integration/partner.service';
-import { Partner } from '../../../../shared/orm-entity/partner';
-import { Transactional } from '../../../../shared/external/typeorm-transactional-cls-hooked/Transactional';
-import { PosindonesiaPayloadVm } from '../../models/posindonesia.payload.vm';
-import { ApiUseTags } from '../../../../shared/external/nestjs-swagger';
+import { Body, Controller, Post, HttpCode, HttpStatus, UseGuards } from '@nestjs/common';
+import { ApiUseTags, ApiBearerAuth } from '../../../../shared/external/nestjs-swagger';
 import { PartnerFastpayService } from '../../services/integration/partner-fastpay.service';
 import { DropCashlessVm } from '../../models/partner/fastpay-drop.vm';
 import { ResponseSerializerOptions } from '../../../../shared/decorators/response-serializer-options.decorator';
+import { AuthFastpayGuard } from '../../../../shared/guards/auth-fastpay.guard';
 
 @ApiUseTags('Partner Integration Sicepat x Fastpay')
 @Controller('integration/partner')
+@ApiBearerAuth()
 export class PartnerFastpayController {
   constructor() {}
 
   @Post('fastpay/drop/cashless')
   @HttpCode(HttpStatus.OK)
-  // @ApiBearerAuth()
-  // @UseGuards(AuthenticatedGuard)
+  // @UseGuards(AuthFastpayGuard)
   // @ApiOkResponse({ type: TrackingNoteResponseVm })
   @ResponseSerializerOptions({ disable: true })
   public async dropCashless(@Body() payload: DropCashlessVm) {
     return PartnerFastpayService.dropCashless(payload);
   }
-
 }
