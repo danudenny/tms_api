e9c8809700c352d8abe48d599e392905684b048d
Adry Andreas <adryhappier@gmail.com>
2020-03-30 12:42:23 +0700

    :rocket: add endpoint app notification

--- src/servers/main/controllers/mobile/v1/mobile-init.controller.ts
+++ src/servers/main/controllers/mobile/v1/mobile-init.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, HttpCode, HttpStatus, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post, UseGuards, Get, Param, BadRequestException } from '@nestjs/common';
 
 import {
     ResponseSerializerOptions,
@@ -13,6 +13,10 @@ import {
     MobileInitDataDeliveryResponseVm, MobileInitDataResponseVm,
 } from '../../../models/mobile-init-data-response.vm';
 import { V1MobileInitDataService } from '../../../services/mobile/v1/mobile-init-data.service';
+import { RedisService } from '../../../../../shared/services/redis.service';
+import moment = require('moment');
+import { AppNotification } from '../../../../../shared/orm-entity/app-notification';
+import { PinoLoggerService } from '../../../../../shared/services/pino-logger.service';
 
 @ApiUseTags('Mobile Init Data')
 @Controller('mobile/v1')
@@ -48,7 +52,9 @@ export class V1MobileInitController {
   @ResponseSerializerOptions({ disable: true })
   @ApiOkResponse({ type: MobileInitDataDeliveryResponseVm })
   public async initDataDelivery(@Body() payload: MobileInitDataPayloadVm) {
-    return V1MobileInitDataService.getInitDataDelivery(payload.lastSyncDateTime);
+    return V1MobileInitDataService.getInitDataDelivery(
+      payload.lastSyncDateTime,
+    );
   }
 
   @Post('getHistory')
@@ -61,6 +67,41 @@ export class V1MobileInitController {
     return V1MobileInitDataService.getHistoryByRequest(
       payload.doPodDeliverDetailId,
     );
-    return null;
+  }
+
+  @Get('getAppNotification/:appCode')
+  @ResponseSerializerOptions({ disable: true })
+  public async getAppNotification(@Param('appCode') appCode: string) {
+    let message = '';
+    // TODO: check redis app notifiations
+    const dataRedis = await RedisService.get(
+      `appNotification:mobile:${appCode}`,
+    );
+
+    if (dataRedis) {
+      PinoLoggerService.log('Load Data from redis!');
+      message = dataRedis;
+    } else {
+      // get data app notification
+      const appMsg = await AppNotification.findOne({ appCode, isActive: true });
+      if (appMsg) {
+        // set data on redis
+        const expireOnSeconds = 60 * 60 * 3;
+        await RedisService.setex(
+          `appNotification:mobile:${appCode}`,
+          appMsg.message,
+          expireOnSeconds,
+        );
+        message = appMsg.message;
+      } else {
+        throw new BadRequestException('Data Tidak ditemukan !');
+      }
+    }
+
+    return {
+      appCode,
+      message,
+      timeString: moment().format('YYYY-MM-DD HH:mm:ss'),
+    };
   }
 }
