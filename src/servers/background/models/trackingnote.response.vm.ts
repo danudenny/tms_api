4d99482771bd8026a2a047203204d24059243ce4
Candra Putra <candra.ptr19@gmail.com>
2020-04-17 14:25:14 +0700

    Optimize Pool Management & Recursive Async Loop For TmsTrackingNote
    # Conflicts:
    #	src/servers/background/services/integration/trackingnote.service.ts

--- src/servers/background/models/trackingnote.response.vm.ts
+++ src/servers/background/models/trackingnote.response.vm.ts
@@ -1,8 +1,7 @@
 import { ApiModelProperty } from '../../../shared/external/nestjs-swagger';
-import { BaseMetaResponseVm } from '../../../shared/models/base-meta-response.vm';
 import { TrackingNoteVm } from './trackingnote.vm';
 
-export class TrackingNoteResponseVm extends BaseMetaResponseVm {
+export class TrackingNoteResponseVm {
   @ApiModelProperty({ type: () => [TrackingNoteVm] })
   data: TrackingNoteVm[];
 }
--- src/servers/background/services/integration/trackingnote.service.ts
+++ src/servers/background/services/integration/trackingnote.service.ts
@@ -1,45 +1,24 @@
-import { HttpStatus } from '@nestjs/common';
-
 import moment = require('moment');
 import { BaseMetaPayloadVm } from '../../../../shared/models/base-meta-payload.vm';
 import { TrackingNoteResponseVm } from '../../models/trackingnote.response.vm';
 import { RawQueryService } from '../../../../shared/services/raw-query.service';
 import { DatabaseConfig } from '../../config/database/db.config';
 import * as sql from 'mssql';
-import { RequestErrorService } from '../../../../shared/services/request-error.service';
 import { AwbHistoryLastSyncPod } from '../../../../shared/orm-entity/awb-history-last-sync-pod';
+import { PinoLoggerService } from '../../../../shared/services/pino-logger.service';
 
 export class TrackingNoteService {
+
+  private static get logTitle() {
+    return '[INTEGRATION TRACKING NOTE] ';
+  }
+
   static async findLastAwbHistory(
     payload: BaseMetaPayloadVm,
   ): Promise<TrackingNoteResponseVm> {
     const result = new TrackingNoteResponseVm();
-    const awbHistoryLastSyncPod = await AwbHistoryLastSyncPod.findOne({
-      where: {
-        isDeleted: false,
-      },
-    });
-    if (awbHistoryLastSyncPod) {
-      const data = await this.getRawAwbHistory(awbHistoryLastSyncPod.awbHistoryId);
-      if (data) {
-        this.insertTmsTrackingNote(awbHistoryLastSyncPod, data);
-        result.data = data;
-      } else {
-        RequestErrorService.throwObj(
-          {
-            message: 'Awb History Not Found (' + awbHistoryLastSyncPod.awbHistoryId + ')',
-          },
-          HttpStatus.UNPROCESSABLE_ENTITY,
-        );
-      }
-    } else {
-      RequestErrorService.throwObj(
-        {
-          message: 'Last Awb History Empty',
-        },
-        HttpStatus.UNPROCESSABLE_ENTITY,
-      );
-    }
+
+    result.data = await this.insertTmsTrackingNote(5);
 
     return result;
   }
@@ -75,146 +54,151 @@ export class TrackingNoteService {
     });
   }
 
-  private static async getRawLastAwbHistory(): Promise<any> {
-    const query = `
-      SELECT *
-      FROM awb_history_last_sync
-      LIMIT 1;
-    `;
-    return await RawQueryService.query(query);
-  }
+  private static async insertTmsTrackingNote(counter: number = 0): Promise<any> {
+    if (counter > 0) {
+      const awbHistoryLastSyncPod = await AwbHistoryLastSyncPod.findOne({
+        where: {
+          isDeleted: false,
+        },
+      });
 
-  private static async insertTmsTrackingNote(awbHistoryLastSyncPod: any, data: any): Promise<any> {
-    // Connect to POD
-    const conn = await DatabaseConfig.getPodDbConn();
-    const transaction = new sql.Transaction(conn);
+      if (awbHistoryLastSyncPod) {
+        const data = await this.getRawAwbHistory(awbHistoryLastSyncPod.awbHistoryId);
+        if (data) {
+          // Connect to POD
+          const conn = await DatabaseConfig.getPodDbConn();
+          const transaction = new sql.Transaction(conn);
 
-    transaction.begin( (err: any) => {
-        // ... error checks
-      let lastSyncId = 0;
-      let ctr = 0;
-        // Bulk Insert Prepare Table
-        // const table = new sql.Table('TmsTrackingNote');
-        // table.columns.add('AwbHistoryId', sql.Int, {nullable: false});
-        // table.columns.add('ReceiptNumber', sql.VarChar, {nullable: false});
-        // table.columns.add('TrackingDateTime', sql.DateTime, {nullable: false});
-        // table.columns.add('AwbStatusId', sql.Int, {nullable: false});
-        // table.columns.add('TrackingType', sql.VarChar, {nullable: true});
-        // table.columns.add('CourierName', sql.VarChar, {nullable: true});
-        // table.columns.add('Nik', sql.VarChar, {nullable: true});
-        // table.columns.add('BranchCode', sql.VarChar, {nullable: true});
-        // table.columns.add('NoteInternal', sql.VarChar, {nullable: true});
-        // table.columns.add('NotePublic', sql.VarChar, {nullable: true});
-        // table.columns.add('NoteTms', sql.VarChar, {nullable: true});
-        // table.columns.add('UsrCrt', sql.VarChar, {nullable: true});
-        // table.columns.add('UsrUpd', sql.VarChar, {nullable: true});
-        // table.columns.add('DtmCrt', sql.DateTime, {nullable: false});
-        // table.columns.add('DtmUpd', sql.DateTime, {nullable: false});
-        // table.columns.add('ReceiverName', sql.VarChar, {nullable: true});
-        //  table.columns.add('IsPublic', sql.Boolean, {nullable: true});
+          transaction.begin( (err: any) => {
+            let lastSyncId = 0;
+            let ctr = 0;
 
-      for (const item of data) {
-        lastSyncId = item.awbHistoryId;
-        const timeNow = new Date(moment().add(7, 'hours').format('YYYY-MM-DD HH:mm:ss'));
-        const trackDateTime = new Date(moment(item.trackingDateTime).add(7, 'hours').format('YYYY-MM-DD HH:mm:ss'));
+            for (const item of data) {
+              lastSyncId = item.awbHistoryId;
+              const timeNow = new Date(moment().add(7, 'hours').format('YYYY-MM-DD HH:mm:ss'));
+              const trackDateTime = new Date(moment(item.trackingDateTime).add(7, 'hours').format('YYYY-MM-DD HH:mm:ss'));
 
-        // table.rows.add(lastSyncId, item.receiptNumber, item.trackingDateTime, item.awbStatusId, item.trackingType,
-        //   item.courierName, item.nik, item.branchCode,
-        //   // item.noteInternal, // item.notePublic, item.noteTms,
-        //   'TMS', 'TMS', new Date(), new Date());
-        // break;
-        const request = conn.request();
-        request.input('AwbHistoryId', sql.Int, item.awbHistoryId);
-        request.input('ReceiptNumber', sql.VarChar, item.receiptNumber);
-        request.input('TrackingDateTime', sql.DateTime, trackDateTime);
-        request.input('AwbStatusId', sql.Int, item.awbStatusId);
-        request.input('TrackingType', sql.VarChar, item.trackingType);
-        request.input('CourierName', sql.VarChar, item.courierName);
-        request.input('Nik', sql.VarChar, item.nik);
-        request.input('BranchCode', sql.VarChar, item.branchCode);
-        request.input('NoteInternal', sql.VarChar, item.noteInternal);
-        request.input('NotePublic', sql.VarChar, item.notePublic);
-        request.input('NoteTms', sql.VarChar, item.noteTms);
-        request.input('UsrCrt', sql.VarChar, 'TMS');
-        request.input('UsrUpd', sql.VarChar, 'TMS');
-        request.input('DtmCrt', sql.DateTime, timeNow);
-        request.input('DtmUpd', sql.DateTime, timeNow);
-        request.input('ReceiverName', sql.VarChar, item.receiverName);
-        let isPublic = 0;
-        if (item.isPublic === 20) {
-          isPublic = 1;
-        }
-        request.input('IsPublic', sql.Bit, isPublic);
-
-        request.query(`
-          insert into TmsTrackingNote (
-            AwbHistoryId, ReceiptNumber, TrackingDateTime, AwbStatusId, TrackingType, CourierName, Nik, BranchCode, NoteInternal, NotePublic, NoteTms, UsrCrt, UsrUpd, DtmCrt, DtmUpd, ReceiverName, IsPublic
-            )
-          values (
-            @AwbHistoryId, @ReceiptNumber, @TrackingDateTime, @AwbStatusId, @TrackingType, @CourierName, @Nik, @BranchCode, @NoteInternal, @NotePublic, @NoteTms, @UsrCrt, @UsrUpd, @DtmCrt, @DtmUpd, @ReceiverName, @IsPublic
-          )`, (err, result) => {
-            if (!err) {
-              ctr++;
-              if (ctr == data.length) {
-                AwbHistoryLastSyncPod.update(awbHistoryLastSyncPod.awbHistoryLastSyncPodId, {
-                  awbHistoryId: lastSyncId,
-                  updatedTime: new Date(),
-                });
-                transaction.commit(err => {
-                  if (err) {
-                    console.log('[ERROR TRANSACTION] :: ' + err);
-                    transaction.rollback(err => {
-                      if (err) {
-                        console.log('[ERROR ROLLBACK TRANSACTION] :: ' + err);
-                      } else {
-                        console.log('[ROLLBACKED TRANSACTION]===================');
-                      }
-                    });
-                  } else {
-                    console.log('[COMMITTED]===================');
-                  }
-                });
-                console.log('[ALL SUCCESS] Last awb history id === ' + lastSyncId);
+              const request = conn.request();
+              request.input('AwbHistoryId', sql.Int, item.awbHistoryId);
+              request.input('ReceiptNumber', sql.VarChar, item.receiptNumber);
+              request.input('TrackingDateTime', sql.DateTime, trackDateTime);
+              request.input('AwbStatusId', sql.Int, item.awbStatusId);
+              request.input('TrackingType', sql.VarChar, item.trackingType);
+              request.input('CourierName', sql.VarChar, item.courierName);
+              request.input('Nik', sql.VarChar, item.nik);
+              request.input('BranchCode', sql.VarChar, item.branchCode);
+              request.input('NoteInternal', sql.VarChar, item.noteInternal);
+              request.input('NotePublic', sql.VarChar, item.notePublic);
+              request.input('NoteTms', sql.VarChar, item.noteTms);
+              request.input('UsrCrt', sql.VarChar, 'TMS');
+              request.input('UsrUpd', sql.VarChar, 'TMS');
+              request.input('DtmCrt', sql.DateTime, timeNow);
+              request.input('DtmUpd', sql.DateTime, timeNow);
+              request.input('ReceiverName', sql.VarChar, item.receiverName);
+              let isPublic = 0;
+              if (item.isPublic === 20) {
+                isPublic = 1;
               }
-            } else {
-              if (lastSyncId != 0) {
-                transaction.rollback(err => {
-                  if (err) {
-                    console.log('[ERROR ROLLBACK] :: ' + err);
+              request.input('IsPublic', sql.Bit, isPublic);
+
+              request.query(`
+                insert into TmsTrackingNote (
+                  AwbHistoryId, ReceiptNumber, TrackingDateTime, AwbStatusId, TrackingType, CourierName, Nik, BranchCode, NoteInternal, NotePublic, NoteTms, UsrCrt, UsrUpd, DtmCrt, DtmUpd, ReceiverName, IsPublic
+                  )
+                values (
+                  @AwbHistoryId, @ReceiptNumber, @TrackingDateTime, @AwbStatusId, @TrackingType, @CourierName, @Nik, @BranchCode, @NoteInternal, @NotePublic, @NoteTms, @UsrCrt, @UsrUpd, @DtmCrt, @DtmUpd, @ReceiverName, @IsPublic
+                )`, (err, result) => {
+                  if (!err) {
+                    ctr++;
+                    if (ctr == data.length) {
+                      AwbHistoryLastSyncPod.update(awbHistoryLastSyncPod.awbHistoryLastSyncPodId, {
+                        awbHistoryId: lastSyncId,
+                        updatedTime: new Date(),
+                      });
+                      transaction.commit(err => {
+                        if (err) {
+                          PinoLoggerService.debug(this.logTitle, '[ERROR TRANSACTION] :: ' + err);
+                          transaction.rollback(err => {
+                            if (err) {
+                              PinoLoggerService.debug(this.logTitle, '[ERROR ROLLBACK TRANSACTION] :: ' + err);
+                            } else {
+                              PinoLoggerService.debug(this.logTitle, '[ROLLBACKED TRANSACTION]===================');
+                            }
+                          });
+                        } else {
+                          PinoLoggerService.debug(this.logTitle, '[COMMITTED]===================');
+                          this.insertTmsTrackingNote(counter - 1);
+                        }
+                      });
+                      PinoLoggerService.debug(this.logTitle, '[ALL SUCCESS] Last awb history id === ' + lastSyncId);
+                    }
                   } else {
-                    console.log('[ROLLBACKED]===================');
+                    if (lastSyncId != 0) {
+                      transaction.rollback(err => {
+                        if (err) {
+                          PinoLoggerService.debug(this.logTitle, '[ERROR ROLLBACK] :: ' + err);
+                        } else {
+                          PinoLoggerService.debug(this.logTitle, '[ROLLBACKED]===================');
+                        }
+                      });
+                      PinoLoggerService.debug(this.logTitle, '[ERROR STOP] Last awb history id === ' + lastSyncId);
+                    }
                   }
-                });
-                console.log('[ERROR STOP] Last awb history id === ' + lastSyncId);
-              }
-              RequestErrorService.throwObj({
-                message: err,
-              },
-                HttpStatus.UNPROCESSABLE_ENTITY,
+                },
               );
             }
-          },
-        );
-      }
+          });
+        } else {
+          PinoLoggerService.debug(this.logTitle, 'Awb History Empty (' + awbHistoryLastSyncPod.awbHistoryId + ')');
+        }
 
-        // const request = conn.request();
-        // request.bulk(table, (err, result) => {
-        //   // ... error checks
-        //   if (!err) {
-        //     // AwbHistoryLastSyncPod.update(awbHistoryLastSyncPod.awbHistoryLastSyncPodId, {
-        //     //   awbHistoryId: lastSyncId,
-        //     //   updatedTime: new Date(),
-        //     // });
-        //     // console.log('[ALL SUCCESS] Last awb history id === ' + lastSyncId);
-        //   } else {
-        //     console.log('[ERROR STOP] Tracking Note Service === ' + err);
-        //     RequestErrorService.throwObj({
-        //       message: err,
-        //     },
-        //       HttpStatus.UNPROCESSABLE_ENTITY,
-        //     );
-        //   }
-        // });
-    });
+        return data;
+      } else {
+        PinoLoggerService.debug(this.logTitle, 'Last Awb History Empty');
+      }
+    }
   }
 }
+
+// table.rows.add(lastSyncId, item.receiptNumber, item.trackingDateTime, item.awbStatusId, item.trackingType,
+//   item.courierName, item.nik, item.branchCode,
+//   // item.noteInternal, // item.notePublic, item.noteTms,
+//   'TMS', 'TMS', new Date(), new Date());
+// break;
+// Bulk Insert Prepare Table
+// const table = new sql.Table('TmsTrackingNote');
+// table.columns.add('AwbHistoryId', sql.Int, {nullable: false});
+// table.columns.add('ReceiptNumber', sql.VarChar, {nullable: false});
+// table.columns.add('TrackingDateTime', sql.DateTime, {nullable: false});
+// table.columns.add('AwbStatusId', sql.Int, {nullable: false});
+// table.columns.add('TrackingType', sql.VarChar, {nullable: true});
+// table.columns.add('CourierName', sql.VarChar, {nullable: true});
+// table.columns.add('Nik', sql.VarChar, {nullable: true});
+// table.columns.add('BranchCode', sql.VarChar, {nullable: true});
+// table.columns.add('NoteInternal', sql.VarChar, {nullable: true});
+// table.columns.add('NotePublic', sql.VarChar, {nullable: true});
+// table.columns.add('NoteTms', sql.VarChar, {nullable: true});
+// table.columns.add('UsrCrt', sql.VarChar, {nullable: true});
+// table.columns.add('UsrUpd', sql.VarChar, {nullable: true});
+// table.columns.add('DtmCrt', sql.DateTime, {nullable: false});
+// table.columns.add('DtmUpd', sql.DateTime, {nullable: false});
+// table.columns.add('ReceiverName', sql.VarChar, {nullable: true});
+//  table.columns.add('IsPublic', sql.Boolean, {nullable: true});
+// const request = conn.request();
+// request.bulk(table, (err, result) => {
+//   // ... error checks
+//   if (!err) {
+//     // AwbHistoryLastSyncPod.update(awbHistoryLastSyncPod.awbHistoryLastSyncPodId, {
+//     //   awbHistoryId: lastSyncId,
+//     //   updatedTime: new Date(),
+//     // });
+//     // PinoLoggerService.debug(this.logTitle,'[ALL SUCCESS] Last awb history id === ' + lastSyncId);
+//   } else {
+//     PinoLoggerService.debug(this.logTitle,'[ERROR STOP] Tracking Note Service === ' + err);
+//     RequestErrorService.throwObj({
+//       message: err,
+//     },
+//       HttpStatus.UNPROCESSABLE_ENTITY,
+//     );
+//   }
+// });
