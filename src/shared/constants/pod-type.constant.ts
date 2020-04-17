1e87fa6eb539de5cb21eb10e41eae271391531f2
Adry Andreas <adryhappier@gmail.com>
2020-04-17 11:33:19 +0700

    :rocket: take out validate image on create do pod hub

--- src/shared/constants/pod-type.constant.ts
+++ src/shared/constants/pod-type.constant.ts
@@ -1,12 +1,7 @@
-// NOTE:
-// 3010 = OUT_HUB (Hub)
-// 3005 = OUT_BRANCH (Transit)
-// TODO: need confirm
 export const POD_TYPE = {
-  TRANSIT_INTERNAL: 3005, // old Type
   TRANSIT_HUB: 3010, // old Type
   OUT_BRANCH: 3005,
-  OUT_BRANCH_AWB: 3015,
   OUT_HUB: 3010,
-  HUB_TRANSIT: 3020,
+  OUT_BRANCH_AWB: 3015,
+  OUT_HUB_TRANSIT: 3020,
 };
