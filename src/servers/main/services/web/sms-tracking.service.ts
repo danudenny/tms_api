89d97a0d8f805116d745293d4cd8f6c378120ad9
niudanrim <mohammad.niudanri@e.sicepat.com>
2020-05-06 22:44:16 +0700

    update endpoint generate sms tracking

--- src/servers/main/services/web/sms-tracking.service.ts
+++ src/servers/main/services/web/sms-tracking.service.ts
@@ -17,16 +17,17 @@ import {
 import moment = require('moment');
 import { async } from 'rxjs/internal/scheduler/async';
 import { SmsTrackingShift } from '../../../../shared/orm-entity/sms-tracking-shift';
-import {AuthService} from '../../../../shared/services/auth.service';
-import {SmsTrackingMessage} from '../../../../shared/orm-entity/sms-tracking-message';
-import {OrionRepositoryService} from '../../../../shared/services/orion-repository.service';
-import {SmsTrackingUser} from '../../../../shared/orm-entity/sms-tracking-user';
+import { AuthService } from '../../../../shared/services/auth.service';
+import { SmsTrackingMessage } from '../../../../shared/orm-entity/sms-tracking-message';
+import { OrionRepositoryService } from '../../../../shared/services/orion-repository.service';
+import { SmsTrackingUser } from '../../../../shared/orm-entity/sms-tracking-user';
 import { In, createQueryBuilder } from 'typeorm';
 import fs = require('fs');
 import xlsx = require('xlsx');
 import { ExportToCsv } from 'export-to-csv';
 import express = require('express');
-import {format} from 'path';
+import { format } from 'path';
+import { isEmpty } from 'lodash';
 
 export class SmsTrackingService {
   static async storeMessage(
@@ -35,16 +36,16 @@ export class SmsTrackingService {
     const result = new SmsTrackingStoreMessageResponseVm();
     const authMeta = AuthService.getAuthData();
     const smsTrackingMessage = SmsTrackingMessage.create({
-        sentTo: payload.sentTo,
-        isRepeated: payload.isRepeated,
-        isRepeatedOver: payload.isRepeatedOver,
-        note: payload.note,
-        awbStatusId: payload.awbStatusId,
-        userIdCreated: authMeta.userId,
-        createdTime: moment().toDate(),
-        updatedTime: moment().toDate(),
-        userIdUpdated: authMeta.userId,
-      });
+      sentTo: payload.sentTo,
+      isRepeated: payload.isRepeated,
+      isRepeatedOver: payload.isRepeatedOver,
+      note: payload.note,
+      awbStatusId: payload.awbStatusId,
+      userIdCreated: authMeta.userId,
+      createdTime: moment().toDate(),
+      updatedTime: moment().toDate(),
+      userIdUpdated: authMeta.userId,
+    });
     const response = await SmsTrackingMessage.save(smsTrackingMessage);
     result.smsTrackingMessageId = response.smsTrackingMessageId;
     result.message = 'Berhasil Menyimpan sms tracking - message';
@@ -113,11 +114,11 @@ export class SmsTrackingService {
     const result = new SmsTrackingStoreShiftResponseVm();
 
     if (!moment(payload.workFrom, 'HH:mm', true).isValid() ||
-        !moment(payload.workTo, 'HH:mm', true).isValid()) {
-        result.smsTrackingShiftId = null;
-        result.message = `Salah format payload workFrom dan workTo`;
-        result.status = 'error';
-        return result;
+      !moment(payload.workTo, 'HH:mm', true).isValid()) {
+      result.smsTrackingShiftId = null;
+      result.message = `Salah format payload workFrom dan workTo`;
+      result.status = 'error';
+      return result;
     }
 
     const authMeta = AuthService.getAuthData();
@@ -221,9 +222,9 @@ export class SmsTrackingService {
         isDeleted: true,
       });
       const result = {
-                       status: 'success',
-                       message: 'Berhasil Menghapus Data',
-                     };
+        status: 'success',
+        message: 'Berhasil Menghapus Data',
+      };
       db.raw = result;
       return db.raw;
     } catch (error) {
@@ -233,167 +234,221 @@ export class SmsTrackingService {
 
   public static async export(
     res: express.Response,
-    payload: GenerateReportSmsTrackingPayloadVm,
-    ) {
-      // query get all sms tracking message
-      let qb = createQueryBuilder();
-      qb.addSelect('stm.is_repeated_over', 'isRepeatedOver');
-      qb.addSelect('stm.is_repeated', 'isRepeated');
-      qb.addSelect('stm.awb_status_id', 'awbStatusId');
-      qb.from('sms_tracking_message', 'stm');
-      qb.andWhere('stm.is_deleted = false');
-      const smsTrackingMessage = await qb.getRawMany();
+    date: string,
+    id: number,
+  ) {
+    // query get all sms tracking message
+    let qb = createQueryBuilder();
+    qb.addSelect('stm.is_repeated_over', 'isRepeatedOver');
+    qb.addSelect('stm.is_repeated', 'isRepeated');
+    qb.addSelect('stm.awb_status_id', 'awbStatusId');
+    qb.from('sms_tracking_message', 'stm');
+    qb.andWhere('stm.is_deleted = false');
+    const smsTrackingMessage = await qb.getRawMany();
 
-      // query get sms tracking shift on id
-      qb = createQueryBuilder();
-      qb.addSelect('sts.is_repeated', 'isRepeated');
-      qb.addSelect('sts.awb_status_id', 'awbStatusId');
-      qb.from('sms_tracking_shift', 'sts');
-      qb.andWhere('sts.is_deleted = false');
-      qb.andWhere(`sts.sms_tracking_shift_id = '${payload.smsTrackingShiftId}'`);
-      const smsTrackingShift = await qb.getRawOne();
+    // query get sms tracking shift on id
+    qb = createQueryBuilder();
+    qb.addSelect('sts.work_from', 'workFrom');
+    qb.addSelect('sts.work_to', 'workTo');
+    qb.from('sms_tracking_shift', 'sts');
+    qb.andWhere('sts.is_deleted = false');
+    qb.andWhere(`sts.sms_tracking_shift_id = '${id}'`);
+    const smsTrackingShift = await qb.getRawOne();
 
-      const date7DayBefore = moment(payload.date, 'YYYY-MM-DD').subtract(7, 'd')
-                              .format('YYYY-MM-DD');
-      const workFromDT = moment(smsTrackingShift.workFrom, 'hh:mm A');
-      const workToDT = moment(smsTrackingShift.workTo, 'hh:mm A');
+    const date7DayBefore = moment(date, 'YYYY-MM-DD').subtract(7, 'd')
+      .format('YYYY-MM-DD');
+    const workFromDT = moment(smsTrackingShift.workFrom, 'hh:mm A');
+    const workToDT = moment(smsTrackingShift.workTo, 'hh:mm A');
 
-      // query get awb
-      qb = createQueryBuilder();
-      qb.addSelect('awb.awbNumber', 'waybill'); // waybill
-      qb.addSelect(`
-        CASE
-          WHEN prd.recipient_phone ~ '^([/08|/+\62][0-9]{10,13})$' THEN 'valid'
-          ELSE 'invalid'
-        END as "status_phone_recipient"
-      `);
-      qb.addSelect(`
-        CASE
-          WHEN prd.shipper_phone ~ '^([/08|/+\62][0-9]{10,13})$' THEN 'valid'
-          ELSE 'invalid'
-        END as "status_phone_sender"
-      `);
-      qb.from('awb', 'awb');
-      qb.innerJoin(
-        'awb_item_attr',
-        'aia',
-        'aia.awb_id = awb.awb_id AND aia.is_deleted = false',
-      );
-      qb.innerJoin(
-        'pickup_request_detail',
-        'prd',
-        'prd.ref_awb_number = prd.ref_awb_number AND prd.is_deleted = false',
-      );
-      qb.innerJoin(
-        'awb_status',
-        'as',
-        'aia.awb_status_id_last = as.awb_status_id AND as.is_deleted = false',
-      );
-      qb.innerJoin(
-        'sms_tracking_message',
-        'stm',
-        'stm.awb_status_id = aia.awb_status_id_last AND stm.is_deleted = false',
-        );
-      qb.innerJoin(
-        'sms_tracking_user',
-        'stu',
-        'stu.sms_tracking_user_id = stm.sent_to AND stu.is_deleted = false',
-      );
-      // const dateReq1 = moment(payload.date, 'YYYY-MM-DD').format('YYYY-MM-DD 00:00:00');
-      // const dateReq2 = moment(payload.date, 'YYYY-MM-DD').format('YYYY-MM-DD 23:59:59');
+    // query get data for excel
+    qb = createQueryBuilder();
+    qb.addSelect('awb.awb_number', 'waybill'); // waybill
 
-      // query filter by shift awb status
-      smsTrackingMessage.forEach(data => {
-        if (data.isRepeatedOver && data.isRepeated) { // filter 7 hari kebelakang
-          qb.andWhere(`(
-            awb.awb_status_id = '${data.awbStatusId}' AND
-            ( awb.updated_time BETWEEN '${date7DayBefore}' AND '${payload.date}' )
+    qb.addSelect(`(
+      REPLACE(stm.note, '[waybill]', awb.awb_number)
+    )`, 'note');
+    qb.addSelect('abs.awb_status_name', 'awbStatusName');
+    qb.addSelect('prd.recipient_phone', 'recipientPhone');
+    qb.addSelect('prd.recipient_name', 'recipientName');
+    qb.addSelect('prd.shipper_phone', 'senderPhone');
+    qb.addSelect('prd.shipper_name', 'senderName');
+    qb.addSelect('stu.sms_tracking_user_name', 'sentTo');
+    qb.addSelect('abs.awb_status_name', 'awbStatusName');
+
+    qb.addSelect(`
+      CASE
+        WHEN (stu.sms_tracking_user_name = 'Recipient' AND prd.recipient_phone ~ '^([/08|/+62][0-9]{10,13})$') THEN 'valid'
+        WHEN (stu.sms_tracking_user_name = 'Sender' AND prd.shipper_phone ~ '^([/08|/+62][0-9]{10,13})$') THEN 'valid'
+        ELSE 'invalid'
+      END
+    `, 'statusPhone');
+    qb.from('sms_tracking_message', 'stm');
+    qb.innerJoin(
+      'sms_tracking_user',
+      'stu',
+      'stu.sms_tracking_user_id = stm.sent_to AND stu.is_deleted = false',
+    );
+    qb.innerJoin(
+      'awb_item_attr',
+      'aia',
+      'aia.awb_status_id_last = stm.awb_status_id AND aia.is_deleted = false',
+    );
+    qb.innerJoin(
+      'awb_status',
+      'abs',
+      'aia.awb_status_id_last = abs.awb_status_id AND abs.is_deleted = false',
+    );
+    qb.innerJoin(
+      'awb',
+      'awb',
+      'awb.awb_id = aia.awb_id AND awb.is_deleted = false',
+    );
+    qb.innerJoin(
+      'pickup_request_detail',
+      'prd',
+      'prd.ref_awb_number = awb.ref_awb_number AND prd.is_deleted = false',
+    );
+
+    // query filter by shift awb status
+    smsTrackingMessage.forEach(data => {
+      if (data.isRepeatedOver && data.isRepeated) { // filter 7 hari kebelakang
+        qb.orWhere(`(
+          aia.awb_status_id_last = '${data.awbStatusId}' AND
+          aia.updated_time >= '${date7DayBefore} 00:00:00' AND
+          aia.updated_time <= '${date} 23:59:59'
+        )`);
+      } else if (data.isRepeatedOver && !data.isRepeated) { // filter dalam lingkup jam shifting dan 7 hari kebelakang
+        if (workToDT.isAfter(workFromDT)) {
+          qb.orWhere(`(
+            aia.awb_status_id_last = '${data.awbStatusId}' AND
+            aia.updated_time >= '${date7DayBefore} 00:00:00' AND
+            aia.updated_time <= '${date} 23:59:59' AND
+            CAST(aia.updated_time AS TIME) >= '${smsTrackingShift.workFrom}' AND
+            CAST(aia.updated_time AS TIME) <= '${smsTrackingShift.workTo}'
           )`);
-        } else if (data.isRepeatedOver && !data.isRepeated) { // filter dalam lingkup jam shifting dan 7 hari kebelakang
-          if (workToDT.isAfter(workFromDT)) {
-            qb.andWhere(`(
-              awb.awb_status_id = '${data.awbStatusId}' AND
-              ( awb.updated_time BETWEEN '${date7DayBefore}' AND '${payload.date}' ) AND
-              CAST(awb.updated_time AS TIME) >= '${smsTrackingShift.workFrom}' AND
-              CAST(awb.updated_time AS TIME) <= '${smsTrackingShift.workTo}'
-            )`);
-          } else {
-            qb.andWhere(`(
-              awb.awb_status_id = '${data.awbStatusId}' AND
-              ( awb.updated_time BETWEEN '${date7DayBefore}' AND '${payload.date}' ) AND
-              (
-                CAST(awb.updated_time AS TIME) <= '${smsTrackingShift.workFrom}' OR
-                CAST(awb.updated_time AS TIME) => '${smsTrackingShift.workTo}'
-              )
-            )`);
-          }
-        } else if (!data.isRepeatedOver && data.isRepeated) { // filter diluar lingkup jam shifting dan 7 hari kebelakang
-          if (workToDT.isAfter(workFromDT)) {
-            qb.andWhere(`(
-              awb.awb_status_id = '${data.awbStatusId}' AND
-              ( awb.updated_time BETWEEN '${date7DayBefore}' AND '${payload.date}' ) AND
-              (
-                CAST(awb.updated_time AS TIME) < '${smsTrackingShift.workFrom}' OR
-                CAST(awb.updated_time AS TIME) > '${smsTrackingShift.workTo}'
-              )
-            )`);
-          } else { // filter hanya dalam lingkup jam shifting pada tanggal request
-            if (workToDT.isAfter(workFromDT)) {
-              qb.andWhere(`(
-                awb.awb_status_id = '${data.awbStatusId}' AND
-                awb.updated_time >= '${payload.date} 00:00:00' AND
-                awb.updated_time <= '${payload.date} 23:59:59' AND
-                CAST(awb.updated_time AS TIME) >= '${smsTrackingShift.workFrom}' AND
-                CAST(awb.updated_time AS TIME) <= '${smsTrackingShift.workTo}'
-              )`);
-            } else {
-              qb.andWhere(`(
-                awb.awb_status_id = '${data.awbStatusId}' AND
-                awb.updated_time >= '${payload.date} 00:00:00' AND
-                awb.updated_time <= '${payload.date} 23:59:59' AND
-                (
-                  CAST(awb.updated_time AS TIME) <= '${smsTrackingShift.workFrom}' OR
-                  CAST(awb.updated_time AS TIME) => '${smsTrackingShift.workTo}'
-                )
-              )`);
-            }
-          }
+        } else {
+          qb.orWhere(`(
+            aia.awb_status_id_last = '${data.awbStatusId}' AND
+            aia.updated_time >= '${date7DayBefore} 00:00:00' AND
+            aia.updated_time <= '${date} 23:59:59' AND
+            (
+              CAST(aia.updated_time AS TIME) <= '${smsTrackingShift.workFrom}' OR
+              CAST(aia.updated_time AS TIME) >= '${smsTrackingShift.workTo}'
+            )
+          )`);
         }
-      });
-      return 'ok';
-    // qb.andWhere(`(
-    //   (
-    //     stu.sms_tracking_user_name = 'Recipient' AND
-    //     prd.recipient_phone ~ '^([/08|/+\62][0-9]{10,13})$'
-    //   ) OR
-    //   (
-    //     stu.sms_tracking_user_name = 'Sender' AND
-    //     prd.recipient_phone ~ '^([/08|/+\62][0-9]{9,11})$'
-    //   )
-    // )`);
-      const result = await qb.getRawMany();
+      } else if (!data.isRepeatedOver && data.isRepeated) { // filter diluar lingkup jam shifting dan 7 hari kebelakang
+        console.log('filter diluar lingkup jam shifting dan 7 hari kebelakang ' + data.awbStatusId);
+        if (workToDT.isAfter(workFromDT)) {
+          qb.orWhere(`(
+            aia.awb_status_id_last = '${data.awbStatusId}' AND
+            aia.updated_time >= '${date7DayBefore} 00:00:00' AND
+            aia.updated_time <= '${date} 23:59:59' AND
+            CAST(aia.updated_time AS TIME) < '${smsTrackingShift.workFrom}' AND
+            CAST(aia.updated_time AS TIME) > '${smsTrackingShift.workTo}'
+          )`);
+        } else {
+          qb.orWhere(`(
+            aia.awb_status_id_last = '${data.awbStatusId}' AND
+            aia.updated_time >= '${date7DayBefore} 00:00:00' AND
+            aia.updated_time <= '${date} 23:59:59' AND
+            (
+              CAST(aia.updated_time AS TIME) > '${smsTrackingShift.workFrom}' OR
+              CAST(aia.updated_time AS TIME) < '${smsTrackingShift.workTo}'
+            )
+          )`);
+        }
+      } else { // filter hanya dalam lingkup jam shifting pada tanggal request
+        if (workToDT.isAfter(workFromDT)) {
+          qb.orWhere(`(
+            aia.awb_status_id_last = '${data.awbStatusId}' AND
+            aia.updated_time >= '${date} 00:00:00' AND
+            aia.updated_time <= '${date} 23:59:59' AND
+            CAST(aia.updated_time AS TIME) >= '${smsTrackingShift.workFrom}' AND
+            CAST(aia.updated_time AS TIME) <= '${smsTrackingShift.workTo}'
+          )`);
+        } else {
+          qb.orWhere(`(
+            aia.awb_status_id_last = '${data.awbStatusId}' AND
+            aia.updated_time >= '${date} 00:00:00' AND
+            aia.updated_time <= '${date} 23:59:59' AND
+            (
+              CAST(aia.updated_time AS TIME) <= '${smsTrackingShift.workFrom}' OR
+              CAST(aia.updated_time AS TIME) >= '${smsTrackingShift.workTo}'
+            )
+          )`);
+        }
+      }
+    });
+    const data = await qb.getRawMany();
+    const listValid = [];
+    const listInvalid = [];
+    let header = [];
+    let temp = null;
 
-      const obj = [];
-      const obj2 = [];
-      for (let i = 1; i <= 20; i++) {
-      obj.push({
-        name: 'test' + i,
-      });
-      obj2.push({
-        age: i,
-      });
-    }
+    // // override header with first index data, header:[status, phone, note]
+    // if (!isEmpty(data)) {
+    //   header = [
+    //     data[0].awbStatusName,
+    //     data[0].sentTo === 'Sender' ? data[0].senderPhone : data[0].recipientPhone,
+    //     data[0].note,
+    //   ];
+    //   data.shift();
+    // }
+    // mapping data to object excel
+    data.map(function (detail) {
+      const content = {};
+      if (detail.sentTo === 'Sender') {
+        if (detail.statusPhone === 'valid') {
+          content['Waybill Number'] = detail.waybill;
+          content['Phone'] = detail.senderPhone;
+          content['Name of Recipient'] = detail.note;
+          listValid.push(content);
+        } else {
+          content['Waybill Number'] = detail.waybill;
+          content['Name of Sender'] = detail.senderName;
+          content['Phone of Sender'] = detail.senderPhone;
+          content['Name of Recipient'] = detail.recipientName;
+          content['Phone of Recipient'] = detail.recipientPhone;
+          content['Sigesit'] = detail.driverName; //
+          content['Position'] = detail.BranchName; //
+          content['Remark'] = detail.packageNote; //
+          content['Tracking Type'] = detail.awbStatusName;
+          content['Agency Code'] = detail.representativeName; //
+          content['Note of SMS'] = detail.note;
+          listInvalid.push(content);
+        }
+      } else if (detail.sentTo === 'Recipient') {
+        if (detail.statusPhone === 'valid') {
+          content[header[0]] = detail.awbStatusName;
+          content[header[1]] = detail.recipientPhone;
+          content[header[2]] = detail.note;
+          listValid.push(content);
+        } else {
+          content['Waybill Number'] = detail.waybill;
+          content['Name of Sender'] = detail.senderName;
+          content['Phone of Sender'] = detail.senderPhone;
+          content['Name of Recipient'] = detail.recipientName;
+          content['Phone of Recipient'] = detail.recipientPhone;
+          content['Sigesit'] = detail.driverName; //
+          content['Position'] = detail.BranchName; //
+          content['Remark'] = detail.packageNote; //
+          content['Tracking Type'] = detail.awbStatusName;
+          content['Agency Code'] = detail.representativeName; //
+          content['Note of SMS'] = detail.note;
+          listInvalid.push(content);
+        }
+      }
+    });
 
     // NOTE: create excel using unique name
-      const fileName = 'data_' + moment().format('YYMMDD_HHmmss') + '.xlsx';
-      try {
+    const fileName = 'data_' + moment().format('YYMMDD_HHmmss') + '.xlsx';
+    try {
       const newWB = xlsx.utils.book_new();
-      const newWS = xlsx.utils.json_to_sheet(obj);
-      const newWS2 = xlsx.utils.json_to_sheet(obj2);
+      const newWS = xlsx.utils.json_to_sheet(listValid);
+      const newWS2 = xlsx.utils.json_to_sheet(listInvalid);
       xlsx.utils.book_append_sheet(newWB, newWS, 'Sheet1');
       xlsx.utils.book_append_sheet(newWB, newWS2, 'Sheet2');
-      // const resp = newWB.Sheets;
-      // console.log(resp);
       xlsx.writeFile(newWB, fileName);
 
       // const buffExcel = fs.createReadStream(fileName);
