0f6d24dc4e575420ba4c0faea704f1190cc7e86b
Adry Andreas <adryhappier@gmail.com>
2020-05-14 10:02:36 +0700

    :hammer: fixing

--- src/servers/background/models/trackingnote.response.vm.ts
+++ src/servers/background/models/trackingnote.response.vm.ts
@@ -2,9 +2,6 @@ import { ApiModelProperty } from '../../../shared/external/nestjs-swagger';
 import { TrackingNoteVm } from './trackingnote.vm';
 
 export class TrackingNoteResponseVm {
-  @ApiModelProperty()
-  message: string;
-
   @ApiModelProperty({ type: () => [TrackingNoteVm] })
   data: TrackingNoteVm[];
 
