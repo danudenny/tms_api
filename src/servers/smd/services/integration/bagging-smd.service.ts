b861716a538a816fec46129bf0408b6b9a284c8b
niudanrim <niudanri12@gmail.com>
2020-06-07 19:42:58 +0700

    fix error create bagging

--- src/servers/smd/services/integration/bagging-smd.service.ts
+++ src/servers/smd/services/integration/bagging-smd.service.ts
@@ -5,6 +5,8 @@ import { Bagging } from '../../../../shared/orm-entity/bagging';
 import {createQueryBuilder} from 'typeorm';
 import {SmdScanBaggingPayloadVm} from '../../../main/models/smd-bagging-payload.vm';
 import {SmdScanBaggingResponseVm} from '../../../main/models/smd-bagging-response.vm';
+import {BaggingItem} from '../../../../shared/orm-entity/bagging-item';
+import {BagItem} from '../../../../shared/orm-entity/bag-item';
 
 @Injectable()
 export class BaggingSmdService {
@@ -29,25 +31,33 @@ export class BaggingSmdService {
     // check status gabung paket
     const qb = createQueryBuilder();
     qb.addSelect('bi.bag_item_id', 'bagItemId');
+    qb.addSelect('bi.weight', 'weight');
     qb.from('bag', 'b');
     qb.innerJoin('bag_item', 'bi', 'bi.bag_id = b.bag_id AND bi.is_deleted = false');
-    qb.innerJoin('bagging_item', 'bt', 'bt.bag_item_id = bi.bag_item_id AND bt.is_deleted = false ');
-    qb.innerJoin('bagging', 'ba', 'ba.bagging_id = bt.bagging_id AND ba.is_deleted = false AND ba.branch_id = :branchId',
-      { branchId: permissionPayload.branchId },
-    );
     qb.where('b.bag_number = :bagNumber', { bagNumber });
     qb.andWhere('bi.bag_seq = :bagSeq', { bagSeq });
     qb.andWhere('b.is_deleted = false');
     const dataBagging = await qb.getRawOne();
 
-    if (dataBagging) {
+    qb.innerJoin('bagging_item', 'bt', 'bt.bag_item_id = bi.bag_item_id AND bt.is_deleted = false ');
+    qb.innerJoin('bagging', 'ba', 'ba.bagging_id = bt.bagging_id AND ba.is_deleted = false AND ba.branch_id = :branchId',
+      { branchId: permissionPayload.branchId },
+    );
+    const dataExists = await qb.getRawOne();
+    if (dataExists) {
       result.message = 'Resi Gabung Paket sudah di scan';
+      return result;
+    }
+    if (!dataBagging) {
+      result.message = 'Resi gabung paket tidak di temukan';
       return result;
     }
 
     if (payload.baggingId) {
       const q = createQueryBuilder();
       q.addSelect('b.bagging_id', 'baggingId');
+      q.addSelect('b.total_weight', 'totalWeight');
+      q.addSelect('b.totalItem', 'totalItem');
       q.from('bagging', 'b');
       q.andWhere('bagging_id = :baggingId', { baggingId: payload.baggingId });
       const bagging = await q.getRawOne();
@@ -57,12 +67,20 @@ export class BaggingSmdService {
         return result;
       }
       baggingId = bagging.baggingId;
+
+      const total_weight = (Number(dataBagging.weight) + Number(bagging.totalWeight));
+      await Bagging.update(baggingId, {
+        totalWeight: total_weight.toString(),
+        totalItem: (bagging.totalItem + 1),
+      });
+
     } else {
       const bagging = Bagging.create();
       bagging.userId = authMeta.userId.toString();
       bagging.representativeIdTo = payload.representativeCode;
       bagging.branchId = permissionPayload.branchId.toString();
       bagging.totalItem = 1;
+      bagging.totalWeight = dataBagging.weight.toString();
       bagging.baggingCode = await this.generateCode();
       bagging.baggingDate = await this.dateMinus1day(moment().toDate());
       bagging.userIdCreated = authMeta.userId.toString();
@@ -74,7 +92,20 @@ export class BaggingSmdService {
 
       baggingId = bagging.baggingId;
     }
+    const baggingItem = BaggingItem.create();
+    baggingItem.baggingId = baggingId;
+    baggingItem.bagItemId = dataBagging.bagItemId;
+    baggingItem.userIdCreated = authMeta.userId.toString();
+    baggingItem.userIdUpdated = authMeta.userId.toString();
+    baggingItem.createdTime = moment().toDate();
+    baggingItem.updatedTime = moment().toDate();
+    BaggingItem.save(baggingItem);
 
+    await BagItem.update(dataBagging.bagItemId, {
+      baggingIdLast: Number(baggingId),
+    });
+
+    result.status = 'success';
     result.baggingId = baggingId;
     result.message = 'Scan gabung paket berhasil';
     return result;
