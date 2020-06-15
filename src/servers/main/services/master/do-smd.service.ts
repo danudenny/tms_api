9156fbea20914bb359f6c806ba778801791114f0
niudanrim <niudanri12@gmail.com>
2020-06-15 10:14:20 +0700

    fix error salah nm variable

--- src/servers/main/services/master/do-smd.service.ts
+++ src/servers/main/services/master/do-smd.service.ts
@@ -27,7 +27,7 @@ export class DoSmdService {
       ['t1.do_smd_id', 'doSmdId'],
       ['t1.do_smd_code', 'doSmdCode'],
     );
-    q.innerJoin(e => e.doSmdDetail, 't2', j =>
+    q.innerJoin(e => e.doSmdDetails, 't2', j =>
       j.andWhere(e => e.isDeleted, w => w.isFalse()),
     );
     q.andWhere(e => e.isDeleted, w => w.isFalse());
