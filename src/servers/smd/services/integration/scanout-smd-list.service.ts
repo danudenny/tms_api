d50f9bb71b3712242ab521cf8a017dd87fbe624e
oeikurniawan <kurniawansicepat@gmail.com>
2020-06-16 19:29:05 +0700

    Add order by created time history

--- src/servers/smd/services/integration/scanout-smd-list.service.ts
+++ src/servers/smd/services/integration/scanout-smd-list.service.ts
@@ -162,6 +162,7 @@ export class ScanoutSmdListService {
       j.andWhere(e => e.isDeleted, w => w.isFalse()),
     );
     q.andWhere(e => e.isDeleted, w => w.isFalse());
+    q.orderByRaw('dsh.created_time', 'ASC');
 
     const data = await q.exec();
     const total = await q.countWithoutTakeAndSkip();
