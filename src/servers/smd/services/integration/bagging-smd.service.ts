ccfef56b65120f05a55ef1accb5f76484e7ea151
niudanrim <niudanri12@gmail.com>
2020-06-18 16:02:05 +0700

    tambah order by resi

--- src/servers/smd/services/integration/bagging-smd.service.ts
+++ src/servers/smd/services/integration/bagging-smd.service.ts
@@ -121,6 +121,7 @@ export class BaggingSmdService {
         b.bag_number = '${bagNumber}' AND
         bi.bag_seq = '${bagSeq}' AND
         b.is_deleted = false
+      ORDER BY b.created_time DESC
       LIMIT 1;
     `;
     const dataPackage = await RawQueryService.query(rawQuery);
