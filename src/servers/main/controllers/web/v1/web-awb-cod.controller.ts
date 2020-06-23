d5cd1d8ef8c61c9b11db334a696ab12efeeb9b73
Adry Andreas <adryhappier@gmail.com>
2020-06-23 12:48:51 +0700

    :rice: add body payload

--- src/servers/main/controllers/web/v1/web-awb-cod.controller.ts
+++ src/servers/main/controllers/web/v1/web-awb-cod.controller.ts
@@ -15,6 +15,7 @@ import { AuthenticatedGuard } from '../../../../../shared/guards/authenticated.g
 import { BaseMetaPayloadVm } from '../../../../../shared/models/base-meta-payload.vm';
 import { V1WebAwbCodService } from '../../../services/web/v1/web-awb-cod.service';
 import { WebAwbCodListResponseVm } from '../../../models/cod/web-awb-cod-response';
+import { PermissionTokenGuard } from '../../../../../shared/guards/permission-token.guard';
 
 @ApiUseTags('Web Awb COD')
 @Controller('web/v1/cod')
@@ -24,15 +25,57 @@ export class V1WebAwbCodController {
   @HttpCode(HttpStatus.OK)
   @UseGuards(AuthenticatedGuard)
   @ApiOkResponse({ type: WebAwbCodListResponseVm })
-  public async awb(payload: BaseMetaPayloadVm) {
+  public async awb(@Body() payload: BaseMetaPayloadVm) {
     return V1WebAwbCodService.awbCod(payload);
   }
 
   // @Post('transferBranch')
   // @HttpCode(HttpStatus.OK)
-  // @UseGuards(AuthenticatedGuard)
+  // @UseGuards(AuthenticatedGuard, PermissionTokenGuard)
   // @ApiOkResponse({ type: WebAwbCodListResponseVm })
   // public async transferBranch(payload: BaseMetaPayloadVm) {
-  //   return {}; // V1WebTrackingService.awb(payload);
+  //   // TODO:
+  //   // payload:
+  //   // awb_number[]
+  //   // payment_method_code ?? ['cash', 'cashless']
+  //   // user login, branch login
+  //   // scan awb check status only dlv and update terima cabang
+  //   // create table header - child
+  //   // table:
+  //   // cod transaction branch
+  //   // transaction code |datetime | total awb | total value | branch name last | status ? | user || payment method
+  //   // cod transaction branch detail
+  //   // penerima | layanan | branch name last | cod_value | sigesit | status
+  //   return {}; // V1WebAwbCodService.awb(payload);
+  // }
+
+  // @Post('transactionBranch')
+  // @HttpCode(HttpStatus.OK)
+  // @UseGuards(AuthenticatedGuard)
+  // @ApiOkResponse({ type: WebAwbCodListResponseVm })
+  // public async transactionBranch(payload: BaseMetaPayloadVm) {
+  //   // get data transaction branch
+  //   // transaction code |datetime | total awb | total value | branch name last | status ? | user
+  //   return {};
+  // }
+
+  // @Post('detailAwbBranch')
+  // @HttpCode(HttpStatus.OK)
+  // @UseGuards(AuthenticatedGuard)
+  // @ApiOkResponse({ type: WebAwbCodListResponseVm })
+  // public async detailAwbBranch(payload: BaseMetaPayloadVm) {
+  //   // get data detail
+  //   // penerima | layanan | branch name last | cod_value | sigesit | status
+  //   return {};
+  // }
+
+  // @Post('transferHeadOffice')
+  // @HttpCode(HttpStatus.OK)
+  // @UseGuards(AuthenticatedGuard, PermissionTokenGuard)
+  // @ApiOkResponse({ type: WebAwbCodListResponseVm })
+  // public async transferHeadOffice(payload: BaseMetaPayloadVm) {
+  //   // TODO:
+
+  //   return {};
   // }
 }
