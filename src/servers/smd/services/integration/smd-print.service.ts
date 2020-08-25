1c42685766d08d0d4b2a5a400d97356c69067299
edisicepat <edi.santoso@e.sicepat.com>
2020-08-25 09:26:29 +0700

    Fix query where in printReceivedBagForPaper

--- src/servers/smd/services/integration/smd-print.service.ts
+++ src/servers/smd/services/integration/smd-print.service.ts
@@ -694,7 +694,7 @@ export class SmdPrintService {
         },
       })
       .where(e => e.isDeleted, w => w.equals(false))
-      .where(e => e.receivedBagId, w => w.equals(payload.id));
+      .andWhere(e => e.receivedBagId, w => w.equals(payload.id));
 
     if (!receivedBag) {
       RequestErrorService.throwObj({
