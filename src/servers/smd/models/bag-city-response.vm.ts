a5e264870f1ee3bcf21d36b102c42692b868a943
mniudanri <niudanri12@gmail.com>
2020-09-10 21:07:25 +0700

    Revert "fix error"
    This reverts commit 298330d24ca0b0b6a0975d4dfde4a52cf7779cf1.

--- src/servers/smd/models/bag-city-response.vm.ts
+++ src/servers/smd/models/bag-city-response.vm.ts
@@ -1,5 +1,6 @@
 import { ApiModelProperty } from '../../../shared/external/nestjs-swagger';
 import { BaseMetaResponseVm } from '../../../shared/models/base-meta-response.vm';
+import { BagCityInputManualDataPayloadVm } from './bag-city-payload.vm';
 
 export class BagCityResponseVm {
   @ApiModelProperty()
