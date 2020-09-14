f2e23bbe8394a55eb3ff3364ebaf7525bab08ea6
oeikurniawan <kurniawansicepat@gmail.com>
2020-09-14 15:16:22 +0700

    Add sigesit in api checkSPK diva

--- src/servers/background/services/integration/partner-diva.service.ts
+++ src/servers/background/services/integration/partner-diva.service.ts
@@ -29,10 +29,14 @@ export class PartnerDivaService {
     const data = [];
     const rawQuery = `
       SELECT
-        prd.ref_awb_number
+        prd.ref_awb_number,
+        e.fullname,
+        e.nik
       FROM do_pickup_partner dpp
       INNER JOIN do_pickup_partner_detail dppd on dpp.do_pickup_partner_id = dppd.do_pickup_partner_id and dppd.is_deleted = FALSE
       INNER JOIN pickup_request_detail prd ON dppd.work_order_id = prd.work_order_id_last AND prd.is_deleted = FALSE
+      LEFT JOIN work_order wo ON prd.work_order_id_last = wo.work_order_id AND wo.is_deleted = FALSE
+      LEFT JOIN employee e ON wo.employee_id_driver = e.employee_id AND e.is_deleted = FALSE
       WHERE
         dpp.do_pickup_partner_code = '${escape(payload.spk_code)}' AND
         dpp.is_deleted = FALSE
@@ -43,6 +47,8 @@ export class PartnerDivaService {
       for (let a = 0; a < resultData.length; a++) {
         data.push({
           awb_number: resultData[a].ref_awb_number,
+          employee_name: resultData[a].fullname,
+          employee_nik: resultData[a].nik,
         });
       }
       result.statusCode = HttpStatus.OK;
