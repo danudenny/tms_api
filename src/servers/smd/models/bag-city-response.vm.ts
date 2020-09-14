e7bf3ae201c0fc3ba026879329e0e51fa4977d1c
mniudanri <niudanri12@gmail.com>
2020-09-14 12:52:57 +0700

    tambah endpoint create header bag city

--- src/servers/smd/models/bag-city-response.vm.ts
+++ src/servers/smd/models/bag-city-response.vm.ts
@@ -131,3 +131,20 @@ export class ListDetailBagCityResponseVm extends BaseMetaResponseVm {
   @ApiModelProperty({ type: () => [ListDetailBagCityDataVm] })
   data: ListDetailBagCityDataVm[];
 }
+
+export class CreateBagCityResponseVm {
+  @ApiModelProperty()
+  bagRepresentativeId: number;
+
+  @ApiModelProperty()
+  bagRepresentativeCode: string;
+
+  @ApiModelProperty()
+  representativeCode: string;
+
+  @ApiModelProperty()
+  status: string;
+
+  @ApiModelProperty()
+  message: string;
+}
