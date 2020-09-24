00ccb13d301c5a735cdbeb7e0991cd5a0b212b6f
Adry Andreas <adryhappier@gmail.com>
2020-09-24 12:45:11 +0700

    :gear: change host master to domain name

--- ormconfig.production.js
+++ ormconfig.production.js
@@ -10,7 +10,7 @@ module.exports = {
   type: 'postgres',
   replication: {
     master: {
-      host: 'sicepat-tms-new.cchjcxaiivov.ap-southeast-1.rds.amazonaws.com',
+      host: 'tmsnew.sicepat.com',
       port: 5432,
       username: 'sicepattmsuser',
       password: 's1c3p4Ttm$us3R3ncrypT3dbo05$',
