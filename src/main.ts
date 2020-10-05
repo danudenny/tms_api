1cacaa5601f2d217205b00cc1d1f9b2bdad742d7
arjunsumarlan <arjunsumarlan@gmail.com>
2020-10-05 17:24:52 +0700

    add newrelic in cod mobile payment

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { boot } from './main-setup';
 import { SentryService } from './shared/services/sentry.service';
 import { WinstonLogglyService } from './shared/services/winston-loggly.service';
 // tslint:disable-next-line: no-var-requires
-require('newrelic');
+require('./newrelic');
 
 // init setup service
 SentryService.setup();
