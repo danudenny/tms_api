dbabb1beb11d35f37bb9bc22f843b9c5824c4139
mniudanri <niudanri12@gmail.com>
2020-10-19 00:48:17 +0700

    tambah validasi filter

--- src/servers/main/services/web/hub-transit/hub-monitoring.service.ts
+++ src/servers/main/services/web/hub-transit/hub-monitoring.service.ts
@@ -4,6 +4,8 @@ import { RequestQueryBuidlerService } from '../../../../../shared/services/reque
 import { BaseMetaPayloadFilterVm, BaseMetaPayloadFilterVmOperator, BaseMetaPayloadVm } from '../../../../../shared/models/base-meta-payload.vm';
 import { MetaService } from '../../../../../shared/services/meta.service';
 import { BAG_STATUS } from '../../../../../shared/constants/bag-status.constant';
+import {RequestErrorService} from '../../../../../shared/services/request-error.service';
+import {HttpStatus} from '@nestjs/common';
 
 export class HubMonitoringService {
 
@@ -55,6 +57,21 @@ export class HubMonitoringService {
     if (!payload.limit) {
       payload.limit = 10;
     }
+
+    const optrDate = ['gte', 'lte', 'lt', 'gt'];
+    const checkDateFilter = await this.orionFilterToQueryRawBySelectedFilter(payload.filters, '', optrDate, 'createdTime');
+
+    const optr = ['eq'];
+    const checkFilterBranchTo = await this.orionFilterToQueryRawBySelectedFilter(payload.filters, '', optr, 'branchTo');
+    const checkFilterBranchFrom = await this.orionFilterToQueryRawBySelectedFilter(payload.filters, '', optr, 'branchFrom');
+
+    if (!checkFilterBranchTo && !checkFilterBranchFrom) {
+      RequestErrorService.throwObj({message: 'FIlter Tujuan atau Asal tidak boleh kosong'}, HttpStatus.BAD_REQUEST);
+    }
+    if (!checkDateFilter) {
+      RequestErrorService.throwObj({message: 'FIlter Tanggal tidak boleh kosong'}, HttpStatus.BAD_REQUEST);
+    }
+
     const data = await RawQueryService.query(
       await this.getQueryMonitoringSortirListByFilterOrion(payload),
     );
@@ -280,7 +297,7 @@ export class HubMonitoringService {
         INNER JOIN awb awb ON awb.awb_id = dohd.awb_id AND awb.is_deleted = FALSE
         INNER JOIN district dt ON dt.district_id = awb.to_id AND dt.is_deleted = FALSE
         INNER JOIN branch br ON br.branch_id = dt.branch_id_delivery AND br.is_deleted = FALSE
-        LEFT JOIN
+        INNER JOIN
         (
           SELECT
             ai.awb_id,
@@ -354,23 +371,24 @@ export class HubMonitoringService {
     filterQuery += whereQuery;
 
     const query = `
-      WITH detail AS (
+      WITH detail as (
         SELECT
           br.branch_name AS "branchTo",
           MIN(doh.created_time) AS "createdTime",
-          COUNT(distinct bag_sortir.bag_item_id) AS "totalBagSortir",
-          COUNT(distinct dohd.dropoff_hub_detail_id) AS "totalAwb",
-          COUNT(distinct bag_sortir.awb_id) AS "totalScanInAwb",
-          COUNT(DISTINCT dpdb.bag_item_id) AS "totalScanOutBagSortir"
+          COUNT(DISTINCT bag_sortir.awb_id) AS "totalBagSortir",
+          COUNT(DISTINCT dohd.dropoff_hub_detail_id) AS "totalAwb",
+          COUNT(DISTINCT bag_sortir.awb_id) AS "totalScanInAwb",
+          COUNT(DISTINCT bia1.awb_item_id) AS "totalScanOutBagSortir"
         FROM
           dropoff_hub doh
-        INNER JOIN bag b ON b.bag_id = doh.bag_id AND b.is_deleted = FALSE AND b.branch_id is not null
+        INNER JOIN bag bag ON bag.bag_id = doh.bag_id AND bag.is_deleted = FALSE AND bag.branch_id IS NOT NULL
         INNER JOIN bag_item bi ON bi.bag_item_id = doh.bag_item_id AND bi.is_deleted = FALSE
+        INNER JOIN bag_item_awb bia ON bia.bag_item_id = bi.bag_item_id AND bia.is_deleted = FALSE
         INNER JOIN dropoff_hub_detail dohd ON dohd.dropoff_hub_id = doh.dropoff_hub_id AND dohd.is_deleted = FALSE
         INNER JOIN awb awb ON awb.awb_id = dohd.awb_id AND awb.is_deleted = FALSE
         INNER JOIN district dt ON dt.district_id = awb.to_id AND dt.is_deleted = FALSE
         INNER JOIN branch br ON br.branch_id = dt.branch_id_delivery AND br.is_deleted = FALSE
-        LEFT JOIN
+        INNER JOIN
         (
           SELECT
             ai.awb_id,
@@ -384,22 +402,20 @@ export class HubMonitoringService {
           WHERE bia.is_deleted = FALSE ${whereSubQuery ? `AND ${whereSubQuery}` : ''}
         ) bag_sortir ON dohd.awb_id = bag_sortir.awb_id
         LEFT JOIN do_pod_detail_bag dpdb ON dpdb.bag_item_id = bag_sortir.bag_item_id AND dpdb.is_deleted = FALSE
+        LEFT JOIN bag_item_awb bia1 ON bia1.bag_item_id = dpdb.bag_item_id AND bia1.is_deleted = FALSE
         WHERE
-          (
-            doh.branch_id IS NOT NULL
-            ${whereQuery ? `AND ${whereQuery}` : ''}
-            ${payload.search ? `AND br.branch_name ~* '${payload.search}'` : ''}
-          )
+          doh.branch_id IS NOT NULL
+          ${whereQuery ? `AND ${whereQuery}` : ''}
+          ${payload.search ? `AND br.branch_name ~* '${payload.search}'` : ''}
         GROUP BY
-          br.branch_name, bag_sortir.bag_item_id
+          br.branch_name, bi.bag_item_id
       )
       SELECT *
       FROM (
         SELECT *,
           CASE
-            WHEN "totalBagSortir" = "totalScanOutBagSortir" AND "totalBagSortir" > 0 THEN 'Loading'
-            WHEN "totalBagSortir" != "totalScanOutBagSortir" AND "totalBagSortir" > 0 THEN 'Sortir'
-            ELSE 'Do Hub'
+            WHEN "totalBagSortir" = "totalScanOutBagSortir" THEN 'Loading'
+            ELSE 'Sortir'
           END AS "status"
         FROM (
           SELECT
@@ -422,9 +438,9 @@ export class HubMonitoringService {
       ${payload.sortBy && sortingMap[payload.sortBy] ?
       `ORDER BY ${sortingMap[payload.sortBy]} ${payload.sortDir}` :
       `ORDER BY
-        CASE WHEN "status" = 'Do Hub' then 1
-          WHEN "status" = 'Sortir' then 2
-          WHEN "status" = 'Loading' then 3
+        CASE
+          WHEN "status" = 'Sortir' then 1
+          WHEN "status" = 'Loading' then 2
         END`
       }
       LIMIT ${payload.limit}
