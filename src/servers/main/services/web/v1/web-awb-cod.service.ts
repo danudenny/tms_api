8095e6f6b10f071cfc1ac937fc8f5ca850b62175
Firas Luthfi <vrrases@gmail.com>
2020-12-03 16:23:24 +0700

    remove join with employee

--- src/servers/main/services/web/v1/web-awb-cod.service.ts
+++ src/servers/main/services/web/v1/web-awb-cod.service.ts
@@ -121,7 +121,7 @@ export class V1WebAwbCodService {
       ['t2.total_cod_value', 'codValue'],
       ['t6.representative_id', 'representativeId'],
       ['t4.user_id', 'userIdDriver'],
-      [`CONCAT(driver.nik, ' - ', t4.first_name)`, 'driverName'],
+      ['t4.first_name', 'driverName'],
       ['t5.package_type_code', 'packageTypeCode'],
       ['t8.do_pod_deliver_detail_id', 'doPodDeliverDetailId'],
       [`t8.cod_payment_method`, 'codPaymentMethod'],
@@ -166,10 +166,6 @@ export class V1WebAwbCodService {
     );
 
     q.leftJoin(e => e.codPayment.userDriver, 't4', j =>
-      j.andWhere(e => e.isDeleted, w => w.isFalse()),
-    );
-
-    q.leftJoin(e => e.codPayment.userDriver.employee, 'driver', j =>
       j.andWhere(e => e.isDeleted, w => w.isFalse()),
     );
 
@@ -274,7 +270,7 @@ export class V1WebAwbCodService {
       ['t2.total_cod_value', 'codValue'],
       ['t6.representative_id', 'representativeId'],
       ['t4.user_id', 'userIdDriver'],
-      [`CONCAT(driver.nik, ' - ', t4.first_name)`, 'driverName'],
+      ['t4.first_name', 'driverName'],
       ['t5.package_type_code', 'packageTypeCode'],
       ['t8.do_pod_deliver_detail_id', 'doPodDeliverDetailId'],
       [`t8.cod_payment_method`, 'codPaymentMethod'],
@@ -317,10 +313,6 @@ export class V1WebAwbCodService {
     );
 
     q.leftJoin(e => e.codPayment.userDriver, 't4', j =>
-      j.andWhere(e => e.isDeleted, w => w.isFalse()),
-    );
-
-    q.leftJoin(e => e.codPayment.userDriver.employee, 'driver', j =>
       j.andWhere(e => e.isDeleted, w => w.isFalse()),
     );
 
