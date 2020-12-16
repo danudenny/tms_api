f0ce09ab99dab24b5020ee1541f6f2288bc24293
mniudanri <niudanri12@gmail.com>
2020-12-16 17:02:09 +0700

    add endpoints smd report

--- src/servers/report/controllers/main-server-controllers.module.ts
+++ src/servers/report/controllers/main-server-controllers.module.ts
@@ -5,6 +5,10 @@ import { SharedModule } from '../../../shared/shared.module';
 import { MainServerServicesModule } from '../services/main-server-services.module';
 import { V1WebAwbCodController } from './cod/v1/web-awb-cod.controller';
 import { V2WebCodReportController } from './cod/v2/web-cod-report.controller';
+import { SmdVendorReportController } from './smd/smd-vendor-report.controller';
+import { SmdMonitoringReportController } from './smd/smd-monitoring-report.controller';
+import { SmdScaninReportController } from './smd/smd-scanin-report.controller';
+import { SmdScanoutReportController } from './smd/smd-scanout-report.controller';
 
 // #endregion
 @Module({
@@ -12,6 +16,10 @@ import { V2WebCodReportController } from './cod/v2/web-cod-report.controller';
   controllers: [
     V1WebAwbCodController,
     V2WebCodReportController,
+    SmdVendorReportController,
+    SmdMonitoringReportController,
+    SmdScaninReportController,
+    SmdScanoutReportController,
   ],
 })
 export class MainServerControllersModule {}
