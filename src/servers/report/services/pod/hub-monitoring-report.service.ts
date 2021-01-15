d35c9fe6ef394539c48d4e747130fca22aefb343
ajinbramantio <ajinbramantio@gmaikl.com>
2021-01-15 20:07:12 +0700

    modified wrong condition csv

--- src/servers/report/services/pod/hub-monitoring-report.service.ts
+++ src/servers/report/services/pod/hub-monitoring-report.service.ts
@@ -20,10 +20,7 @@ export class HubMonitoringReportService {
     payload: any,
   ): Promise<HubMonitoringExcelStoreResponseVm> {
     const result = new HubMonitoringExcelStoreResponseVm();
-    result.id = await CsvHelper.storePayload(
-      payload,
-      'hub-monitoring',
-    );
+    result.id = await CsvHelper.storePayload(payload, 'hub-monitoring');
     return result;
   }
 
@@ -63,6 +60,7 @@ export class HubMonitoringReportService {
       'hub-monitoring',
       queryParams.id,
     );
+
     if (!payload) {
       RequestErrorService.throwObj(
         {
@@ -81,8 +79,12 @@ export class HubMonitoringReportService {
     await CsvHelper.generateCSV(res, data, fileName);
   }
 
-  static async getDataCsvHubMonitoring(payload: BaseMetaPayloadVm): Promise<any> {
-    let query = await HubMonitoringService.getQueryMonitoringHubBagByFilterOrion(payload);
+  static async getDataCsvHubMonitoring(
+    payload: BaseMetaPayloadVm,
+  ): Promise<any> {
+    let query = await HubMonitoringService.getQueryMonitoringHubBagByFilterOrion(
+      payload,
+    );
     query = query.split('"doPodDateTime"').join('"Tanggal"');
     query = query.split('"origin"').join('"Origin"');
     query = query.split('"status"').join('"Status"');
@@ -97,10 +99,14 @@ export class HubMonitoringReportService {
     return data;
   }
 
-  static async getDataCsvHubMonitoringSortir(payload: BaseMetaPayloadVm): Promise<any> {
-    const query = await HubMonitoringService.getQueryCSVMonitoringSortirByFilterOrion(payload);
+  static async getDataCsvHubMonitoringSortir(
+    payload: BaseMetaPayloadVm,
+  ): Promise<any> {
+    const query = await HubMonitoringService.getQueryCSVMonitoringSortirByFilterOrion(
+      payload,
+    );
     const data = await RawQueryService.query(query);
-    if (data.length) {
+    if (!data.length) {
       RequestErrorService.throwObj(
         {
           message: 'Data Monitoring Sortir Not Found!',
