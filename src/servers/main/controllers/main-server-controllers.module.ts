eb04e9403c07b0db912accaec8db5c9005434d06
Adry Andreas <adryhappier@gmail.com>
2021-01-15 13:33:19 +0700

    :ramen: init data mobile v2

--- src/servers/main/controllers/main-server-controllers.module.ts
+++ src/servers/main/controllers/main-server-controllers.module.ts
@@ -65,6 +65,7 @@ import { WebLastMileController } from './web/web-last-mile.controller';
 import { WebMonitoringController } from './web/web-monitoring.controller';
 import { RolePodManualController } from './web/web-role-pod-manual.controller';
 import { V1WebAwbHighValueController } from './web/v1/web-awb-high-value.controller';
+import { V2MobileInitController } from './mobile/v2/mobile-init.controller';
 
 // #endregion
 @Module({
@@ -116,6 +117,7 @@ import { V1WebAwbHighValueController } from './web/v1/web-awb-high-value.control
     DoReturnStatusController,
     DoReturnController,
     V1MobileInitController,
+    V2MobileInitController,
     V1MobileSyncController,
     V2MobileSyncController,
     V1MobileAttendanceController,
--- src/servers/main/models/mobile-init-data-response.vm.ts
+++ src/servers/main/models/mobile-init-data-response.vm.ts
@@ -1,6 +1,6 @@
 import { ApiModelProperty } from '../../../shared/external/nestjs-swagger';
 import { AwbStatusVm } from './awb-status.vm';
-import { MobileDeliveryVm } from './mobile-delivery.vm';
+import { MobileDeliveryVm, MobileDeliveryV2Vm } from './mobile-delivery.vm';
 import { ReasonVm } from './reason.vm';
 import { MobileCheckInResponseVm } from './mobile-check-in-response.vm';
 
@@ -21,9 +21,34 @@ export class MobileInitDataResponseVm {
   checkIn: MobileCheckInResponseVm;
 }
 
+export class MobileInitDataResponseV2Vm {
+  @ApiModelProperty({ type: () => [ReasonVm] })
+  reason: ReasonVm[];
+
+  @ApiModelProperty({ type: () => [AwbStatusVm] })
+  awbStatus: AwbStatusVm[];
+
+  @ApiModelProperty({ type: [MobileDeliveryV2Vm] })
+  delivery: MobileDeliveryV2Vm[];
+
+  @ApiModelProperty({ format: 'date-time' })
+  serverDateTime: string;
+
+  @ApiModelProperty()
+  checkIn: MobileCheckInResponseVm;
+}
+
 export class MobileInitDataDeliveryResponseVm {
   @ApiModelProperty({ type: [MobileDeliveryVm] })
   delivery: MobileDeliveryVm[];
+
+  @ApiModelProperty({ format: 'date-time' })
+  serverDateTime: string;
+}
+
+export class MobileInitDataDeliveryV2ResponseVm {
+  @ApiModelProperty({ type: [MobileDeliveryV2Vm] })
+  delivery: MobileDeliveryV2Vm[];
 
   @ApiModelProperty({ format: 'date-time' })
   serverDateTime: string;
