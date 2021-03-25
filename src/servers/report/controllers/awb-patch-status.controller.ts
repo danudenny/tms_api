c7279f9f9b94c248d9d4f5ac864448397d116bae
Adry Andreas <adryhappier@gmail.com>
2021-03-25 11:29:48 +0700

    :hamburger: add endpoint for awb patch status dlv

---
+++ src/servers/report/controllers/awb-patch-status.controller.ts
@@ -0,0 +1,22 @@
+import { HttpCode, Controller, Post, HttpStatus, UseGuards, Body } from '@nestjs/common';
+import { ApiUseTags, ApiImplicitHeader, ApiOkResponse } from '../../../shared/external/nestjs-swagger';
+import { AuthKeyCodGuard } from '../../../shared/guards/auth-key-cod.guard';
+import { AwbPatchStatusSuccessResponseVm, AwbPatchStatusPayloadVm } from '../models/awb/awb-patch-status.vm';
+import { AwbPatchStatusService } from '../services/awb/patch-status.service';
+
+@ApiUseTags('Awb Patch Status')
+@Controller('awb/patchStatus')
+export class AwbPatchStatusController {
+  constructor() {}
+
+  @Post('dlv')
+  @HttpCode(HttpStatus.OK)
+  @ApiImplicitHeader({ name: 'x-api-key' })
+  @UseGuards(AuthKeyCodGuard)
+  @ApiOkResponse({ type: AwbPatchStatusSuccessResponseVm })
+  public async patchDataDlv(
+    @Body() payload: AwbPatchStatusPayloadVm,
+  ) {
+    return AwbPatchStatusService.patchDataDlv(payload);
+  }
+}
---
+++ src/servers/report/services/awb/patch-status.service.ts
@@ -0,0 +1,87 @@
+import { AwbPatchStatusPayloadVm, AwbPatchStatusSuccessResponseVm } from '../../models/awb/awb-patch-status.vm';
+import { DoPodDeliverDetail } from '../../../../shared/orm-entity/do-pod-deliver-detail';
+import { AWB_STATUS } from '../../../../shared/constants/awb-status.constant';
+import { AwbItemAttr } from '../../../../shared/orm-entity/awb-item-attr';
+import { DoPodDetailPostMetaQueueService } from '../../../queue/services/do-pod-detail-post-meta-queue.service';
+import { AwbStatus } from '../../../../shared/orm-entity/awb-status';
+import { User } from '../../../../shared/orm-entity/user';
+
+export class AwbPatchStatusService {
+  constructor() {}
+
+  static async patchDataDlv(payload: AwbPatchStatusPayloadVm): Promise<AwbPatchStatusSuccessResponseVm> {
+    // get status DLV
+    const awbStatus = await AwbStatus.findOne(
+      {
+        awbStatusId:
+          AWB_STATUS.DLV,
+      },
+      { cache: true },
+    );
+    const errorMsg = [];
+    for (const awb of payload.data) {
+      let message = null;
+      if (awb.length == 12) {
+        const awbPod = await DoPodDeliverDetail.findOne({
+          awbNumber: awb,
+          awbStatusIdLast: AWB_STATUS.DLV,
+          isDeleted: false,
+        });
+        if (awbPod) {
+          // get data employee
+          const userDriver = await User.findOne(
+            {
+              userId: awbPod.userIdUpdated,
+              isDeleted: false,
+            },
+            { cache: true },
+          );
+
+          const awbAttr = await AwbItemAttr.findOne({
+            awbNumber: awb,
+            awbStatusIdLast: AWB_STATUS.ANT,
+            isDeleted: false,
+          });
+
+          // status still ANT insert data awb history DLV
+          if (awbAttr && userDriver) {
+            // NOTE: queue by Bull
+            DoPodDetailPostMetaQueueService.createJobV1MobileSync(
+              awbPod.awbItemId,
+              awbPod.awbStatusIdLast,
+              awbPod.userIdUpdated,
+              awbAttr.branchIdLast,
+              awbPod.userIdUpdated,
+              userDriver.employeeId,
+              awbPod.reasonIdLast,
+              awbPod.descLast,
+              awbPod.consigneeName,
+              awbStatus.awbStatusName,
+              awbStatus.awbStatusTitle,
+              awbPod.awbStatusDateTimeLast,
+              awbPod.latitudeDeliveryLast,
+              awbPod.longitudeDeliveryLast,
+            );
+          } else {
+            message = `Status Resi ${awb} bukan Antar`;
+          }
+        } else {
+          message = `Surat Jalan Resi ${awb} tidak valid`;
+        }
+      } else {
+        message = `panjang resi ${awb} tidak valid`;
+      }
+
+      if (message) {
+        errorMsg.push(message);
+      }
+    }
+
+    const result = new AwbPatchStatusSuccessResponseVm();
+    result.errors = errorMsg;
+    result.message = 'Update Status Success';
+    result.status = 200;
+    return result;
+  }
+
+}
