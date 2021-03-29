87891ea9d51a2ce158950e94974f65a793381add
Firas Luthfi <vrrases@gmail.com>
2021-03-29 17:27:21 +0700

    add note in report cod mongo

--- src/servers/main/services/web/v1/web-report-stream-cod.service.ts
+++ src/servers/main/services/web/v1/web-report-stream-cod.service.ts
@@ -1333,6 +1333,7 @@ export class V1WebReportCodStreamService {
     };
   }
 
+  // Report COD non fee stream
   static async printNonCodSupplierInvoice(filters, response) {
     // TODO: query get data
     const dbTransactionDetail = await MongoDbConfig.getDbSicepatCod(
@@ -1541,6 +1542,7 @@ export class V1WebReportCodStreamService {
     return coll.aggregate(queryParam);
   }
 
+  // Report COD fee stream
   static async printCodSupplierInvoice(filters, response) {
     // TODO: query get data
     // step 1 : query get data by filter
