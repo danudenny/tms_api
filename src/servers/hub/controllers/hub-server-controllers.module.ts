c3ed950c7b8b660ccd439c37d472e792bd3be4ce
mniudanri <niudanri12@gmail.com>
2021-04-06 14:46:35 +0700

    add endpoint monitoring hub

--- src/servers/hub/controllers/hub-server-controllers.module.ts
+++ src/servers/hub/controllers/hub-server-controllers.module.ts
@@ -4,12 +4,14 @@ import { SharedModule } from '../../../shared/shared.module';
 import { HubServerServicesModule } from '../services/hub-server-services.module';
 import { HubMachinePackageController } from './integration/hub-machine-package.controller';
 import { HubMachineSortirController } from './integration/hub-machine-sortir.controller';
+import { HubController } from './integration/hub-monitoring.controller';
 
 @Module({
   imports: [SharedModule, HubServerServicesModule],
   controllers: [
     HubMachineSortirController,
     HubMachinePackageController,
+    HubController,
   ],
 })
 export class HubServerControllersModule {}
---
+++ src/servers/hub/controllers/integration/hub-monitoring.controller.ts
@@ -0,0 +1,31 @@
+import { Body, Controller, HttpCode, HttpStatus, Post, UseGuards } from '@nestjs/common';
+import {
+    ApiBearerAuth, ApiOkResponse, ApiUseTags,
+} from '../../../../shared/external/nestjs-swagger';
+import { AuthenticatedGuard } from '../../../../shared/guards/authenticated.guard';
+import { PermissionTokenGuard } from '../../../../shared/guards/permission-token.guard';
+import { MonitoringBagHubResponseVm, MonitoringSortirHubResponseVm } from '../../../main/models/hub-monitoring.response.vm';
+import { HubMonitoringService } from '../../../main/services/web/hub-transit/hub-monitoring.service';
+import { BaseMetaPayloadVm } from '../../../../shared/models/base-meta-payload.vm';
+
+@ApiUseTags('Hub Monitoring')
+@Controller('pod/hub')
+@ApiBearerAuth()
+@UseGuards(AuthenticatedGuard, PermissionTokenGuard)
+export class HubController {
+  constructor() {}
+
+  @Post('monitoring/bag')
+  @HttpCode(HttpStatus.OK)
+  @ApiOkResponse({ type: MonitoringBagHubResponseVm })
+  public async monitoringBagHub(@Body() payload: BaseMetaPayloadVm) {
+    return HubMonitoringService.monitoringBagHub(payload);
+  }
+
+  @Post('monitoring/sortir')
+  @HttpCode(HttpStatus.OK)
+  @ApiOkResponse({ type: MonitoringSortirHubResponseVm })
+  public async monitoringSortirHub(@Body() payload: BaseMetaPayloadVm) {
+    return HubMonitoringService.monitoringSortirHub(payload);
+  }
+}
