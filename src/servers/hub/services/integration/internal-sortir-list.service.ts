f5e674cd791413ef50d89e7ebf5bfb289e01b4d9
mniudanri <niudanri12@gmail.com>
2021-04-14 13:52:23 +0700

    fix query is_manual, is_sortir - list log sortir

--- src/servers/hub/services/integration/internal-sortir-list.service.ts
+++ src/servers/hub/services/integration/internal-sortir-list.service.ts
@@ -116,16 +116,21 @@ export class InternalSortirListService {
       j.andWhere(e => e.isDeleted, w => w.isFalse()),
     );
     q.leftJoin(e => e.bagItemAwb, 'bia', j =>
+    (
       j.andWhere(e => e.isDeleted, w => w.isFalse()),
+      j.andWhere(e => e.isSortir, w => w.isTrue())
+    ),
     );
     q.leftJoin(e => e.bagItemAwb.bagItem, 'bi', j =>
       j.andWhere(e => e.isDeleted, w => w.isFalse()),
     );
     q.leftJoin(e => e.bagItemAwb.bagItem.bag, 'bag', j =>
-      j.andWhere(e => e.isDeleted, w => w.isFalse()),
+      (
+        j.andWhere(e => e.isDeleted, w => w.isFalse()),
+        j.andWhere(e => e.isManual, w => w.isFalse())
+      ),
     );
     q.andWhere(e => e.isDeleted, w => w.isFalse());
-    q.andWhere(e => e.bagItemAwb.bagItem.bag.isManual, w => w.isFalse());
 
     const data = await q.exec();
     const total = await q.countWithoutTakeAndSkip();
