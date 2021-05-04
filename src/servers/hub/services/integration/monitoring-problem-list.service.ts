ca533d14d4bc22ebbd803e01e027bb62d218ccfe
mniudanri <niudanri12@gmail.com>
2021-05-04 12:11:42 +0700

    fix error

--- src/servers/hub/services/integration/monitoring-problem-list.service.ts
+++ src/servers/hub/services/integration/monitoring-problem-list.service.ts
@@ -17,16 +17,17 @@ export class MonitoringProblemListService {
   ): Promise<MonitoringHubProblemVm> {
     const statusProblemStr = (await this.getListStatusAwbProblem()).join(',');
 
-    payload.fieldResolverMap['createdTime'] = 'dohd.created_time';
-    payload.fieldResolverMap['scanDate'] = 'dohd.created_time';
-    payload.fieldResolverMap['branchIdFrom'] = 'doh.branch_id';
+    payload.fieldResolverMap['createdTime'] = 'doh.created_time';
+    payload.fieldResolverMap['scanDate'] = 'doh.created_time';
+    payload.fieldResolverMap['branchIdFrom'] = 'bag_sortir.branch_id';
+    payload.fieldResolverMap['branchNameFrom'] = 'bag_sortir.branch_name';
     payload.fieldResolverMap['branchIdTo'] = 'scan_out.branch_id';
     payload.fieldResolverMap['branchTo'] = 'scan_out.branch_name';
     payload.fieldResolverMap['awbNumber'] = 'dohd.awb_number';
     payload.fieldResolverMap['bagNumber'] = 'doh.bag_number';
     payload.fieldResolverMap['bagSortir'] = 'bag_sortir.bag_number';
     payload.fieldResolverMap['bagSeqSortir'] = 'bag_sortir.bag_seq';
-    payload.fieldResolverMap['cityId'] = 'c.city_id';
+    payload.fieldResolverMap['cityId'] = 'bag_sortir.city_id';
 
     payload.globalSearchFields = [
       {
@@ -83,22 +84,29 @@ export class MonitoringProblemListService {
       `
         br.branch_id = doh.branch_id AND br.is_deleted = FALSE
         INNER JOIN bag bag ON bag.bag_id = doh.bag_id AND bag.is_deleted = FALSE AND bag.branch_id IS NOT NULL AND (is_sortir IS NULL OR is_sortir = FALSE)
-        INNER JOIN district d ON d.district_id = br.district_id AND d.is_deleted = FALSE
-        INNER JOIN city c ON c.city_id = d.city_id AND c.is_deleted = FALSE
         INNER JOIN bag_item bi ON bi.bag_item_id = doh.bag_item_id AND bi.is_deleted = FALSE
         INNER JOIN bag_item_awb bia ON bia.bag_item_id = bi.bag_item_id AND bia.is_deleted = FALSE
+        INNER JOIN awb_item ai ON ai.awb_item_id = bia.awb_item_id AND ai.is_deleted = FALSE
         INNER JOIN dropoff_hub_detail dohd ON dohd.dropoff_hub_id = doh.dropoff_hub_id AND dohd.is_deleted = FALSE
         LEFT JOIN LATERAL
         (
           SELECT
             bi1.bag_seq,
             ai1.awb_id,
-            b1.bag_number
+            b1.bag_number,
+            bi1.created_time,
+            c1.city_name,
+            c1.city_id,
+            br1.branch_name,
+            br1.branch_code,
+            br1.branch_id
           FROM bag_item_awb bia1
-          INNER JOIN awb_item ai1 ON ai1.awb_item_id = bia1.awb_item_id AND ai1.is_deleted = FALSE AND dohd.awb_id = ai1.awb_id
+          INNER JOIN awb_item ai1 ON ai1.awb_item_id = bia1.awb_item_id AND ai1.is_deleted = FALSE AND ai.awb_id = ai1.awb_id
           INNER JOIN bag_item bi1 ON bi1.bag_item_id = bia1.bag_item_id AND bi1.is_deleted = FALSE
           INNER JOIN bag b1 ON b1.bag_id = bi1.bag_id AND b1.is_deleted = FALSE AND b1.branch_id_to IS NOT NULL
-          INNER JOIN users u1 ON u1.user_id = bi1.user_id_created AND u1.is_deleted = FALSE
+          INNER JOIN branch br1 ON br1.branch_id = b1.branch_id_to AND br1.is_deleted = FALSE
+          INNER JOIN district d1 ON d1.district_id = br1.district_id AND d1.is_deleted = FALSE
+          INNER JOIN city c1 ON c1.city_id = d1.city_id AND c1.is_deleted = FALSE
           WHERE bia1.is_deleted = FALSE
         ) AS bag_sortir ON true
         LEFT JOIN LATERAL (
@@ -130,12 +138,13 @@ export class MonitoringProblemListService {
     `);
     q.andWhere(e => e.isDeleted, w => w.isFalse());
     q.andWhereRaw('doh.branch_id IS NOT NULL');
+    q.andWhereRaw('bag.branch_id IS NOT NULL AND (bag.is_sortir IS NULL OR bag.is_sortir = FALSE)');
     q.groupByRaw(`
       dohd.awb_number,
       bag_sortir.bag_number,
       bag_sortir.bag_seq,
       doh.bag_number,
-      dohd.created_time,
+      doh.created_time,
       -- br.branch_name,
       bag_sortir.awb_id,
       scan_out.awb_id,
@@ -145,7 +154,7 @@ export class MonitoringProblemListService {
       bag.is_manual,
       last_status.awb_status_name,
       last_status.awb_status_id,
-      c.city_id
+      bag_sortir.city_id
     `);
 
     const data = await q.exec();
@@ -166,8 +175,8 @@ export class MonitoringProblemListService {
 
     payload.fieldResolverMap['createdTime'] = '"doh"."created_time"::DATE';
     payload.fieldResolverMap['scanDate'] = 'doh.created_time';
-    payload.fieldResolverMap['branchIdFrom'] = 'doh.branch_id';
-    payload.fieldResolverMap['branchNameFrom'] = 'doh.branch_name';
+    payload.fieldResolverMap['branchIdFrom'] = 'bag_sortir.branch_id';
+    payload.fieldResolverMap['branchNameFrom'] = 'bag_sortir.branch_name';
     payload.fieldResolverMap['branchIdTo'] = 'scan_out.branch_id';
     payload.fieldResolverMap['branchNameTo'] = 'scan_out.branch_name';
     payload.fieldResolverMap['awbNumber'] = 'dohd.awb_number';
@@ -306,15 +315,15 @@ export class MonitoringProblemListService {
 
     payload.fieldResolverMap['createdTime'] = '"doh"."created_time"::DATE';
     payload.fieldResolverMap['scanDate'] = 'doh.created_time';
-    payload.fieldResolverMap['branchIdFrom'] = 'doh.branch_id';
-    payload.fieldResolverMap['branchNameFrom'] = 'doh.branch_name';
+    payload.fieldResolverMap['branchIdFrom'] = 'bag_sortir.branch_id';
+    payload.fieldResolverMap['branchNameFrom'] = 'bag_sortir.branch_name';
     payload.fieldResolverMap['branchIdTo'] = 'scan_out.branch_id';
     payload.fieldResolverMap['branchNameTo'] = 'scan_out.branch_name';
     payload.fieldResolverMap['awbNumber'] = 'dohd.awb_number';
     payload.fieldResolverMap['bagNumber'] = 'doh.bag_number';
     payload.fieldResolverMap['bagSortir'] = 'bag_sortir.bag_number';
     payload.fieldResolverMap['bagSeqSortir'] = 'bag_sortir.bag_seq';
-    payload.fieldResolverMap['cityId'] = 'c.city_id';
+    payload.fieldResolverMap['cityId'] = 'bag_sortir.city_id';
 
     payload.globalSearchFields = [
       {
@@ -335,7 +344,10 @@ export class MonitoringProblemListService {
             THEN CONCAT(bag_sortir.bag_number, LPAD(bag_sortir.bag_seq::text, 3, '0'))
           ELSE doh.bag_number
         END`, 'bagNumber'],
-      ['\'Yes\'', 'do'],
+      [`CASE WHEN
+          dohd.awb_number IS NULL THEN 'Yes'
+          ELSE 'No'
+        END`, 'do'],
       [`CASE WHEN bag_sortir.awb_id IS NOT NULL THEN 'Yes' ELSE 'No' END`, 'in'],
       [`CASE WHEN scan_out.awb_id IS NOT NULL THEN 'Yes' ELSE 'No' END`, 'out'],
       [`last_status.awb_status_name`, 'awbStatusName'],
@@ -397,7 +409,7 @@ export class MonitoringProblemListService {
         ) AS last_status ON true
     `);
     q.andWhere(e => e.isDeleted, w => w.isFalse());
-    q.andWhereRaw('bag.branch_id IS NOT NULL AND (bag.is_sortir IS NULL OR bag.is_sortir = FALSE)');
+    q.andWhereRaw('bag.branch_id IS NOT NULL AND (bag.is_sortir IS NULL OR bag.is_sortir = FALSE) AND dohd.awb_number IS NULL');
 
     q.groupByRaw(`
       bag_sortir.branch_name,
