787f63aa747c28d0eee75f74203ff8b2046ca6f5
mniudanri <niudanri12@gmail.com>
2021-05-21 16:58:31 +0700

    fix query detail lebih sortir

--- src/servers/hub/services/integration/monitoring-problem-lebih-sortir-list.service.ts
+++ src/servers/hub/services/integration/monitoring-problem-lebih-sortir-list.service.ts
@@ -9,177 +9,6 @@ import { MonitoringProblemListService } from './monitoring-problem-list.service'
 
 @Injectable()
 export class MonitoringProblemLebihSortirListService {
-  static async getLebihSortirDetail(
-    payload: BaseMetaPayloadVm,
-    isManual = null,
-    isProblem = null,
-    isScanOut = null,
-  ): Promise<MonitoringHubProblemVm> {
-    const statusProblemStr = (await MonitoringProblemListService.getListStatusAwbProblem()).join(',');
-
-    payload.fieldResolverMap['createdTime'] = 'doh.created_time';
-    payload.fieldResolverMap['scanDate'] = 'doh.created_time';
-    payload.fieldResolverMap['branchIdFrom'] = 'br.branch_id';
-    payload.fieldResolverMap['branchNameFrom'] = 'br.branch_name';
-    payload.fieldResolverMap['branchIdTo'] = 'scan_out.branch_id';
-    payload.fieldResolverMap['branchTo'] = 'scan_out.branch_name';
-    payload.fieldResolverMap['awbNumber'] = 'dohd.awb_number';
-    payload.fieldResolverMap['bagNumber'] = 'doh.bag_number';
-    payload.fieldResolverMap['bagSortir'] = 'bag_sortir.bag_number';
-    payload.fieldResolverMap['bagSeqSortir'] = 'bag_sortir.bag_seq';
-    payload.fieldResolverMap['cityId'] = 'c.city_id';
-
-    payload.globalSearchFields = [
-      {
-        field: 'scan_out.branch_name',
-      },
-    ];
-
-    if (!payload.sortBy) {
-      payload.sortBy = 'createdTime';
-    }
-
-    payload = MonitoringProblemListService.formatPayloadFiltersAwbProblem(payload);
-
-    const repo = new OrionRepositoryService(Bag, 'doh');
-    const q = repo.findAllRaw();
-
-    payload.applyToOrionRepositoryQuery(q);
-    q.selectRaw(
-      [`doh.created_time`, 'scanDate'],
-      [`bag_sortir.created_time`, 'scanDateInHub'],
-      [`dohd.awb_number`, 'awbNumber'],
-      [`CASE
-          WHEN bag_sortir.bag_number IS NOT NULL AND scan_out.awb_id IS NOT NULL AND last_status.awb_status_id NOT IN (${statusProblemStr})
-            THEN CONCAT(bag_sortir.bag_number, LPAD(bag_sortir.bag_seq::text, 3, '0'))
-          ELSE doh.bag_number
-        END`, 'bagNumber'],
-      ['\'Yes\'::text', 'do'],
-      [`CASE WHEN bag_sortir.bag_number IS NOT NULL THEN 'Yes' ELSE 'No' END`, 'in'],
-      [`CASE WHEN scan_out.awb_id IS NOT NULL THEN 'Yes' ELSE 'No' END`, 'out'],
-      [`last_status.awb_status_name`, 'awbStatusName'],
-    );
-
-    // ignore filter isManual = null
-    if (isManual === false) {
-      q.andWhereRaw(`bag.is_manual = FALSE
-      AND bag_sortir.awb_id IS NOT NULL
-      AND scan_out.awb_id IS NOT NULL
-      AND last_status.awb_status_id NOT IN (${statusProblemStr})`);
-    } else if (isManual === true) {
-      q.andWhereRaw(`bag.is_manual = TRUE
-      AND bag_sortir.awb_id IS NOT NULL
-      AND scan_out.awb_id IS NOT NULL
-      AND last_status.awb_status_id NOT IN (${statusProblemStr})`);
-    }
-
-    // ignore filter isProblem = null or isProblem = false
-    if (isProblem === true) {
-      q.andWhereIsolated(qw => {
-        qw.whereRaw(`bag_sortir.awb_id IS NULL
-        OR scan_out.awb_id IS NULL
-        OR last_status.awb_status_id IN (${statusProblemStr})`);
-      });
-    }
-
-    // ignore isScanOut = null
-    if (isScanOut === false) { // NOT SCAN OUT
-      q.andWhereRaw(`scan_out.awb_id IS NULL`);
-    } else if (isScanOut === true) {
-      q.andWhereRaw(`scan_out.awb_id IS NOT NULL`);
-    }
-
-    q.innerJoinRaw(
-      'bag_item',
-      'bi',
-      `
-        bi.bag_id = bag.bag_id AND bi.is_deleted = FALSE
-        LEFT JOIN dropoff_hub doh ON doh.bag_id = bag.bag_id AND doh.is_deleted = FALSE and doh.branch_id IS NOT NULL
-        LEFT JOIN dropoff_hub_detail dohd ON dohd.dropoff_hub_id = doh.dropoff_hub_id AND dohd.is_deleted = FALSE
-        INNER JOIN LATERAL (
-          SELECT
-            *
-          FROM
-            bag_item_awb bia00
-          WHERE bia00.bag_item_id = bi.bag_item_id
-            AND bia00.is_deleted = false
-          ORDER BY
-            bia00.bag_item_awb_id ASC
-          LIMIT 1
-        ) AS bia ON TRUE
-        INNER JOIN awb_item ai ON ai.awb_item_id = bia.awb_item_id AND ai.is_deleted = FALSE
-        LEFT JOIN branch br ON br.branch_id = dohd.branch_id AND br.is_deleted = FALSE
-        LEFT JOIN district d ON d.district_id = br.district_id AND d.is_deleted = FALSE
-        LEFT JOIN city c ON c.city_id = d.city_id AND c.is_deleted = FALSE
-        LEFT JOIN LATERAL
-        (
-          SELECT
-            bi1.bag_seq,
-            ai1.awb_id,
-            b1.bag_number,
-            bi1.created_time
-          FROM bag_item_awb bia1
-          INNER JOIN awb_item ai1 ON ai1.awb_item_id = bia1.awb_item_id AND ai1.is_deleted = FALSE AND dohd.awb_id = ai1.awb_id
-          INNER JOIN bag_item bi1 ON bi1.bag_item_id = bia1.bag_item_id AND bi1.is_deleted = FALSE
-          INNER JOIN bag b1 ON b1.bag_id = bi1.bag_id AND b1.is_deleted = FALSE AND b1.branch_id_to IS NOT NULL
-          WHERE bia1.is_deleted = FALSE
-        ) AS bag_sortir ON true
-        INNER JOIN LATERAL (
-          SELECT
-            ai2.awb_id,
-            dpdb2.bag_number,
-            br2.branch_id,
-            dpdb2.created_time
-          FROM do_pod dp2
-          INNER JOIN do_pod_detail_bag dpdb2 ON dpdb2.do_pod_id = dp2.do_pod_id AND dpdb2.is_deleted = FALSE
-          INNER JOIN branch br2 ON br2.branch_id = dp2.branch_id_to AND br2.is_deleted = FALSE
-          INNER JOIN bag_item_awb bia2 ON bia2.bag_item_id = dpdb2.bag_item_id AND bia2.is_deleted = FALSE
-          INNER JOIN awb_item ai2 ON ai2.awb_item_id = bia2.awb_item_id AND ai2.is_deleted = FALSE AND dohd.awb_id = ai2.awb_id
-          INNER JOIN users u2 ON u2.user_id = dpdb2.user_id_created AND u2.is_deleted = FALSE
-          WHERE
-          dp2.is_deleted = FALSE
-          AND dp2.do_pod_type = ${POD_TYPE.OUT_HUB}
-          AND dp2.user_id_driver IS NOT NULL AND dp2.branch_id_to IS NOT NULL
-        ) AS scan_out ON true
-        INNER JOIN LATERAL (
-          SELECT
-            ah3.awb_status_id
-          FROM
-            awb_history ah3
-          INNER JOIN bag_item_awb bia3 ON bia3.awb_item_id = ah3.awb_item_id AND bia3.awb_number = dohd.awb_number
-            AND bia3.is_deleted = FALSE
-          ORDER BY
-            ah3.history_date DESC
-          LIMIT 1
-        ) AS last_status ON TRUE
-    `);
-    q.andWhere(e => e.isDeleted, w => w.isFalse());
-    q.andWhereRaw('doh.branch_id IS NOT NULL');
-    q.andWhereRaw('bag.branch_id IS NOT NULL AND (bag.is_sortir IS NULL OR bag.is_sortir = FALSE)');
-    q.groupByRaw(`
-      dohd.awb_number,
-      bag_sortir.bag_number,
-      bag_sortir.bag_seq,
-      doh.bag_number,
-      doh.created_time,
-      bag_sortir.created_time,
-      -- br.branch_name,
-      scan_out.awb_id,
-      last_status.awb_status_name,
-      last_status.awb_status_id,
-      c.city_id
-    `);
-
-    const data = await q.exec();
-    const total = await q.countWithoutTakeAndSkip();
-
-    const result = new MonitoringHubProblemVm();
-
-    result.data = data;
-    result.paging = MetaService.set(payload.page, payload.limit, total);
-
-    return result;
-  }
 
   static async getLebihSortir(
     payload: BaseMetaPayloadVm,
@@ -251,13 +80,13 @@ export class MonitoringProblemLebihSortirListService {
           INNER JOIN bag_item bi1 ON bi1.bag_item_id = bia1.bag_item_id AND bi1.is_deleted = FALSE
           INNER JOIN bag b1 ON b1.bag_id = bi1.bag_id AND b1.is_deleted = FALSE
           -- AND b1.branch_id_to IS NOT NULL
-          LEFT JOIN branch br1 ON br1.branch_id = b1.branch_id AND br1.is_deleted = FALSE
-          LEFT JOIN district d1 ON d1.district_id = br1.district_id AND d1.is_deleted = FALSE
-          LEFT JOIN city c1 ON c1.city_id = d1.city_id AND c1.is_deleted = FALSE
+          INNER JOIN branch br1 ON br1.branch_id = b1.branch_id AND br1.is_deleted = FALSE
+          INNER JOIN district d1 ON d1.district_id = br1.district_id AND d1.is_deleted = FALSE
+          INNER JOIN city c1 ON c1.city_id = d1.city_id AND c1.is_deleted = FALSE
           WHERE bia1.is_deleted = FALSE
             AND bia1.awb_number = bia.awb_number
         ) AS bag_sortir ON TRUE
-        JOIN LATERAL (
+        LEFT JOIN LATERAL (
          	SELECT
          		ai2.awb_id,
          		dpdb2.bag_number,
@@ -272,7 +101,7 @@ export class MonitoringProblemLebihSortirListService {
          		AND dp2.user_id_driver IS NOT NULL
          		AND dp2.branch_id_to IS NOT NULL
         ) AS scan_out ON TRUE
-        INNER JOIN LATERAL (
+        LEFT JOIN LATERAL (
           SELECT
             ah3.awb_status_id,
             as3.awb_status_name
@@ -290,7 +119,6 @@ export class MonitoringProblemLebihSortirListService {
     q.andWhere(e => e.isDeleted, w => w.isFalse());
     q.andWhereRaw(`
       dohd.awb_number is null
-      AND bag_sortir.branch_id is not null
     `);
     q.groupByRaw(`
       bag_sortir.awb_number,
@@ -349,12 +177,7 @@ export class MonitoringProblemLebihSortirListService {
       [`bag_sortir.branch_code`, 'branchCode'],
       [`bag_sortir.branch_id`, 'branchId'],
       [`COUNT (
-          DISTINCT CASE
-          WHEN (
-            dohd.awb_number IS NULL
-          ) THEN
-            ai.awb_id
-          END
+          DISTINCT ai.awb_id
         )`, 'lebihSortir',
       ],
     );
@@ -392,9 +215,9 @@ export class MonitoringProblemLebihSortirListService {
         INNER JOIN bag_item bi1 ON bi1.bag_item_id = bia1.bag_item_id AND bi1.is_deleted = FALSE
         INNER JOIN bag b1 ON b1.bag_id = bi1.bag_id AND b1.is_deleted = FALSE
         -- AND b1.branch_id_to IS NOT NULL
-        LEFT JOIN branch br1 ON br1.branch_id = b1.branch_id AND br1.is_deleted = FALSE
-        LEFT JOIN district d1 ON d1.district_id = br1.district_id AND d1.is_deleted = FALSE
-        LEFT JOIN city c1 ON c1.city_id = d1.city_id AND c1.is_deleted = FALSE
+        INNER JOIN branch br1 ON br1.branch_id = b1.branch_id AND br1.is_deleted = FALSE
+        INNER JOIN district d1 ON d1.district_id = br1.district_id AND d1.is_deleted = FALSE
+        INNER JOIN city c1 ON c1.city_id = d1.city_id AND c1.is_deleted = FALSE
         WHERE bia1.is_deleted = FALSE
           AND bia1.awb_number = bia.awb_number
       ) AS bag_sortir ON TRUE
@@ -403,7 +226,6 @@ export class MonitoringProblemLebihSortirListService {
     q.andWhere(e => e.isDeleted, w => w.isFalse());
     q.andWhereRaw(`
       dohd.awb_number is null
-      AND bag_sortir.branch_id is not null
     `);
 
     q.groupByRaw(`
