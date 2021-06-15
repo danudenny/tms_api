ccf397bf6d7c8bcb05ecae8c4a1c1198c90c132b
oeikurniawan <kurniawan.sicepat@gmail.com>
2021-06-15 21:24:26 +0700

    Revert "Merge branch 'rey-dev' into development"
    This reverts commit 21e406425413cb7adf1a81cb71451cc4325e4b4c, reversing
    changes made to 0ad5dce7adef08ea06d903b3c7dcbc45ce438c6d.

--- src/servers/hub/controllers/integration/hub-machine-package.controller.ts
+++ src/servers/hub/controllers/integration/hub-machine-package.controller.ts
@@ -9,8 +9,6 @@ import { HubMachineService } from '../../services/integration/hub-machine-packag
 import { PackageMachinePayloadVm } from '../../models/hub-gabungan-mesin-payload.vm';
 import { Transactional } from '../../../../shared/external/typeorm-transactional-cls-hooked/Transactional';
 import { getConnection, Transaction } from 'typeorm';
-import * as hash from 'object-hash';
-import { RedisService } from 'src/shared/services/redis.service';
 
 @ApiUseTags('Hub Mesin Sortir Resi Bag')
 @Controller('hub/sortir/combine')
@@ -23,18 +21,6 @@ export class HubMachinePackageController {
   @UseGuards(AuthXAPIKeyGuard)
   @ApiOkResponse({ type: MachinePackageResponseVm })
   public async checkSpk(@Body() payload: PackageMachinePayloadVm) {
-    const hashObj = {
-      sorting_branch_id: payload.sorting_branch_id,
-      reference_numbers: payload.reference_numbers,
-      tag_seal_number: payload.tag_seal_number,
-      chute_number: payload.chute_number,
-    };
-
-    const h = hash(hashObj);
-    const cacheKey = `cache:sorting-machine:push-payload:${h}`;
-    let data = await RedisService.get(cacheKey, true);
-    if (data) return data;
-
-    return await HubMachineService.processMachineBagging(payload, cacheKey);
+    return await HubMachineService.processMachineBagging(payload);
   }
 }
