9e505291f46db8f5fe218085c8d1d23c4836f314
oeikurniawan <kurniawan.sicepat@gmail.com>
2021-06-16 14:25:08 +0700

    bagItemAwb is Machine

--- src/servers/hub/services/integration/hub-machine-package.services.ts
+++ src/servers/hub/services/integration/hub-machine-package.services.ts
@@ -400,6 +400,7 @@ export class HubMachineService {
         updatedTime: batchData.timestamp,
         userIdUpdated: batchData.userId,
         isSortir: true,
+        isMachine: true,
       });
     });
 
