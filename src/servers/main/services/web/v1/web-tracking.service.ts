f00bf4953a8b1e4ac91f851a9392a2a1d0fa05e1
niudanrim <mohammad.niudanri@e.sicepat.com>
2021-06-30 14:47:17 +0700

    modify response weight - Tracking GSK Detail

--- src/servers/main/services/web/v1/web-tracking.service.ts
+++ src/servers/main/services/web/v1/web-tracking.service.ts
@@ -421,8 +421,8 @@ export class V1WebTrackingService {
     const query = `
       SELECT
         bri.ref_awb_number AS "awbNumber",
-        awb.total_weight_final_rounded AS "totalWeightFinalRounded",
-        awb.total_weight_final AS "totalWeightFinal",
+        awb.total_weight_final_rounded::numeric(10, 2) AS "totalWeightFinalRounded",
+        awb.total_weight::numeric(10, 2) AS "totalWeightFinal",
         pt.package_type_code AS "packageTypeCode"
       FROM bag_representative_item bri
       INNER JOIN awb ON awb.awb_id = bri.awb_id AND awb.is_deleted = FALSE
