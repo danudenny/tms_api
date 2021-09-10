bc3f4ac5dd79c66a1eebb4ecc8acf637d428cac9
Maulana Syarif Hidayatulloh <maulana.syarif@sicepat.com>
2021-09-10 08:07:46 +0700

    [POD-63] - fix sort berat pada daftar scan masuk gerai (LM)

--- src/servers/main/services/web/web-delivery-in.service.ts
+++ src/servers/main/services/web/web-delivery-in.service.ts
@@ -388,7 +388,7 @@ export class WebDeliveryInService {
     payload.fieldResolverMap['totalCodValue'] = 't3.total_cod_value';
     payload.fieldResolverMap['branchId'] = 't3.branch_id';
     payload.fieldResolverMap['branchName'] = 't4.branch_name';
-    payload.fieldResolverMap['totalWeightFinal'] = 't3.total_weight_final';
+    payload.fieldResolverMap['totalWeightFinal'] = 't3.total_weight';
     payload.fieldResolverMap['podScanInBranchId'] = 't1.pod_scan_in_branch_id';
     if (payload.sortBy === '') {
       payload.sortBy = 'createdTime';
