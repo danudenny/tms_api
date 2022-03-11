f3fb482768e06cc24d046a262128e7932e43e16e
Louis Leslie <louis.leslie@sicepat.com>
2022-03-11 14:29:54 +0700

    Fix join and select column names in sortation scanout list

--- src/servers/hub/services/sortation/web/sortation-scanout-list.service.ts
+++ src/servers/hub/services/sortation/web/sortation-scanout-list.service.ts
@@ -16,35 +16,35 @@ export class SortationScanOutListService {
 
     // SELECT CLAUSE
     const selectColumns = [
-      'do_sortation.do_sortation_id AS doSortationId',
-      'do_sortation.do_sortation_code AS doSortationCode',
-      'do_sortation.do_sortation_time AS doSortationTime',
-      'e.fullname AS fullName',
-      'e.employee_id AS employeeId',
-      'dsv.vehicle_number AS vehicleNumber',
-      'br.branch_name AS branchFromName',
-      'do_sortation.branch_name_to_list AS branchToName',
-      'do_sortation.total_bag AS totalBag',
-      'do_sortation.total_bag_sortir AS totalbagSortation',
-      'dss.do_sortation_status_title AS doSortationStatusTitle',
-      'e.nickname AS nickName',
-      'e.nik AS nik',
-      'do_sortation.arrival_date_time AS arrivalDateTime',
-      'do_sortation.departure_date_time AS departureDateTime',
+      'do_sortation.do_sortation_id AS "doSortationId"',
+      'do_sortation.do_sortation_code AS "doSortationCode"',
+      'do_sortation.do_sortation_time AS "doSortationTime"',
+      'e.fullname AS "fullName"',
+      'e.employee_id AS "employeeId"',
+      'dsv.vehicle_number AS "vehicleNumber"',
+      'br.branch_name AS "branchFromName"',
+      'do_sortation.branch_name_to_list AS "branchToName"',
+      'do_sortation.total_bag AS "totalBag"',
+      'do_sortation.total_bag_sortir AS "totalbagSortation"',
+      'dss.do_sortation_status_title AS "doSortationStatusTitle"',
+      'e.nickname AS "nickName"',
+      'e.nik AS "nik"',
+      'do_sortation.arrival_date_time AS "arrivalDateTime"',
+      'do_sortation.departure_date_time AS "departureDateTime"',
     ];
     q.select(selectColumns)
       .innerJoin('do_sortation.branchFrom', 'br', 'br.is_deleted = FALSE')
-      .innerJoin(
-        'do_sortation.doSortationVehicle',
-        'dsv',
-        'dsv.is_deleted = FALSE',
-      )
       .innerJoin(
         'do_sortation.doSortationStatus',
         'dss',
         'dss.is_deleted = FALSE',
       )
-      .innerJoin('dsv.employee', 'e', 'e.is_deleted = FALSE');
+      .leftJoin(
+        'do_sortation.doSortationVehicle',
+        'dsv',
+        'dsv.is_deleted = FALSE',
+      )
+      .leftJoin('dsv.employee', 'e', 'e.is_deleted = FALSE');
 
     // WHERE CLAUSE
     let where;
