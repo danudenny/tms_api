0f4dca95a1ec0223b8d7994d2e99f9e6e61e1fb5
Louis Leslie <louis.leslie@sicepat.com>
2022-03-17 09:17:10 +0700

    Change getScanOutSortationList implementation with orion repository

--- src/servers/hub/services/sortation/web/sortation-scanout-list.service.ts
+++ src/servers/hub/services/sortation/web/sortation-scanout-list.service.ts
@@ -15,95 +15,82 @@ import {
 } from '../../../models/sortation/web/sortation-scanout-list.payload.vm';
 import { createQueryBuilder, SelectQueryBuilder } from 'typeorm';
 import { DoSortationDetailItem } from '../../../../../shared/orm-entity/do-sortation-detail-item';
+import { OrionRepositoryService } from 'src/shared/services/orion-repository.service';
+import { DoSortation } from 'src/shared/orm-entity/do-sortation';
 
 @Injectable()
 export class SortationScanOutListService {
-  static async getScanOutSortationList(
+  public static async getScanOutSortationList(
     payload: BaseMetaPayloadVm,
   ): Promise<ScanOutSortationListResponseVm> {
-    const q = RepositoryService.doSortation.createQueryBuilder();
-
-    // SELECT CLAUSE
-    const selectColumns = [
-      'do_sortation.do_sortation_id AS "doSortationId"',
-      'do_sortation.do_sortation_code AS "doSortationCode"',
-      'do_sortation.do_sortation_time AS "doSortationTime"',
-      'e.fullname AS "fullName"',
-      'e.employee_id AS "employeeId"',
-      'dsv.vehicle_number AS "vehicleNumber"',
-      'br.branch_name AS "branchFromName"',
-      'do_sortation.branch_name_to_list AS "branchToName"',
-      'do_sortation.total_bag AS "totalBag"',
-      'do_sortation.total_bag_sortir AS "totalbagSortation"',
-      'dss.do_sortation_status_title AS "doSortationStatusTitle"',
-      'e.nickname AS "nickName"',
-      'e.nik AS "nik"',
-      'do_sortation.arrival_date_time AS "arrivalDateTime"',
-      'do_sortation.departure_date_time AS "departureDateTime"',
-    ];
-    q.select(selectColumns)
-      .innerJoin('do_sortation.branchFrom', 'br', 'br.is_deleted = FALSE')
-      .innerJoin(
-        'do_sortation.doSortationStatus',
-        'dss',
-        'dss.is_deleted = FALSE',
-      )
-      .leftJoin(
-        'do_sortation.doSortationVehicle',
-        'dsv',
-        'dsv.is_deleted = FALSE',
-      )
-      .leftJoin('dsv.employee', 'e', 'e.is_deleted = FALSE');
-
-    // WHERE CLAUSE
-    let where;
-    if (payload.filters) {
-      payload.filters.forEach(filter => {
-        const field = ['endDate', 'startDate'].includes(filter.field)
-          ? 'do_sortation_time'
-          : filter.field;
-        if (field == 'branchIdTo') {
-          // handles where clause for column type varchar[]
-          where = `'${filter.value}' = ANY(do_sortation.branch_id_to_list)`;
-        } else {
-          where = `do_sortation.${field} ${filter.sqlOperator} :${
-            filter.field
-          }`;
-        }
-        const whereParams = { [filter.field]: filter.value };
-        q.andWhere(where, whereParams);
-      });
-    }
-    if (payload.search) {
-      q.andWhere(`do_sortation.do_sortation_code LIKE :search`, {
-        search: `%${payload.search.toUpperCase()}%`,
-      });
-    }
-    q.andWhere('do_sortation.is_deleted = FALSE');
-
-    // ORDER & PAGINATION
-    const sortMap = {
-      createdTime: 'do_sortation.created_time',
-      doSortationTime: 'do_sortation.do_sortation_time',
-      departureDateTime: 'do_sortation.departure_date_time',
-      arrivalDateTime: 'do_sortation.arrival_date_time',
-      totalBag: 'do_sortation.total_bag',
-      totalBagSortation: 'do_sortation.total_bag_sortir',
+    payload.fieldResolverMap = {
+      createdTime: 'ds.created_time',
+      doSortationTime: 'ds.do_sortation_time',
+      doSortationCode: 'ds.do_sortation_code',
+      departureDateTime: 'ds.departure_date_time',
+      arrivalDateTime: 'ds.arrival_date_time',
+      totalBag: 'ds.total_bag',
+      totalBagSortation: 'ds.total_bag_sortir',
+      branchIdFrom: 'ds.branch_id_from',
+      branchIdTo: 'ds.branch_id_to',
       doSortationStatusTitle: 'dss.do_sortation_status_title',
       vehicleNumber: 'dsv.vehicle_number',
       nickName: 'e.nickname',
     };
-    const orderBy: string = sortMap[payload.sortBy] || sortMap.doSortationTime;
-    const orderDir: 'ASC' | 'DESC' = payload.sortDir === 'asc' ? 'ASC' : 'DESC';
-    const limit = payload.limit || 10;
-    const page = payload.page || 1;
-    q.orderBy(orderBy, orderDir)
-      .limit(limit)
-      .offset((page - 1) * limit);
+    payload.sortBy = payload.sortBy || 'createdTime';
+    const searchFields = ['doSortationCode'];
+    payload.globalSearchFields = searchFields.map(field => ({ field }));
+
+    // handle 'ANY' operator in WHERE clause separately for VARCHAR[] column type
+    const isBranchIdTo = field =>
+      ['branchIdTo', 'branch_id_to', 'branchToName'].includes(field);
+    const branchIdToFilter = payload.filters.filter(f => isBranchIdTo(f.field));
+    payload.filters = payload.filters.filter(f => !isBranchIdTo(f.field));
+
+    const repo = new OrionRepositoryService(DoSortation, 'ds');
+    const q = repo.findAllRaw();
+    payload.applyToOrionRepositoryQuery(q, true);
+
+    const selectColumns = [
+      ['ds.do_sortation_id', 'doSortationId'],
+      ['ds.do_sortation_code', 'doSortationCode'],
+      ['ds.do_sortation_time', 'doSortationTime'],
+      ['e.fullname', 'fullName'],
+      ['e.employee_id', 'employeeId'],
+      ['dsv.vehicle_number', 'vehicleNumber'],
+      ['bf.branch_name', 'branchFromName'],
+      ['ds.branch_name_to_list', 'branchToName'],
+      ['ds.total_bag', 'totalBag'],
+      ['ds.total_bag_sortir', 'totalbagSortation'],
+      ['dss.do_sortation_status_title', 'doSortationStatusTitle'],
+      ['e.nickname', 'nickName'],
+      ['e.nik', 'nik'],
+      ['ds.arrival_date_time', 'arrivalDateTime'],
+      ['ds.departure_date_time', 'departureDateTime'],
+    ];
+    q.selectRaw(...selectColumns)
+      .innerJoin(e => e.branchFrom, 'bf', j =>
+        j.andWhere(e => e.isDeleted, w => w.isFalse()),
+      )
+      .innerJoin(e => e.doSortationStatus, 'dss', j =>
+        j.andWhere(e => e.isDeleted, w => w.isFalse()),
+      )
+      .leftJoin(e => e.doSortationVehicle, 'dsv', j =>
+        j.andWhere(e => e.isDeleted, w => w.isFalse()),
+      )
+      .leftJoin(e => e.doSortationVehicle.employee, 'e', j =>
+        j.andWhere(e => e.isDeleted, w => w.isFalse()),
+      );
+
+    if (branchIdToFilter.length > 0) {
+      q.andWhereRaw(
+        `'${branchIdToFilter[0].value}' = ANY(ds.branch_id_to_list)`,
+      );
+    }
 
     const [scanOutSortations, count] = await Promise.all([
-      q.getRawMany(),
-      q.getCount(),
+      q.exec(),
+      q.countWithoutTakeAndSkip(),
     ]);
 
     const result = new ScanOutSortationListResponseVm();
@@ -115,7 +102,6 @@ export class SortationScanOutListService {
 
     return result;
   }
-
   static async getScanOutSortationRouteDetail(
     payload: ScanOutSortationRouteDetailPayloadVm,
   ): Promise<ScanOutSortationRouteDetailResponseVm> {
