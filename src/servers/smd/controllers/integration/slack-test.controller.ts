3a399efedf928dcc309bafa90e1cf16ac560f843
Noffrialdi <noffrialdi@sicepat.com>
2022-04-14 22:04:13 +0700

    fixing slack api

--- src/servers/smd/controllers/integration/slack-test.controller.ts
+++ src/servers/smd/controllers/integration/slack-test.controller.ts
@@ -1,7 +1,6 @@
 import { Body, Controller, Post, Req } from "@nestjs/common";
 import { ApiUseTags } from "../../../../shared/external/nestjs-swagger";
 import { SlackService } from "nestjs-slack";
-// import { SlackMessage } from "../../../../shared/util/slack-message";
 import { PinoLoggerService } from "../../../../shared/services/pino-logger.service";
 
 @ApiUseTags('SLACK TEST')
