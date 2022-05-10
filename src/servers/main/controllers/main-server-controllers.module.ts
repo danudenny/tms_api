31b724ea59b056c6a04d32b5abbc6476c4517a55
Rio Suga Catra Pratama <riosugacp@gmail.com>
2022-05-10 23:48:54 +0700

    biar ga conflict

--- src/servers/main/controllers/main-server-controllers.module.ts
+++ src/servers/main/controllers/main-server-controllers.module.ts
@@ -52,9 +52,9 @@ import { WebAwbCountController } from './web/web-awb-count.controller';
 import { WebAwbDeliverController } from './web/web-awb-deliver.controller';
 import { WebAwbFilterController } from './web/web-awb-filter.controller';
 import { WebAwbPodController } from './web/web-awb-pod.controller';
+import { WebAwbScanPriorityController } from './web/web-awb-scan-priority.controller';
 import { WebAwbReturnController } from './web/web-awb-return.controller';
 import { WebAwbReturnCancelController } from './web/web-awb-return-cancel.controller';
-import { WebAwbScanPriorityController } from './web/web-awb-scan-priority.controller';
 import { WebAwbUpdateStatusController } from './web/web-awb-update-status.controller';
 import { WebBagPodController } from './web/web-bag-pod.controller';
 import { WebDeliveryInController } from './web/web-delivery-in.controller';
@@ -111,9 +111,9 @@ import { WebAwbCodReportController } from './web/v1/web-awb-cod-report.controlle
     WebAwbUpdateStatusController,
     WebAwbCountController,
     WebFirstMileController,
+    WebAwbScanPriorityController,
     WebAwbReturnController,
     WebAwbReturnCancelController,
-    WebAwbScanPriorityController,
     ProvincesController,
     CitiesController,
     DistrictsController,
