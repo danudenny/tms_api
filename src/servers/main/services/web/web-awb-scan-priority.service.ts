e933f5a66eea3991fb327acdc574d53ee249f5de
Rio Suga Catra Pratama <riosugacp@gmail.com>
2022-05-20 14:47:13 +0700

    add flag to skip validation

--- src/servers/main/services/web/web-awb-scan-priority.service.ts
+++ src/servers/main/services/web/web-awb-scan-priority.service.ts
@@ -3,27 +3,44 @@ import { PriorityServiceApi } from '../../../../shared/services/priority.service
 import { AuthService } from '../../../../shared/services/auth.service';
 import { AwbService } from '../v1/awb.service';
 export class WebAwbScanPriorityService {
-  static async scanProirity(awbNumber : string): Promise<WebAwbScanPriorityResponse>{
+  static async scanProirity(awbNumber: string, isValidated: boolean = false, awbItemId : number  = 0): Promise<WebAwbScanPriorityResponse> {
     //validasi manifest
-    const awb = await AwbService.validAwbNumber(awbNumber);
+
     const result = new WebAwbScanPriorityResponse();
-    if(awb){
-      const permissonPayload = AuthService.getPermissionTokenPayload();
+    const permissonPayload = AuthService.getPermissionTokenPayload();
+
+    if (isValidated == true) {
+      const awb = await AwbService.validAwbNumber(awbNumber);
+      if (awb) {
+        let dataPriority = await PriorityServiceApi.checkPriority(awbNumber, permissonPayload.branchId);
+        result.awbNumber = awbNumber;
+        result.kelurahan = dataPriority.data.kelurahan;
+        if (await AwbService.isManifested(awb.awbNumber, awb.awbItemId)) {
+          result.status = 'ok';
+          result.message = 'success';
+          result.routeAndPriority = dataPriority.data.zone + dataPriority.data.priority;
+        } else {
+          result.status = 'error';
+          result.message = `Resi ${awbNumber} belum pernah di MANIFESTED`;
+          result.routeAndPriority = dataPriority.data.zone;
+        }
+      } else {
+        result.status = 'error';
+        result.message = `Resi ${awbNumber} tidak ditemukan`;
+      }
+    }else{
       let dataPriority = await PriorityServiceApi.checkPriority(awbNumber, permissonPayload.branchId);
       result.awbNumber = awbNumber;
       result.kelurahan = dataPriority.data.kelurahan;
-      if(await AwbService.isManifested(awb.awbNumber, awb.awbItemId)){
+      if (await AwbService.isManifested(awbNumber, awbItemId)) {
         result.status = 'ok';
         result.message = 'success';
-        result.routeAndPriority = dataPriority.data.zone+dataPriority.data.priority;
-      }else{
+        result.routeAndPriority = dataPriority.data.zone + dataPriority.data.priority;
+      } else {
         result.status = 'error';
         result.message = `Resi ${awbNumber} belum pernah di MANIFESTED`;
         result.routeAndPriority = dataPriority.data.zone;
       }
-    }else{
-      result.status = 'error';
-      result.message = `Resi ${awbNumber} tidak ditemukan`;
     }
     return result;
   }
