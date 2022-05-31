9fa230ce2fb3da54ed2aec49289a8f2bf448a65c
Indra Hasan <indra.hasan@sicepat.com>
2022-05-31 09:04:00 +0700

    remove unused file

--- src/servers/hub/models/sortation/mobile/mobile-sortation-handover-image.payload.vm.ts
+++
@@ -1,9 +0,0 @@
-import { ApiModelProperty } from '../../../../../shared/external/nestjs-swagger';
-
-export class MobileSortationHandoverImagePayloadVm {
-  @ApiModelProperty()
-  doSortationId: string;
-
-  @ApiModelProperty()
-  imageType: string;
-}
