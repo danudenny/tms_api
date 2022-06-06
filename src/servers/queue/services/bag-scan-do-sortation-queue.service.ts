f6ecd8e4d26c2ac60529753749a9a279e096327f
Louis Leslie <louis.leslie@sicepat.com>
2022-06-06 11:26:59 +0700

    Fix bag scan do sortation queue to upsert in_hub hub_summary_awb

--- src/servers/queue/services/bag-scan-do-sortation-queue.service.ts
+++ src/servers/queue/services/bag-scan-do-sortation-queue.service.ts
@@ -4,10 +4,11 @@ import { BAG_STATUS } from '../../../shared/constants/bag-status.constant';
 import { BagItem } from '../../../shared/orm-entity/bag-item';
 import { BagItemAwb } from '../../../shared/orm-entity/bag-item-awb';
 import { ConfigService } from '../../../shared/services/config.service';
+import { OrionRepositoryService } from '../../../shared/services/orion-repository.service';
 import { BagItemHistoryQueueService } from './bag-item-history-queue.service';
 import { DoPodDetailPostMetaQueueService } from './do-pod-detail-post-meta-queue.service';
 import { QueueBullBoard } from './queue-bull-board';
-import { UpdateHubSummaryAwbOutQueueService } from './update-hub-summary-awb-out-queue.service';
+import { UpsertHubSummaryBagSortirQueueService } from './upsert-hub-summary-bag-sortir-queue.service';
 
 export class BagScanDoSortationQueueService {
   public static queue = QueueBullBoard.createQueue.add(
@@ -57,12 +58,25 @@ export class BagScanDoSortationQueueService {
         },
       );
 
-      const bagItemsAwb = await BagItemAwb.find({
-        where: {
-          bagItemId: data.bagItemId,
-          isDeleted: false,
-        },
-      });
+      const repo = new OrionRepositoryService(BagItemAwb, 'bia');
+      const q = repo.findAllRaw();
+      q.selectRaw(
+        ['bia.awb_item_id', 'awbItemId'],
+        ['bia.awb_number', 'awbNumber'],
+        ['bi.bag_id', 'bagId'],
+        ['bi.bag_item_id', 'bagItemId'],
+        ['b.bag_number', 'bagNumber'],
+      )
+        .innerJoin(e => e.bagItem, 'bi', j =>
+          j.andWhere(e => e.isDeleted, w => w.isFalse()),
+        )
+        .innerJoin(e => e.bagItem.bag, 'b', j =>
+          j.andWhere(e => e.isDeleted, w => w.isFalse()),
+        )
+        .andWhere(e => e.bagItemId, w => w.equals(data.bagItemId))
+        .andWhere(e => e.isDeleted, w => w.isFalse());
+
+      const bagItemsAwb = await q.exec();
 
       for (const itemAwb of bagItemsAwb) {
         DoPodDetailPostMetaQueueService.createJobByAwbFilter(
@@ -70,10 +84,15 @@ export class BagScanDoSortationQueueService {
           data.branchId,
           data.userId,
         );
-        UpdateHubSummaryAwbOutQueueService.perform(
-          data.branchId,
+        UpsertHubSummaryBagSortirQueueService.perform(
+          itemAwb.bagId,
+          itemAwb.bagItemId,
+          itemAwb.bagNumber,
+          itemAwb.awbItemId,
           itemAwb.awbNumber,
           data.userId,
+          data.branchId,
+          moment().toDate(),
         );
       }
     });
