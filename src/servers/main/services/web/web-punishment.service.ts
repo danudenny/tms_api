c6bb40ccb80c4d6a7db99b38dc2198bffb92f336
Rio Suga Catra Pratama <riosugacp@gmail.com>
2022-06-21 08:54:17 +0700

    add end point of branch list by user

---
+++ src/servers/main/services/web/web-punishment.service.ts
@@ -0,0 +1,26 @@
+import { Employee } from '../../../../shared/orm-entity/employee';
+import { HttpStatus, Injectable, BadRequestException } from '@nestjs/common';
+import { OrionRepositoryService } from '../../../../shared/services/orion-repository.service';
+import { PunishmentBranchListResponse } from '../../models/punishment-response.vm';
+import { PunishmentBranchListPayload } from '../../models/punishment.vm';
+import { eq } from 'lodash';
+
+export class WebPunishmentService {
+  static async findEmployeeofBranch(payload : PunishmentBranchListPayload): Promise<PunishmentBranchListResponse> {
+    const repo = new OrionRepositoryService(Employee, 't1');
+    const q = repo.findAllRaw();
+    q.selectRaw(
+      ['t2.branch_code','branchCode'],
+      ['t2.branch_id','branchId'],
+      ['t2.branch_name','branchName'],
+    );
+    q.innerJoin(e => e.branch,'t2');
+    q.andWhere(e => e.employeeId, w => w.equals(payload.employeeId));
+    const data = await q.exec();
+    const result = new PunishmentBranchListResponse();
+    result.branchCode = data[0].branchCode;
+    result.branchId = data[0].branchId;
+    result.branchName = data[0].branchName;
+    return result;
+  }
+}
