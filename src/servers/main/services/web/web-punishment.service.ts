c38c243fcbef40614934a704955fdf575f6ca472
Rio Suga Catra Pratama <riosugacp@gmail.com>
2022-06-22 14:42:59 +0700

    add bad request untuk ga ada datanya

--- src/servers/main/services/web/web-punishment.service.ts
+++ src/servers/main/services/web/web-punishment.service.ts
@@ -1,9 +1,8 @@
 import { Employee } from '../../../../shared/orm-entity/employee';
-import { HttpStatus, Injectable, BadRequestException } from '@nestjs/common';
+import { RequestErrorService } from '../../../../shared/services/request-error.service';
 import { OrionRepositoryService } from '../../../../shared/services/orion-repository.service';
 import { PunishmentBranchListResponse } from '../../models/punishment-response.vm';
 import { PunishmentBranchListPayload } from '../../models/punishment.vm';
-import { eq } from 'lodash';
 
 export class WebPunishmentService {
   static async findEmployeeofBranch(payload : PunishmentBranchListPayload): Promise<PunishmentBranchListResponse> {
@@ -17,10 +16,17 @@ export class WebPunishmentService {
     q.leftJoin(e => e.branch,'t2');
     q.andWhere(e => e.employeeId, w => w.equals(payload.employeeId));
     const data = await q.exec();
-    const result = new PunishmentBranchListResponse();
-    result.branchCode = data[0].branchCode;
-    result.branchId = data[0].branchId;
-    result.branchName = data[0].branchName;
-    return result;
+    if(data.length > 0){
+      const result = new PunishmentBranchListResponse();
+      result.branchCode = data[0].branchCode;
+      result.branchId = data[0].branchId;
+      result.branchName = data[0].branchName;
+      return result;
+    }else{
+      RequestErrorService.throwObj({
+        message: 'Data Karyawan tidak ditemukan',
+      });
+    }
+    
   }
 }
