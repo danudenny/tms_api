750f87ed7540b145badbe0c560a8d1ea2e11569b
Louis Leslie <louis.leslie@sicepat.com>
2022-07-05 11:03:51 +0700

    Refactor external.monitoring.service

--- src/servers/hub/services/monitoring/external.monitoring.service.ts
+++ src/servers/hub/services/monitoring/external.monitoring.service.ts
@@ -2,8 +2,11 @@ import { HttpException, Injectable } from '@nestjs/common';
 
 import { ConfigService } from '../../../../shared/services/config.service';
 import { HttpRequestAxiosService } from '../../../../shared/services/http-request-axios.service';
+import {
+  BaseMonitoringHubPackage,
+  PayloadMonitoringHubPackageList,
+} from '../../models/monitoring/monitoring-hub-package.vm';
 import { ExtMonitoringDetailListPayloadVm } from '../../models/monitoring/monitoring-payload.vm';
-import { BaseMonitoringHubPackage, PayloadMonitoringHubPackageList } from '../../models/monitoring/monitoring-hub-package.vm';
 
 @Injectable()
 export class ExternalHubMonitoringService {
@@ -11,12 +14,48 @@ export class ExternalHubMonitoringService {
 
   private readonly BASE_URL = ConfigService.get('hubMonitoring.baseUrl');
 
-  public async getDetail(
-    params: ExtMonitoringDetailListPayloadVm,
-  ): Promise<any> {
+  public getDetail(params: ExtMonitoringDetailListPayloadVm): Promise<any> {
     const { type, ...payload } = params;
-    const url = `${this.BASE_URL}/monitoring/${type}/list`;
+    return this.post(`/monitoring/${type}/list`, payload);
+  }
+
+  public generateReportHub(
+    report_type: string,
+    payload: BaseMonitoringHubPackage,
+  ): Promise<any> {
+    /*const url = `https://swagger.s.sicepat.tech/operation/sortation/sortation/api/v1/reporting/${report_type}/generate`;
+    const config = { headers: {
+      'accept': 'application/json',
+      'x-username': 'username',
+      'x-source': 'source service',
+      'x-channel-id': 'channel tribe',
+      'Content-Type': 'application/json',
+    }};*/
+    return this.post(`/reporting/${report_type}/generate`, payload);
+  }
+
+  public getListReporting(
+    report_type: string,
+    payload: PayloadMonitoringHubPackageList,
+  ): Promise<any> {
+    /*const url = `https://swagger.s.sicepat.tech/operation/sortation/sortation/api/v1/reporting/${report_type}/list`;
+    const config = { headers: {
+      'accept': 'application/json',
+      'x-username': 'username',
+      'x-source': 'source service',
+      'x-channel-id': 'channel tribe',
+      'Content-Type': 'application/json',
+    }};*/
+    return this.post(`/reporting/${report_type}/list`, payload);
+  }
+
+  public getTotal(params: any): Promise<any> {
+    return this.post('/monitoring/total/list', params);
+  }
+
+  private async post(path: string, payload: any): Promise<any> {
     try {
+      const url = `${this.BASE_URL}${path}`;
       const response = await this.httpRequestService
         .post(url, payload)
         .toPromise();
@@ -25,81 +64,8 @@ export class ExternalHubMonitoringService {
       if (err.response) {
         const status = err.response.status || 400;
         const errResponse = {
-          message: err.response.data && err.response.data.error,
-          statusCode: status,
-        };
-        throw new HttpException(errResponse, status);
-      }
-      throw err;
-    }
-  }
-
-  public async generateReportHub(report_type: string, payload: BaseMonitoringHubPackage): Promise<any> {
-    try {
-      const url = `${this.BASE_URL}/reporting/${report_type}/generate`;
-      /*const url = `https://swagger.s.sicepat.tech/operation/sortation/sortation/api/v1/reporting/${report_type}/generate`;
-      const config = { headers: {
-        'accept': 'application/json',
-        'x-username': 'username',
-        'x-source': 'source service',
-        'x-channel-id': 'channel tribe',
-        'Content-Type': 'application/json',
-      }};*/
-      const response = await this.httpRequestService.post(url, payload).toPromise();
-      return response;
-    } catch (err) {
-      if (err.response) {
-        const status = err.response.status || 400;
-        const errResponse = {
-          message: err.response.data && err.response.data.error,
-          statusCode: status,
-        };
-        throw new HttpException(errResponse, status);
-      }
-      throw err;
-    }
-
-}
-
-  public async getListReporting(report_type: string, payload: PayloadMonitoringHubPackageList): Promise<any> {
-    try {
-      const url = `${this.BASE_URL}/reporting/${report_type}/list`;
-      /*const url = `https://swagger.s.sicepat.tech/operation/sortation/sortation/api/v1/reporting/${report_type}/list`;
-      const config = { headers: {
-        'accept': 'application/json',
-        'x-username': 'username',
-        'x-source': 'source service',
-        'x-channel-id': 'channel tribe',
-        'Content-Type': 'application/json',
-      }};*/
-      const response = await this.httpRequestService.post(url, payload).toPromise();
-      return response;
-    } catch (err) {
-      if (err.response) {
-        const status = err.response.status || 400;
-        const errResponse = {
-          message: err.response.data && err.response.data.error,
-          statusCode: status,
-        };
-        throw new HttpException(errResponse, status);
-      }
-      throw err;
-    }
-
-  }
-
-  public async getTotal(params: any): Promise<any> {
-    const url = `${this.BASE_URL}/monitoring/total/list`;
-    try {
-      const response = await this.httpRequestService
-          .post(url, params)
-          .toPromise();
-      return response;
-    } catch (err) {
-      if (err.response) {
-        const status = err.response.status || 400;
-        const errResponse = {
-          message: err.response.data && err.response.data.error,
+          error: err.response.data && err.response.data.error,
+          message: err.response.data && err.response.data.message,
           statusCode: status,
         };
         throw new HttpException(errResponse, status);
