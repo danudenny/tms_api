cbb306d27e1c95eea7172f054d842884495e0d00
Noffrialdi <noffrialdi5@gmail.com>
2022-07-20 19:57:37 +0700

    remove unused import

--- src/servers/hub/services/sortation/web/sortation-l2-list.service.ts
+++ src/servers/hub/services/sortation/web/sortation-l2-list.service.ts
@@ -1,6 +1,5 @@
 import { HttpStatus, Injectable } from '@nestjs/common';
 import { SortationFinishHistoryResponVm } from '../../../models/sortation/web/sortation-l2-module-list.response.vm';
-import moment = require('moment');
 import { SortationFinishHistory } from '../../../../../shared/orm-entity/sortation-finish-history';
 import { BaseMetaPayloadVm } from '../../../../../shared/models/base-meta-payload.vm';
 import { OrionRepositoryService } from '../../../../../shared/services/orion-repository.service';
