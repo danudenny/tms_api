83ec95d4f160d1738bfc4bfbd549e46907345f4f
Noffrialdi <noffrialdi5@gmail.com>
2022-07-20 19:57:37 +0700

    remove unused import

--- src/servers/hub/controllers/sortation/web/sortation-l2-list.controller.ts
+++ src/servers/hub/controllers/sortation/web/sortation-l2-list.controller.ts
@@ -1,14 +1,7 @@
 import { ApiUseTags } from '../../../../../shared/external/nestjs-swagger';
 import { Body, Controller, Post, UseGuards } from '@nestjs/common';
-import { Transactional } from '../../../../../shared/external/typeorm-transactional-cls-hooked';
 import { AuthenticatedGuard } from '../../../../../shared/guards/authenticated.guard';
 import { PermissionTokenGuard } from '../../../../../shared/guards/permission-token.guard';
-import {
-  SortationL2ModuleFinishManualPayloadVm,
-  SortationL2ModuleHandoverPayloadVm,
-  SortationL2ModuleSearchPayloadVm,
-} from '../../../models/sortation/web/sortation-l2-module-search.payload.vm';
-import { SortationL2ModuleService } from '../../../services/sortation/web/sortation-l2-module.service';
 import { BaseMetaPayloadVm } from '../../../../../shared/models/base-meta-payload.vm';
 import { SortationL2ListModuleService } from '../../../services/sortation/web/sortation-l2-list.service';
 
