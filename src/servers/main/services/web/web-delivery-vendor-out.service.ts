07a1f8d1da4088a550428d60d04e380136e224c3
Rio Suga Catra Pratama <riosugacp@gmail.com>
2022-09-05 14:07:17 +0700

    resolve conflict 1

--- src/servers/main/services/web/web-delivery-vendor-out.service.ts
+++ src/servers/main/services/web/web-delivery-vendor-out.service.ts
@@ -197,4 +197,155 @@ export class WebDeliveryVendorOutService {
       listPrinterName,
     });
   }
+  static async awb(
+    payload: ScanOutPropertyAwbPayloadVm,
+  ): Promise<ScanOutPropertyAwbResponseVm> {
+    const result = new ScanOutPropertyAwbResponseVm();
+    const dataItem = [];
+    const pickupData = await this.getPickupData(payload.user_id);
+    for (const awbNumber of payload.awbNumber) {
+      const dataAwb = await this.getRawAwb(awbNumber);
+      const response = new ScanOutPropertyAwbResponse();
+      if (dataAwb) {
+        // mapping data
+        response.awbNumber = dataAwb.awbnumber;
+        response.status = "ok";
+        response.message = "Resi " + dataAwb.awbnumber + " berhasil mendapatkan datanya";
+        response.reference_no = dataAwb.reference_no;
+        response.pickup_name = pickupData.pickup_name;
+        response.pickup_address = pickupData.pickup_adress;
+        response.pickup_phone = pickupData.pickup_phone;
+        response.pickup_email = pickupData.pickup_email;
+        response.pickup_postal_code = parseInt(pickupData.pickup_postal_code);
+        response.pickup_contact = pickupData.pickup_contact;
+        response.pickup_latitude = pickupData.pickup_latitude;
+        response.pickup_longitude = pickupData.pickup_longitude;
+        response.pickup_district_id = parseInt(pickupData.pickup_district_id);
+        response.pickup_district_code = pickupData.pickup_district_code;
+        response.service_type_code = dataAwb.service_type_code;
+        response.quantity = parseInt(dataAwb.quantity);
+        response.total_item = parseInt(dataAwb.total_item);
+        response.weight = parseInt(dataAwb.weight);
+        response.volumetric = dataAwb.volumetric;
+        response.description_item = dataAwb.description_item;
+        response.item_value = parseInt(dataAwb.item_value);
+        response.insurance_flag = parseInt(dataAwb.insurance_flag);
+        response.insurance_type_code = dataAwb.insurance_type_code;
+        response.insurance_value = parseInt(dataAwb.insurance_value);
+        response.cod_flag = parseInt(dataAwb.cod_flag);
+        response.cod_value = parseInt(dataAwb.cod_value);
+        response.shipper_name = dataAwb.shipper_name;
+        response.shipper_phone = dataAwb.shipper_phone;
+        response.shipper_email = dataAwb.shipper_email;
+        response.shipper_contact = dataAwb.shipper_contact;
+        response.destination_district_code = dataAwb.destination_district_code;
+        response.destination_district_id = parseInt(dataAwb.destination_district_id);
+        response.receiver_name = dataAwb.receiver_name;
+        response.receiver_address = dataAwb.receiver_address;
+        response.receiver_phone = dataAwb.receiver_phone;
+        response.receiver_postal_code = parseInt(dataAwb.receiver_postal_code);
+        response.receiver_contact = dataAwb.receiver_contact;
+        response.special_instruction = dataAwb.special_instruction;
+        response.return_district_code = dataAwb.return_district_code;
+        response.return_phone = dataAwb.return_phone;
+        response.return_contact = dataAwb.return_contact;
+        response.return_address = dataAwb.return_address;
+      }
+      dataItem.push(response);
+    }
+    result.data = dataItem
+    return result;
+  }
+
+  // private method
+  private static async getRawAwb(awbNumber: string): Promise<any> {
+    const query = `
+    select
+        a.awb_number as awbNumber,
+        a.awb_number as reference_no,
+        ai.item_qty as quantity,
+        a.total_item as total_item,
+        a.total_weight_real_rounded as weight,
+        concat(ai.length,'x',ai.width,'x', ai.height) as volumetric,
+        ai.item_description as description_item,
+        prd.parcel_value as item_value,
+        case when prd.insurance_value = 0 then 1 else 2 end as insurance_flag,
+        case when prd.insurance_value != 0 then 'INS01' else null end as insurance_type_code,
+        prd.insurance_value as insurance_value, --kasih kondisi kalau 0 = 1
+        case when a.is_cod = true then 1 else 2 end as cod_flag,
+        ai.cod_value as cod_value,
+        a.consignee_name as receiver_name,
+        a.consignee_address as receiver_address,
+        a.consignee_phone as receiver_phone,
+        a.consignee_zip as receiver_postal_code,
+        a.consignee_name as receiver_contact,
+        a.claim_special_case as special_instruction,
+        ca.customer_account_name as shipper_name,
+        ca.phone1 as shipper_phone,
+        ca.email1 as shipper_email,
+        ca.mobile1 as shipper_contact,
+        dt.district_code as destination_district_code,
+        dt.district_id as destination_district_id
+      FROM awb a
+        INNER JOIN awb_item ai ON a.awb_id = ai.awb_id AND ai.is_deleted = false
+        LEFT JOIN package_type pt ON pt.package_type_id = a.package_type_id
+        LEFT JOIN customer_account ca ON ca.customer_account_id = a.customer_account_id
+        LEFT JOIN branch b ON b.branch_id = a.branch_id
+        LEFT JOIN pickup_request_detail prd ON ai.awb_item_id = prd.awb_item_id
+        LEFT JOIN district df ON df.district_id = a.from_id AND a.from_type = 40
+        LEFT JOIN district dt ON dt.district_id = a.to_id AND a.to_type = 40
+        LEFT JOIN branch bt ON bt.branch_id = dt.branch_id_delivery
+        LEFT JOIN representative r ON r.representative_id = bt.representative_id
+        LEFT JOIN awb_status ast ON ast.awb_status_id = ai.awb_status_id_last
+        LEFT JOIN branch bl on bl.branch_id = a.branch_id_last
+        LEFT JOIN payment_method p ON p.payment_method_id = a.payment_method_id
+        LEFT JOIN bag_item bi ON bi.bag_item_id = ai.bag_item_id_last AND bi.is_deleted = false
+        LEFT JOIN bag ba ON ba.bag_id = bi.bag_id AND ba.is_deleted = false
+        LEFT JOIN do_pod_deliver_detail dpd ON dpd.awb_id = a.awb_id
+          AND dpd.awb_status_id_last <> 14000 AND dpd.is_deleted = false
+        LEFT JOIN do_pod_return_detail dprd ON dprd.awb_id = a.awb_id
+          AND dprd.awb_status_id_last in (25650, 25000) AND dprd.is_deleted = false
+        LEFT JOIN awb_return ar ON ar.origin_awb_id = ai.awb_id AND ar.is_deleted = false
+        LEFT JOIN awb_substitute asub ON asub.awb_number = a.awb_number
+      WHERE a.awb_number = :awbNumber
+      AND a.is_deleted = false
+      LIMIT 1;
+    `;
+
+    const rawData = await RawQueryService.queryWithParams(query, {
+      awbNumber,
+    });
+    return rawData ? rawData[0] : null;
+  }
+
+  private static async getPickupData(userID: number): Promise<any> {
+    const query = `
+    SELECT e.fullname as pickup_name,
+      c.address as pickup_adress,
+      e.phone1 as pickup_phone,
+      a.email as pickup_email,
+      e.zip_code_id_card as pickup_postal_code,
+      e.fullname as pickup_contact,
+      c.latitude as pickup_latitude,
+      c.longitude as pickup_longitude,
+      d.district_code as pickup_district_code,
+      d.district_id as pickup_district_id,
+      d.district_code as return_district_code,
+      e.phone1 as return_phone,
+      e.fullname as return_contact,
+      c.address as return_address
+    FROM users a
+      JOIN user_role b on a.user_id = b.user_id
+      JOIN branch c on b.branch_id = c.branch_id
+      JOIN district d on c.district_id = d.district_id
+      JOIN employee e on a.employee_id = e.employee_id
+    WHERE a.user_id = :userID
+    ;`;
+
+    const rawData = await RawQueryService.queryWithParams(query, {
+      userID,
+    });
+    return rawData ? rawData[0] : null;
+  }
+
 }
