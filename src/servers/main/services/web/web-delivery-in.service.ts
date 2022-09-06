14adf338ad705418546cfe22ab40b65d449307a8
Noffrialdi <noffrialdi5@gmail.com>
2022-09-06 09:23:46 +0700

    add interval 7 hour

--- src/servers/main/services/web/web-delivery-in.service.ts
+++ src/servers/main/services/web/web-delivery-in.service.ts
@@ -675,7 +675,7 @@ export class WebDeliveryInService {
       ['t1.ref_representative_code', 'representativeCode'],
       ['t2.bag_seq', 'bagSeq'],
       ['t2.bag_item_id', 'bagItemId'],
-      ['t1.created_time', 'createdTime'],
+      [`t1.created_time - INTERVAL '7 HOUR'`, 'createdTime'],
       ['t3.branch_name', 'branchName'], // Gerai Tujuan
       ['t3.branch_id', 'branchId'],
       ['t5.branch_name', 'branchScanName'], // Hub Sortir
