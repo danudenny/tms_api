fb88ca735a3da408f596581873681dbe3ce711f2
Noffrialdi <noffrialdi5@gmail.com>
2022-10-13 10:13:10 +0700

    rename backdoor to sanity

--- src/servers/hub/controllers/sanity/endpoint-sanity.controller.ts
+++ src/servers/hub/controllers/sanity/endpoint-sanity.controller.ts
@@ -3,7 +3,8 @@ import { ApiUseTags } from '../../../../shared/external/nestjs-swagger';
 import { AuthenticatedGuard } from '../../../../shared/guards/authenticated.guard';
 import { PermissionTokenGuard } from '../../../../shared/guards/permission-token.guard';
 import { BaseMetaPayloadVm } from '../../../../shared/models/base-meta-payload.vm';
-import { EndpointSanityService } from '../../services/backdoor/endpoint-sanity.service';
+import { DoSmdSanityPayloadVm } from '../../models/sanity/endpoint-sanity.payload.vm';
+import { EndpointSanityService } from '../../services/sanity/endpoint-sanity.service';
 
 @ApiUseTags('Endpoint Sanity')
 @Controller('backdoor/sanity')
@@ -16,7 +17,7 @@ export class EndpointSanityController {
 
   @Post('doSmd')
   @UseGuards(AuthenticatedGuard, PermissionTokenGuard)
-  public async updateDoSmd(@Body() payload: BaseMetaPayloadVm) {
+  public async updateDoSmd(@Body() payload: DoSmdSanityPayloadVm) {
     return this.sanityService.updateDoSmd(payload);
   }
 }
