f141179d4779fd6ea280292c87a9d15dac7402df
Louis Leslie <louis.leslie@sicepat.com>
2022-10-13 10:54:05 +0700

    Add permission role guard

--- src/servers/hub/controllers/sanity/sanity.controllers.ts
+++ src/servers/hub/controllers/sanity/sanity.controllers.ts
@@ -2,19 +2,24 @@ import {
   Body,
   Controller,
   Delete,
+  Get,
   Inject,
   Post,
   UseGuards,
 } from '@nestjs/common';
+import { RoleAuthGuardOptions } from '../../../../shared/decorators/role-auth-guard-options.decorator';
 
 import { ApiUseTags } from '../../../../shared/external/nestjs-swagger';
 import { AuthenticatedGuard } from '../../../../shared/guards/authenticated.guard';
 import { PermissionTokenGuard } from '../../../../shared/guards/permission-token.guard';
+import { PermissionRoleGuard } from '../../../../shared/guards/permission.role.guard';
 import { SANITY_SERVICE, SanityService } from '../../interfaces/sanity.service';
 import { DeleteBagRequest } from '../../models/sanity/sanity.request';
 import { DeleteBagResponse } from '../../models/sanity/sanity.response';
 
 @ApiUseTags('Backdoor APIs for Sanity Testing')
+@RoleAuthGuardOptions('1', '11')
+@UseGuards(AuthenticatedGuard, PermissionTokenGuard, PermissionRoleGuard)
 @Controller('sanity')
 export class SanityController {
   constructor(
@@ -22,8 +27,7 @@ export class SanityController {
   ) {}
 
   @Delete('bag')
-  @UseGuards(AuthenticatedGuard, PermissionTokenGuard)
-  public scanOutList(
+  public deleteBag(
     @Body() payload: DeleteBagRequest,
   ): Promise<DeleteBagResponse> {
     return this.service.deleteBag(payload);
