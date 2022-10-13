a6e26a328b40ad3b349833ffc8551fd57d36391e
Louis Leslie <louis.leslie@sicepat.com>
2022-10-13 13:20:42 +0700

    Fix deleteBag implementation in sanity service

--- src/servers/hub/services/sanity/sanity.service.ts
+++ src/servers/hub/services/sanity/sanity.service.ts
@@ -1,37 +1,53 @@
 import { HttpStatus } from '@nestjs/common';
+import _ = require('lodash');
 import moment = require('moment');
 import { getManager } from 'typeorm';
 
+import { BagService } from '../../../../servers/main/services/v1/bag.service';
 import { Bag } from '../../../../shared/orm-entity/bag';
 import { BagItem } from '../../../../shared/orm-entity/bag-item';
-import { OrionRepositoryService } from '../../../../shared/services/orion-repository.service';
+import { AuthService } from '../../../../shared/services/auth.service';
 import { SanityService } from '../../interfaces/sanity.service';
 import { DeleteBagRequest } from '../../models/sanity/sanity.request';
 import { DeleteBagResponse } from '../../models/sanity/sanity.response';
 
 export default class DefaultSanityService implements SanityService {
+  // Delete bag(s) by bag_number(s) and all bag_items in the corresponding bags
   public async deleteBag(
     payload: DeleteBagRequest,
   ): Promise<DeleteBagResponse> {
-    const q = new OrionRepositoryService(Bag, 'b').createQueryBuilder();
-    const bags = await q
-      .select('b.bag_id', 'id')
-      .andWhere('bag_number IN (:...bagNumbers)', {
-        bagNumbers: payload.bagNumbers,
-      })
-      .andWhere('is_deleted = FALSE')
-      .execute();
-    const bagIds = bags.map(bag => bag.id);
+    const auth = AuthService.getAuthMetadata();
+    let bags = await Promise.all(
+      payload.bagNumbers.map(bagNumber => BagService.validBagNumber(bagNumber)),
+    );
+    bags = _.uniq(_.compact(bags));
+    if (!bags.length) {
+      return {
+        statusCode: HttpStatus.NO_CONTENT,
+        message: 'Bag number(s) not found or have been deleted',
+        data: {
+          bagNumbers: payload.bagNumbers,
+        },
+      };
+    }
+    const bagIds = bags.map(bag => bag.bagId);
     const now = moment().toDate();
     await getManager().transaction(async manager => {
-      await manager.update(Bag, `bag_id IN (${bagIds.join(',')})`, {
+      await manager.update(Bag, bagIds, {
         isDeleted: true,
         updatedTime: now,
+        userIdUpdated: auth.userId,
       });
-      await manager.update(BagItem, `bag_id IN (${bagIds.join(',')})`, {
-        isDeleted: true,
-        updatedTime: now,
-      });
+      await manager
+        .createQueryBuilder()
+        .update(BagItem)
+        .set({
+          isDeleted: true,
+          updatedTime: now,
+          userIdUpdated: auth.userId,
+        })
+        .where('bag_id IN (:...ids)', { ids: bagIds })
+        .execute();
     });
     return {
       statusCode: HttpStatus.OK,
