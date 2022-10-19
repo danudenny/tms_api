0828600e751cc8cd6647d4d58f4de900965ce705
Rio Suga Catra Pratama <riosugacp@gmail.com>
2022-10-19 09:54:09 +0700

    di remove bagian dlv di pod manual

--- src/servers/main/services/web/web-awb-deliver.service.ts
+++ src/servers/main/services/web/web-awb-deliver.service.ts
@@ -75,24 +75,12 @@ export class WebAwbDeliverService {
                 syncManualDelivery = true;
               }
 
-              let isNotLost = true;
-              if(delivery.awbStatusId == AWB_STATUS.DLV){
-                //ketika mau scan deliver
-                let checkLost = await AwbService.validationContainAwBStatus(true, awb.awbNumber, awb.awbItemId, false, true);
-                if(checkLost[0] == true){
-                  isNotLost = false
-                }
-              }
-
               if (syncManualDelivery) {
                 // add handel final status
                 const statusFinal = [AWB_STATUS.DLV];
                 if (statusFinal.includes(awb.awbStatusIdLast)) {
                   response.status = 'error';
                   response.message = `Resi ${delivery.awbNumber} sudah Final Status !`;
-                } else if(delivery.awbStatusId == AWB_STATUS.DLV && !isNotLost){
-                  response.status = 'error';
-                  response.message = `Resi ${delivery.awbNumber} lost`;
                 } else {
                   // set data deliver for sync data
                   delivery.doPodDeliverId = awbDeliver.doPodDeliverId;
