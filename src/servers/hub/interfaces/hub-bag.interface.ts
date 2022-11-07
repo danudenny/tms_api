4e90e5cfd48250ed7a1db93e4915577a6bbec73e
Louis Leslie <louis.leslie@sicepat.com>
2022-11-07 13:17:43 +0700

    Fix HubBagService.insertAWB interface definition

--- src/servers/hub/interfaces/hub-bag.interface.ts
+++ src/servers/hub/interfaces/hub-bag.interface.ts
@@ -1,7 +1,12 @@
-import { HubBagInsertAwbPayload } from '../models/bag/hub-bag.payload';
+import {
+  HubBagInsertAwbPayload,
+  HubBagInsertAwbResponse,
+} from '../models/bag/hub-bag.payload';
 
 export const HUB_BAG_SERVICE = 'HUB_BAG_SERVICE';
 
 export interface HubBagService {
-  insertAWB: (payload: HubBagInsertAwbPayload) => Promise<any>;
+  insertAWB: (
+    payload: HubBagInsertAwbPayload,
+  ) => Promise<HubBagInsertAwbResponse>;
 }
