0baf76ecd376c6e666d247ad2abae25bb69df725
Indra Hasan <indra.hasan@sicepat.com>
2022-11-24 17:35:42 +0700

    change loop to random index array

--- src/shared/services/bag-number.service.ts
+++ src/shared/services/bag-number.service.ts
@@ -29,17 +29,9 @@ export class BagNumberService {
         const length = 8;
         let randomBagNumber = sampleSize('012345678900123456789001234567890ABCDEFGHIJKLMNOPQRSTUVWXYZZYWVUTSRQPONMLKJIHGFEDCBA', length).join('');
         const arrayLastTreeChar = randomBagNumber.substr(length - 3).split('');
-        for (let i = 0; i <= 2; i++) {
-            let char = arrayLastTreeChar[i];
-            if (/^[A-Z]$/i.test(char)) {
-                // jika alphabet keluar loop
-                break;
-            } else {
-                // jika number replace dan keluar loop
-                char = sampleSize('ABCDEFGHIJKLMNOPQRSTUVWXYZ', 1).join('');
-                arrayLastTreeChar[i] = char;
-                break;
-            }
+        const randomIndex = Math.floor(Math.random() * arrayLastTreeChar.length);
+        if (/^\d+$/.test(arrayLastTreeChar[randomIndex])) {
+            arrayLastTreeChar[randomIndex] = sampleSize('ABCDEFGHIJKLMNOPQRSTUVWXYZ', 1).join('');
         }
         const lastThreeChar = arrayLastTreeChar.toString().replace(/,/g, '');
         randomBagNumber = prefix + randomBagNumber.slice(0, -3) + lastThreeChar;
