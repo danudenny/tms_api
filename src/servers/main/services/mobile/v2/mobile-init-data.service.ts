bb081ca62e3458469d23547be8ed58f1133d202f
Antonius <antonius@sicepat.com>
2022-12-13 10:51:38 +0700

    improvement for last sync date time

--- src/servers/main/services/mobile/v2/mobile-init-data.service.ts
+++ src/servers/main/services/mobile/v2/mobile-init-data.service.ts
@@ -131,18 +131,18 @@ export class V2MobileInitDataService {
       qw.orWhere(p => p.reasonCategory, w => w.equals('pod_cod'));
     });
 
-    if (fromDate) {
-      q.andWhereIsolated(qw => {
-        qw.where(
-          e => e.updatedTime,
-          w => w.greaterThanOrEqual(moment(fromDate).toDate()),
-        );
-        qw.orWhere(
-          e => e.createdTime,
-          w => w.greaterThanOrEqual(moment(fromDate).toDate()),
-        );
-      });
-    }
+    // if (fromDate) {
+    //   q.andWhereIsolated(qw => {
+    //     qw.where(
+    //       e => e.updatedTime,
+    //       w => w.greaterThanOrEqual(moment(fromDate).toDate()),
+    //     );
+    //     qw.orWhere(
+    //       e => e.createdTime,
+    //       w => w.greaterThanOrEqual(moment(fromDate).toDate()),
+    //     );
+    //   });
+    // }
     return await q.exec();
   }
 
